933ff5ec5b7e030c19dac745c1382059f0c9963f
Yoshifumi Inoue <yosin@chromium.org>
2013-12-20 18:00:00 +0900

    [Evita] Add resources

---
+++ evita/res/build.h
@@ -0,0 +1,16 @@
+#if defined(UNICODE)
+#define MY_Product "Evita Common Lisp"
+#define MY_ProductW L"Evita Common Lisp"
+#else // defined(UNICODE)
+#define MY_Product "Evita Common Lisp"
+#define MY_ProductW L"Evita Common Lisp"
+#endif // defined(UNICODE)
+#define MY_VersionMajor 3
+#define MY_VersionMinor 0
+#define MY_VersionBuild 9518
+#define MY_VersionIncre 1
+#define MY_FILEVERSION 3,0,9518,1
+#define MY_FileVersion "3.0.9518.1\0"
+#define MY_FileVersionW L"3.0.9518.1\0"
+#define MY_CompanyName "Project Vogue\0"
+#define MY_LegalCopyright   "Copyright © 1994-2007 by Project Vogue\0"

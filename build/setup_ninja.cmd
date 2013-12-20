9df48d6f1489316468014bf337a0b7281bc3ffbf
Yoshifumi Inoue <yosin@chromium.org>
2013-12-20 18:47:04 +0900

    [Build] Add comments

--- build/setup_ninja.cmd
+++ build/setup_ninja.cmd
@@ -1,9 +1,18 @@
+@if "%_echo%"=="" echo off
 : Create *.ninja files under out/
-cd src
+setlocal
+
+: See /tools/gyp/pylib/gyp/MSVSVersion.py
+: -G msvs_version=2013
+: GYP_MSVS_VERSION=2013|2013e|...
+: GYP_MSVS_OVERRIDE_PATH
+
 .\tools\gyp\gyp ^
-    -G msvs-version=12 ^
-    -f ninja ^
+  --debug=general --debug=includes --debug=variables ^
+    --format=ninja ^
     --generator-output=.. ^
     --include build\common.gypi ^
   build\all.gyp
+
+endlocal
 exit/b

ac0d0365b5e2b271ce11bf88f08f073274a1bc1b
Eval <eval1749@gmail.com>
2013-12-21 16:01:46 +0900

    [Build] Introduce Windows pre-compiled header support

---
+++ build/precompile.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define STRICT
+#define INC_OLE2
+//#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+// warning C4530: C++ exception handler used, but unwind semantics are not
+// enabled. Specify /EHsc
+#pragma warning(disable: 4530)

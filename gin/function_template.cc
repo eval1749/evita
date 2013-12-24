1aa1f7e9839b189ab8514561285824d2d43d3448
Yoshifumi Inoue <yosin@chromium.org>
2013-12-24 17:06:15 +0900

    [Build] Import chromium/gin

---
+++ gin/function_template.cc
@@ -0,0 +1,15 @@
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "gin/function_template.h"
+
+namespace gin {
+
+namespace internal {
+
+WrapperInfo CallbackHolderBase::kWrapperInfo = { kEmbedderNativeGin };
+
+}  // namespace internal
+
+}  // namespace gin

04ce77816ea4363a09945d4e5718fafad77efc45
Eval <eval1749@gmail.com>
2013-12-28 13:59:49 +0900

    [evita] Fix coding style for evita/cm_CmdProc.{cpp,h}

--- evita/cm_CmdProc.cpp
+++ evita/cm_CmdProc.cpp
@@ -9,228 +9,190 @@
 //
 // @(#)$Id: //proj/evcl3/mainline/listener/winapp/cm_CmdProc.cpp#2 $
 //
-#include "./cm_CmdProc.h"
+#include "evita/cm_CmdProc.h"
 
-#include "./ed_util.h"
-
+#include "base/logging.h"
+#include "base/strings/string16.h"
 #include "evita/editor/application.h"
 #include "evita/editor/dom_lock.h"
-#include "./vi_Buffer.h"
-#include "./vi_EditPane.h"
-#include "./vi_Selection.h"
-#include "./vi_TextEditWindow.h"
+#include "evita/ed_util.h"
+#include "evita/vi_Buffer.h"
+#include "evita/vi_EditPane.h"
+#include "evita/vi_Selection.h"
+#include "evita/vi_TextEditWindow.h"
 
-namespace Command
-{
+namespace Command {
 
 KeyBindEntry* g_pQuotedInsert;
 KeyBindEntry* g_pStartArgument;
 
 /// <summary>
-///  Construct KeyBinds object
+/// Construct KeyBinds object
 /// </summary>
-KeyBinds::KeyBinds()
-{
-    myZeroMemory(m_rgpBind, sizeof(m_rgpBind));
-} // KeyBinds::KeyBinds
+KeyBinds::KeyBinds() {
+  myZeroMemory(m_rgpBind, sizeof(m_rgpBind));
+}
+
+//////////////////////////////////////////////////////////////////////
+//
+// Processor
+//
+Processor::Processor()
+    : m_pLastCommand(nullptr),
+      m_pThisCommand(nullptr) {
+  Reset();
+}
 
 /// <summary>
-///   Advance command processing state with specified keyboard event.
+/// Advance command processing state with specified keyboard event.
 /// </summary>
 /// <param name="fRepeat">True if keyboard repeat event.</param>
 /// <param name="nKey">Key code.</param>
 /// <param name="pWindow">A window object causing an event.</param>
-void Processor::Execute(
-    CommandWindow*  pWindow,
-    uint            nKey,
-    uint            fRepeat )
-{
-    KeyBindEntry* pEntry = NULL;
+void Processor::Execute(CommandWindow* pWindow, uint32 nKey, uint32 fRepeat) {
+  KeyBindEntry* pEntry = nullptr;
 
-    m_pWindow = pWindow;
-    m_wchLast = static_cast<char16>(nKey);
+  m_pWindow = pWindow;
+  m_wchLast = static_cast<char16>(nKey);
 
-    TextEditWindow* pEditWindow = pWindow->DynamicCast<TextEditWindow>();
+  TextEditWindow* pEditWindow = pWindow->DynamicCast<TextEditWindow>();
 
-    if (NULL == pEditWindow)
-    {
-        m_pFrame     = Application::instance()->GetActiveFrame();
-        m_pSelection = NULL;
-    }
-    else
-    {
-        m_pSelection = pEditWindow->GetSelection();
+  if (!pEditWindow) {
+    m_pFrame = Application::instance()->GetActiveFrame();
+    m_pSelection = nullptr;
+  } else {
+    m_pSelection = pEditWindow->GetSelection();
+    m_pFrame = m_pSelection->GetWindow()->container_widget()
+        .as<EditPane>()->GetFrame();
+  }
 
-        m_pFrame = m_pSelection->GetWindow()->container_widget().
-            as<EditPane>()->GetFrame();
-    }
+  m_rgnKey[m_cKeys] = nKey;
+  m_cKeys += 1;
 
-    m_rgnKey[m_cKeys] = nKey;
-    m_cKeys += 1;
-
-    switch (m_eState)
-    {
+  switch (m_eState) {
     case State_Arg:
-        if (nKey >= '0' && nKey <= '9')
-        {
-            m_iArg *= 10;
-            m_iArg += nKey - '0';
-            return;
-        }
-
-        m_iArg *= m_iSign;
-
-        pEntry = pWindow->MapKey(nKey);
-        break;
+      if (nKey >= '0' && nKey <= '9') {
+        m_iArg *= 10;
+        m_iArg += nKey - '0';
+        return;
+      }
+      m_iArg *= m_iSign;
+      pEntry = pWindow->MapKey(nKey);
+      break;
 
     case State_Continue:
-        pEntry = pWindow->MapKey(nKey);
-        break;
+      pEntry = pWindow->MapKey(nKey);
+      break;
 
     case State_Quote:
-        if (NULL != m_pSelection)
-        {
-            // Insert ASCII character into buffer.
-            if (nKey >= (Mod_Ctrl | 'A') && nKey <= (Mod_Ctrl | 0x5F))
-            {
-                nKey &= 0x1F;
-            }
+      if (m_pSelection) {
+        // Insert ASCII character into buffer.
+        if (nKey >= (Mod_Ctrl | 'A') && nKey <= (Mod_Ctrl | 0x5F))
+          nKey &= 0x1F;
 
-            if (nKey <= 0x7F)
-            {
-                m_pSelection->TypeChar(static_cast<char16>(nKey));
-            }
-        }
-        Reset();
-        return;
+        if (nKey <= 0x7F)
+          m_pSelection->TypeChar(static_cast<char16>(nKey));
+      }
+      Reset();
+      return;
 
     case State_StartArg:
-        if ('-' == nKey)
-        {
-            m_eState = State_Arg;
-            m_iSign  = -1;
-            return;
-        } // if
-
-        if (nKey >= '0' && nKey <= '9')
-        {
-            m_eState = State_Arg;
-            m_iArg = static_cast<Count>(nKey - '0');
-            return;
-        } // if
-
-        m_iArg = 4;
-
-        pEntry = pWindow->MapKey(nKey);
-        break;
+      if (nKey == '-') {
+        m_eState = State_Arg;
+        m_iSign = -1;
+        return;
+      }
+      if (nKey >= '0' && nKey <= '9') {
+        m_eState = State_Arg;
+        m_iArg = static_cast<Count>(nKey - '0');
+        return;
+      }
+      m_iArg = 4;
+      pEntry = pWindow->MapKey(nKey);
+      break;
 
     case State_Start:
-        m_pFrame->ResetMessages();
-
-        pEntry = pWindow->MapKey(nKey);
-
-        m_iArg = fRepeat ? 2 : 1;
-
-        if (pEntry == g_pStartArgument)
-        {
-            m_eState = State_StartArg;
-            m_iArg  = 0;
-            m_iSign = 1;
-            return;
-        } // if
-        break;
+      m_pFrame->ResetMessages();
+      pEntry = pWindow->MapKey(nKey);
+      m_iArg = fRepeat ? 2 : 1;
+      if (pEntry == g_pStartArgument) {
+        m_eState = State_StartArg;
+        m_iArg = 0;
+        m_iSign = 1;
+        return;
+      }
+      break;
 
     default:
-        CAN_NOT_HAPPEN();
-    } // switch state
+      NOTREACHED();
+  }
 
-    if (pEntry == g_pStartArgument)
-    {
-        m_eState = State_Continue;
-        m_iArg *= 4;
-        return;
-    } // if
+  if (pEntry == g_pStartArgument) {
+    m_eState = State_Continue;
+    m_iArg *= 4;
+    return;
+  }
 
-    if (pEntry == g_pQuotedInsert)
-    {
-        m_eState = State_Quote;
-        return;
-    }
+  if (pEntry == g_pQuotedInsert) {
+    m_eState = State_Quote;
+    return;
+  }
 
-    if (pEntry == NULL)
-    {
-        reportUnboundKeys();
-        Reset();
-        return;
-    } // if
+  if (!pEntry) {
+    reportUnboundKeys();
+    Reset();
+    return;
+  }
 
-    switch (pEntry->GetKind())
-    {
+  switch (pEntry->GetKind()) {
     case Bind_Command:
-        m_pThisCommand = pEntry->StaticCast<Command>();
-        {
-          UI_DOM_AUTO_LOCK_SCOPE();
-          pEntry->StaticCast<Command>()->Execute(this);
-        }
-        m_pLastCommand = m_pThisCommand;
-        Reset();
-        break;
+      m_pThisCommand = pEntry->StaticCast<Command>();
+      {
+        UI_DOM_AUTO_LOCK_SCOPE();
+        pEntry->StaticCast<Command>()->Execute(this);
+      }
+      m_pLastCommand = m_pThisCommand;
+      Reset();
+      break;
 
     case Bind_KeyBinds:
-        m_pKeyBinds = pEntry->StaticCast<KeyBinds>();
-        m_eState = State_Continue;
-        break;
+      m_pKeyBinds = pEntry->StaticCast<KeyBinds>();
+      m_eState = State_Continue;
+      break;
 
     default:
-        CAN_NOT_HAPPEN();
-    } // switch kind
-} // Processor::Execute
+      NOTREACHED();
+  }
+}
 
-void Processor::reportUnboundKeys()
-{
-    CharSink_<> oSink;
-    for (uint i = 0; i < m_cKeys; i++)
-    {
-        auto const nKey = m_rgnKey[i];
+void Processor::reportUnboundKeys() {
+  base::string16 key_sequence;
+  for (uint i = 0; i < m_cKeys; i++) {
+    auto const nKey = m_rgnKey[i];
+    if (nKey & Mod_Ctrl)
+      key_sequence.append(L"Ctrl+");
 
-        if (nKey & Mod_Ctrl)
-        {
-            oSink.Add(L"Ctrl+");
-        }
+    if (nKey & Mod_Shift)
+      key_sequence.append(L"Shift+");
 
-        if (nKey & Mod_Shift)
-        {
-            oSink.Add(L"Shift+");
-        }
+    if (nKey <= 0xFF)
+      key_sequence.push_back(static_cast<base::char16>(nKey));
+    else
+      key_sequence.append(GetKeyName(nKey));
+  }
 
-        if (nKey <= 0xFF)
-        {
-            oSink.Add(static_cast<char16>(nKey));
-        }
-        else
-        {
-            oSink.Add(GetKeyName(nKey));
-        }
-    } // for
+  Application::instance()->GetActiveFrame()->ShowMessage(
+      MessageLevel_Warning,
+      IDS_NOT_COMMAND,
+      key_sequence.c_str());
+}
 
-    oSink.Add(static_cast<char16>(0));
+void Processor::Reset() {
+  m_eState = State_Start;
+  m_cKeys = 0;
+  m_iArg = 0;
+  m_iSign = 0;
+  m_pKeyBinds = nullptr;
+}
 
-    Application::instance()->GetActiveFrame()->ShowMessage(
-        MessageLevel_Warning,
-        IDS_NOT_COMMAND,
-        oSink.GetStart() );
-} // reportUnboundKeys
-
-/// <summary>
-///   Reset command processor.
-/// </summary>
-void Processor::Reset()
-{
-    m_eState    = State_Start;
-    m_cKeys     = 0;
-    m_iArg      = 0;
-    m_iSign     = 0;
-    m_pKeyBinds = NULL;
-} // Processor::Reset
-
-} // Command
+}  // namespace Command
--- evita/cm_CmdProc.h
+++ evita/cm_CmdProc.h
@@ -1,81 +1,68 @@
-//////////////////////////////////////////////////////////////////////////////
-//
-// evcl - listener - edit buffer
-// listener/winapp/ed_buffer.h
-//
-// Copyright (C) 1996-2007 by Project Vogue.
+// Copyright (C) 1996-2013 by Project Vogue.
 // Written by Yoshifumi "VOGUE" INOUE. (yosi@msn.com)
-//
-// @(#)$Id: //proj/evcl3/mainline/listener/winapp/cm_CmdProc.h#1 $
-//
 #if !defined(INCLUDE_listener_winapp_command_processor_h)
 #define INCLUDE_listener_winapp_command_processor_h
 
-#include "./li_util.h"
+#include "evita/li_util.h"
 
 class CommandWindow;
 class Frame;
 class Selection;
 
-enum Mod
-{
-    Mod_None        = 0,
-    Mod_Ctrl        = 0x200,
-    Mod_Shift       = 0x400,
-    Mod_CtrlShift   = 0x600,
-}; // Mod
+enum Mod {
+  Mod_None = 0,
+  Mod_Ctrl = 0x200,
+  Mod_Shift = 0x400,
+  Mod_CtrlShift = 0x600,
+};
 
+namespace Command {
 
-namespace Command
-{
-
-enum Bind
-{
-    Bind_Command,
-    Bind_KeyBinds,
-    Bind_Argument,
-}; // Bind
+enum Bind {
+  Bind_Command,
+  Bind_KeyBinds,
+  Bind_Argument,
+};
 
 //////////////////////////////////////////////////////////////////////
 //
 // Context
 //
-class Context
-{
-    protected: enum State
-    {
-        State_Arg,
-        State_Continue,
-        State_Quote,
-        State_Start,
-        State_StartArg,
-    }; // State
+class Context {
+  protected: enum State {
+    State_Arg,
+    State_Continue,
+    State_Quote,
+    State_Start,
+    State_StartArg,
+  };
 
-    protected: State            m_eState;
-    protected: Count            m_iArg;
-    protected: Count            m_iSign;
-    protected: uint             m_cKeys;
-    protected: uint             m_rgnKey[10];
-    protected: CommandWindow*   m_pWindow;
-    protected: Frame*     m_pFrame;
-    protected: Selection*       m_pSelection;
-    protected: char16               m_wchLast;
+  protected: State m_eState;
+  protected: Count m_iArg;
+  protected: Count m_iSign;
+  protected: uint m_cKeys;
+  protected: uint m_rgnKey[10];
+  protected: CommandWindow* m_pWindow;
+  protected: Frame* m_pFrame;
+  protected: Selection* m_pSelection;
+  protected: char16 m_wchLast;
 
-    // ctor
-    public: Context() {}
+  // ctor
+  public: Context() = default;
+  public: ~Context() = default;
 
-    // [G]
-    public: Count          GetArg()       const { return m_iArg; }
-    public: Frame*   GetFrame()     const { return m_pFrame; }
-    public: char16         GetLastChar()  const { return m_wchLast; }
-    public: Selection*     GetSelection() const { return m_pSelection; }
-    public: CommandWindow* GetWindow()    const { return m_pWindow; }
+  // [G]
+  public: Count GetArg() const { return m_iArg; }
+  public: Frame* GetFrame() const { return m_pFrame; }
+  public: char16 GetLastChar() const { return m_wchLast; }
+  public: Selection* GetSelection() const { return m_pSelection; }
+  public: CommandWindow* GetWindow() const { return m_pWindow; }
 
-    // [H]
-    public: bool HasArg() const
-        { return 0 != m_iSign; }
-}; // Context
+  // [H]
+  public: bool HasArg() const { return 0 != m_iSign; }
 
+  DISALLOW_COPY_AND_ASSIGN(Context);
+};
 
 class Command;
 class KeyBinds;
@@ -84,90 +71,88 @@ class KeyBinds;
 //
 // KeyBindEntry
 //
-class KeyBindEntry : public Castable_<KeyBindEntry>
-{
-    public: virtual Bind            GetKind() const = 0;
-}; // KeyBindEntry
-
+class KeyBindEntry : public Castable_<KeyBindEntry> {
+  public: KeyBindEntry() = default;
+  public: virtual ~KeyBindEntry() = default;
+  public: virtual Bind GetKind() const = 0;
+  DISALLOW_COPY_AND_ASSIGN(KeyBindEntry);
+};
 
 //////////////////////////////////////////////////////////////////////
 //
 // Command
 //
-class Command : public HasKind_<Command, KeyBindEntry, Bind>
-{
-    public: static Bind Kind_() { return Bind_Command; }
+class Command : public HasKind_<Command, KeyBindEntry, Bind> {
+  public: static Bind Kind_() { return Bind_Command; }
 
-    public: typedef void (*CommandFn)(const Context*);
-    private: CommandFn  m_pfn;
-    public: Command(CommandFn pfn) : m_pfn(pfn) {}
-    public: void Execute(const Context* pCtx) { m_pfn(pCtx); }
-}; // Command
+  public: typedef void (*CommandFn)(const Context*);
+  private: CommandFn m_pfn;
+  public: Command(CommandFn pfn) : m_pfn(pfn) {}
+  public: void Execute(const Context* pCtx) { m_pfn(pCtx); }
 
+  DISALLOW_COPY_AND_ASSIGN(Command);
+};
 
 //////////////////////////////////////////////////////////////////////
 //
 // KeyBinds
 //
-class KeyBinds : public HasKind_<KeyBinds, KeyBindEntry, Bind>
-{
-    public: static Bind Kind_() { return Bind_KeyBinds; }
+class KeyBinds : public HasKind_<KeyBinds, KeyBindEntry, Bind> {
+  public: static Bind Kind_() { return Bind_KeyBinds; }
 
-    private: KeyBindEntry* m_rgpBind[0x100 * 8];
+  private: KeyBindEntry* m_rgpBind[0x100 * 8];
 
-    public: KeyBinds();
-    public: void Bind(uint, KeyBindEntry*);
-    public: void Bind(uint, Command::CommandFn);
-    public: KeyBindEntry* MapKey(uint) const;
-}; // KeyBinds
+  public: KeyBinds();
+  public: void Bind(uint, KeyBindEntry*);
+  public: void Bind(uint, Command::CommandFn);
+  public: KeyBindEntry* MapKey(uint) const;
 
+  DISALLOW_COPY_AND_ASSIGN(KeyBinds);
+};
 
 //////////////////////////////////////////////////////////////////////
 //
 // Processor
 //
-class Processor : public Context
-{
-    private: static KeyBinds* sm_pGlobakBinds;
+class Processor : public Context {
+  private: static KeyBinds* sm_pGlobakBinds;
 
-    private: Command*   m_pLastCommand;
-    private: KeyBinds*  m_pKeyBinds;
-    private: Command*   m_pThisCommand;
+  private: Command* m_pLastCommand;
+  private: KeyBinds* m_pKeyBinds;
+  private: Command* m_pThisCommand;
 
-    // ctor
-    public: Processor() :
-        m_pLastCommand(NULL),
-        m_pThisCommand(NULL)
-            { Reset(); }
+  // ctor
+  public: Processor();
+  public: virtual ~Processor() = default;
 
-    // [E]
-    public: void Execute(CommandWindow*, uint, uint);
+  // [E]
+  public: void Execute(CommandWindow*, uint32, uint32);
 
-    // [G]
-    public: Command* GetLastCommand() const { return m_pLastCommand; }
-    public: Command* GetThisCommand() const { return m_pThisCommand; }
-    public: static void GlobalInit();
+  // [G]
+  public: Command* GetLastCommand() const { return m_pLastCommand; }
+  public: Command* GetThisCommand() const { return m_pThisCommand; }
+  public: static void GlobalInit();
 
-    // [R]
-    protected: void reportUnboundKeys();
-    public: void    Reset();
-}; // Processor
+  // [R]
+  protected: void reportUnboundKeys();
+  public: void Reset();
+
+  DISALLOW_COPY_AND_ASSIGN(Processor);
+};
 
 extern KeyBinds* g_pGlobalBinds;
 
 void TypeChar(const Context*);
 
-inline uint MapVKey(uint nVKey)
-{
-    return nVKey | 0x100;
-} // MapVKey
+inline uint MapVKey(uint nVKey) {
+  return nVKey | 0x100;
+}
 
 const char16* GetKeyName(uint);
 
-
 #define DEFCOMMAND(mp_name) \
-    void mp_name(const Context* pCtx)
+  void mp_name(const Context* pCtx)
 
-} //Command
+}  // namespace Command
 
 #endif //!defined(INCLUDE_listener_winapp_command_processor_h)

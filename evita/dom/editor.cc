fa0b2b0bf0d68c8813f98d26950d201f7f6e2e5d
Eval <eval1749@gmail.com>
2013-12-29 22:59:02 +0900

    [evita] Introduce |editor.createFrame()| for debugging DOM implementation.

--- evita/dom/editor.cc
+++ evita/dom/editor.cc
@@ -4,6 +4,8 @@
 #include "evita/dom/editor.h"
 
 #include "evita/cm_CmdProc.h"
+#include "evita/gc/local.h"
+#include "evita/dom/editor_window.h"
 #include "evita/dom/lock.h"
 #include "evita/dom/script_command.h"
 #include "evita/dom/script_thread.h"
@@ -19,6 +21,16 @@ void AddKeyBinding(int key_code, v8::Handle<v8::Object> command) {
   ASSERT_DOM_LOCKED();
   Command::g_pGlobalBinds->Bind(key_code, new ScriptCommand(command));
 }
+
+// TODO(yosi): Until we enable |new EditorWindow()|, we use
+// |editor.createFrame()|.
+v8::Handle<v8::Object> CreateFrame() {
+  ASSERT_CALLED_ON_SCRIPT_THREAD();
+  ASSERT_DOM_LOCKED();
+  auto window = gc::MakeLocal(new EditorWindow());
+  return window->GetWrapper(v8::Isolate::GetCurrent());
+}
+
 }  // namespace
 
 Editor::Editor() {
@@ -37,6 +49,7 @@ gin::ObjectTemplateBuilder Editor::GetObjectTemplateBuilder(
     v8::Isolate* isolate) {
   return GetEmptyObjectTemplateBuilder(isolate)
       .SetMethod("addKeyBinding", AddKeyBinding)
+      .SetMethod("createFrame", CreateFrame)
       .SetProperty("version", &Editor::version);
 }
 

7eb5dc84521ff9ed3d22d069000b4a72907d1d40
Eval <eval1749@gmail.com>
2013-12-29 20:33:42 +0900

    [evita] Introduce v8_glue::ScopedPersistent<T> template class for ease of managing v8::Persistent<T>. This patch is a preparation of introducuing ScriptCommand class which holds Command JavaScript object handle.

---
+++ evita/v8_glue/scoped_persistent.h
@@ -0,0 +1,32 @@
+// Copyright (C) 2013 by Project Vogue.
+// Written by Yoshifumi "VOGUE" INOUE. (yosi@msn.com)
+#if !defined(INCLUDE_evita_v8_glue_scoped_persistent_h)
+#define INCLUDE_evita_v8_glue_scoped_persistent_h
+
+#include "evita/v8_glue/v8.h"
+
+namespace v8_glue {
+
+template<typename ObjectType>
+class ScopedPersistent {
+  private: v8::Persistent<ObjectType> handle_;
+
+  public: ScopedPersistent(v8::Isolate* isolate,
+                           v8::Handle<ObjectType> handle)
+      : handle_(isolate, handle) {
+  }
+
+  public: ~ScopedPersistent() {
+    handle_.Reset();
+  }
+
+  public: v8::Local<ObjectType> NewLocal(v8::Isolate* isolate) {
+    return v8::Local<ObjectType>::New(isolate, handle_);
+  }
+
+  DISALLOW_COPY_AND_ASSIGN(ScopedPersistent);
+};
+
+}  // namespace v8_glue
+
+#endif //!defined(INCLUDE_evita_v8_glue_scoped_persistent_h)

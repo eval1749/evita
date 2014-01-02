0422e77c7496d994d15386e68902bb266061e83d
Eval <eval1749@gmail.com>
2014-01-02 13:04:46 +0900

    [evita] Introduce |v8_glue::FunctionTemplateBuilder| for creating constructor callback like |gin::ObjectTemplateBuilder|. This patch is a preparation of introducing JavaScript class |Document|.

---
+++ evita/v8_glue/function_template_builder.h
@@ -0,0 +1,131 @@
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#if !defined(INCLUDE_evita_v8_glue_function_template_builder)
+#define INCLUDE_evita_v8_glue_function_template_builder
+
+BEGIN_V8_INCLUDE
+#include "gin/object_template_builder.h"
+END_V8_INCLUDE
+
+namespace v8_glue {
+
+namespace internal {
+
+// Base template - used only for non-member function pointers. Other types
+// either go to one of the below specializations, or go here and fail to compile
+// because of base::Bind().
+template<typename T, typename Enable = void>
+struct CallbackTraits {
+  static v8::Handle<v8::FunctionTemplate> CreateTemplate(
+      v8::Isolate* isolate, v8::Handle<v8::FunctionTemplate> class_template,
+      T callback) {
+    return gin::CreateFunctionTemplate(isolate, base::Bind(callback),
+                                  class_template);
+  }
+};
+
+// Specialization for base::Callback.
+template<typename T>
+struct CallbackTraits<base::Callback<T> > {
+  static v8::Handle<v8::FunctionTemplate> CreateTemplate(
+      v8::Isolate* isolate, v8::Handle<v8::FunctionTemplate> class_template,
+      const base::Callback<T>& callback) {
+    return gin::CreateFunctionTemplate(isolate, callback, class_template);
+  }
+};
+
+// Specialization for member function pointers. We need to handle this case
+// specially because the first parameter for callbacks to MFP should typically
+// come from the the JavaScript "this" object the function was called on, not
+// from the first normal parameter.
+template<typename T>
+struct CallbackTraits<T, typename base::enable_if<
+                           base::is_member_function_pointer<T>::value>::type> {
+  static v8::Handle<v8::FunctionTemplate> CreateTemplate(
+      v8::Isolate* isolate, v8::Handle<v8::FunctionTemplate> class_template,
+      T callback) {
+    return gin::CreateFunctionTemplate(
+        isolate, base::Bind(callback), class_template,
+        gin::HolderIsFirstArgument);
+  }
+};
+
+// This specialization allows people to construct function templates directly if
+// they need to do fancier stuff.
+template<>
+struct CallbackTraits<v8::Handle<v8::FunctionTemplate> > {
+  static v8::Handle<v8::FunctionTemplate> CreateTemplate(
+      v8::Handle<v8::FunctionTemplate> templ) {
+    return templ;
+  }
+};
+
+}  // namespace internal
+
+// FunctionTemplateBuilder provides a handy interface to creating
+// v8::ObjectTemplate instances with various sorts of properties.
+class FunctionTemplateBuilder {
+  public: FunctionTemplateBuilder(v8::Isolate* isolate,
+      v8::Handle<v8::FunctionTemplate> function_tempalte);
+  public: explicit FunctionTemplateBuilder(v8::Isolate* isolate);
+  public: ~FunctionTemplateBuilder();
+
+  // It's against Google C++ style to return a non-const ref, but we take some
+  // poetic license here in order that all calls to Set() can be via the '.'
+  // operator and line up nicely.
+  public: template<typename T>
+  FunctionTemplateBuilder& SetValue(const base::StringPiece& name, T val) {
+    return SetImpl(name, ConvertToV8(isolate_, val));
+  }
+
+  // In the following methods, T and U can be function pointer, member function
+  // pointer, base::Callback, or v8::FunctionTemplate. Most clients will want to
+  // use one of the first two options. Also see gin::CreateFunctionTemplate()
+  // for creating raw function templates.
+  public: template<typename T>
+  FunctionTemplateBuilder& SetMethod(const base::StringPiece& name,
+                                   const T& callback) {
+    return SetImpl(name, internal::CallbackTraits<T>::CreateTemplate(
+        isolate_, class_template_, callback));
+  }
+  public: template<typename T>
+  FunctionTemplateBuilder& SetProperty(const base::StringPiece& name,
+                                     const T& getter) {
+    return SetPropertyImpl(name,
+                           internal::CallbackTraits<T>::CreateTemplate(
+                              isolate_, class_template_, getter),
+                           v8::Local<v8::FunctionTemplate>());
+  }
+  public: template<typename T, typename U>
+  FunctionTemplateBuilder& SetProperty(const base::StringPiece& name,
+                                     const T& getter, const U& setter) {
+    return SetPropertyImpl(
+        name,
+        internal::CallbackTraits<T>::CreateTemplate(isolate_, class_template_,
+                                                    getter),
+        internal::CallbackTraits<U>::CreateTemplate(isolate_, class_template_,
+                                                    setter));
+  }
+
+  public: v8::Local<v8::FunctionTemplate> Build();
+
+  private: FunctionTemplateBuilder& SetImpl(const base::StringPiece& name,
+                                           v8::Handle<v8::Data> val);
+  private: FunctionTemplateBuilder& SetPropertyImpl(
+      const base::StringPiece& name, v8::Handle<v8::FunctionTemplate> getter,
+      v8::Handle<v8::FunctionTemplate> setter);
+
+  private: v8::Isolate* isolate_;
+
+  // Class(FunctionTempale) of object being built.
+  private: v8::Local<v8::FunctionTemplate> class_template_;
+
+  // FunctionTemplateBuilder should only be used on the stack.
+  private: v8::Local<v8::FunctionTemplate> template_;
+};
+
+}  // namespace v8_glue
+
+#endif //!defined(INCLUDE_evita_v8_glue_function_template_builder)

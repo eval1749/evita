b29b943ca06c2312deb1fc2ca71787153f17df59
Eval <eval1749@gmail.com>
2014-01-05 22:16:42 +0900

    [evita] dom: Rename |editor.addKeyBinding()| to |editor.setKeyBinding_()|

--- evita/dom/editor.cc
+++ evita/dom/editor.cc
@@ -16,12 +16,6 @@ namespace dom {
 
 namespace {
 
-void AddKeyBinding(int key_code, v8::Handle<v8::Object> command) {
-  ASSERT_CALLED_ON_SCRIPT_THREAD();
-  ASSERT_DOM_LOCKED();
-  Command::g_pGlobalBinds->Bind(key_code, new ScriptCommand(command));
-}
-
 // TODO(yosi): Until we enable |new EditorWindow()|, we use
 // |editor.createFrame()|.
 v8::Handle<v8::Object> CreateFrame() {
@@ -29,6 +23,12 @@ v8::Handle<v8::Object> CreateFrame() {
   ASSERT_DOM_LOCKED();
   auto window = gc::MakeLocal(new EditorWindow());
   return window->GetWrapper(v8::Isolate::GetCurrent());
+}
+
+void SetKeyBinding(int key_code, v8::Handle<v8::Object> command) {
+  ASSERT_CALLED_ON_SCRIPT_THREAD();
+  ASSERT_DOM_LOCKED();
+  Command::g_pGlobalBinds->Bind(key_code, new ScriptCommand(command));
 }
 
 class EditorWrapperInfo : public v8_glue::WrapperInfo {
@@ -47,8 +47,8 @@ class EditorWrapperInfo : public v8_glue::WrapperInfo {
   private: virtual void SetupInstanceTemplate(
       ObjectTemplateBuilder& builder) override {
     builder
-      .SetMethod("addKeyBinding", AddKeyBinding)
       .SetMethod("createFrame", CreateFrame)
+      .SetMethod("setdKeyBinding_", SetKeyBinding)
       .SetProperty("version", &Editor::version);
   }
 };

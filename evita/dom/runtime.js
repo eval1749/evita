ee8d1cdcc759cab1f6f10a3adb64cd482b476263
Eval <eval1749@gmail.com>
2014-01-10 17:38:00 +0900

    [evita] dom: Introduce |localizeText(format_text, variable_map): string|

--- evita/dom/runtime.js
+++ evita/dom/runtime.js
@@ -1,4 +1,18 @@
 // Copyright (c) 2014 Project Vogue. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-function runtime() { return 1; }
+
+'use strict';
+
+/**
+ * @param {string} format_text
+ * @param {dict}
+ * @return {string}
+ */
+function localizeText(format_text, variable_map) {
+  var text = format_text;
+  Object.keys(variable_map).forEach(function(key) {
+    text = text.replace('__' + key + '__', variable_map[key]);
+  });
+  return text;
+}

756b78588a94702f486c1e968175bcfe2fc59b2b
Eval <eval1749@gmail.com>
2014-01-16 13:01:21 +0900

    [evita] dom: Add JS runtime function externs.

---
+++ evita/dom/runtime_externs.js
@@ -0,0 +1,21 @@
+// Copyright (c) 2014 Project Vogue. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview Runtime functions.
+ * @externs
+ */
+
+/**
+ * @param {string} format_text
+ * @param {Object.<string, string>=} opt_dict
+ * @return {string}
+ */
+var localizeText = function(format_text, opt_dict) {};
+
+/**
+ * @param {string} spec
+ * @return {number} key code.
+*/
+var parseKeyCombination = function(spec) {};

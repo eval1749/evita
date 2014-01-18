7a03e7a2ec9d3b251ef59c574f240a63d0087f77
Eval <eval1749@gmail.com>
2014-01-18 13:03:36 +0900

    [evita] Remove unused function |Array.prototype.find|.

--- evita/dom/polyfill.js
+++ evita/dom/polyfill.js
@@ -3,21 +3,3 @@
 // found in the LICENSE file.
 
 'use strict';
-
-// Ecmascript6: Array.prototype.find
-if (!Array.prototype.find) {
-  /**
-   * @param {function(*) : boolean} callback
-   * @param {*=} opt_thisObject
-   * @return {Object|undefined}
-   */
-  Array.prototype.find = function(callback, opt_thisObject) {
-    var thisObject = opt_thisObject || this;
-    for (var i = 0; i < this.length; ++i) {
-      var element = this[i];
-      if (callback.call(thisObject, element))
-        return element;
-    }
-    return undefined;
-  };
-}

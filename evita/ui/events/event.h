685a13d2e66c81625235a1a7bfa4b6a0936c2b31
Eval <eval1749@gmail.com>
2014-01-23 18:24:57 +0900

    [evita] ui: Introduce |ui::KeyCode| and |ui::Modifier| enum, and |ui::KeyboardEvent::control_key()| and |ui::KeyboardEvent::shift_key()|

--- evita/ui/events/event.h
+++ evita/ui/events/event.h
@@ -17,6 +17,28 @@ enum class EventType {
   MouseWheel,
 };
 
+enum class KeyCode {
+  ArrowDown = VK_DOWN | 0x100,
+  ArrowLeft = VK_LEFT | 0x100,
+  ArrowRight = VK_LEFT | 0x100,
+  ArrowUp = VK_UP | 0x100,
+  Backspace = VK_BACK | 0x100,
+  Delete = VK_DELETE | 0x100,
+  End = VK_END | 0x100,
+  Enter = VK_RETURN | 0x100,
+  Home = VK_HOME | 0x100,
+  Insert = VK_INSERT | 0x100,
+  PageDown  = VK_NEXT | 0x100,
+  PageUp = VK_PRIOR | 0x100,
+  Tab = VK_TAB | 0x100,
+};
+
+enum class Modifier {
+  None = 0,
+  Control = 0x200,
+  Shift = 0x400,
+};
+
 class Event {
   private: EventType event_type_;
 

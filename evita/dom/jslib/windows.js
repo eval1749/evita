08135ade9a90e5a11fc9a130cc939965c40be73f
Eval <eval1749@gmail.com>
2014-01-25 01:01:00 +0900

    [evita] Move |newEditorWindow()| and friends to |jslib/windows.js|.

---
+++ evita/dom/jslib/windows.js
@@ -0,0 +1,43 @@
+// Copyright (C) 2014 by Project Vogue.
+// Written by Yoshifumi "VOGUE" INOUE. (yosi@msn.com)
+
+/**
+ * @const @type{{
+ *  activate: function(!Window, !Document),
+ *  newEditorWindow: function(!Document),
+ *  newTextWindow: function(!Window, !Document)
+ * }}
+ */
+global.windows = {
+  /**
+   * @param {!Window} parent
+   * @param {!Document} document
+   */
+  activate: function(parent, document) {
+    var present = parent.children.find(function(window) {
+      return window instanceof TextWindow && window.document == document;
+    });
+    if (present) {
+      present.focus();
+      return;
+    }
+    windows.newTextWindow(parent, document);
+  },
+
+  /**
+   * @param {!Document} document
+   */
+  newEditorWindow: function(document) {
+    var editor_window = new EditorWindow();
+    windows.newTextWindow(editor_window, document);
+    editor_window.realize();
+  },
+
+  /**
+   * @param {!Window} parent
+   * @param {!Document} document
+   */
+  newTextWindow: function(parent, document) {
+    parent.add(new TextWindow(new Range(document)));
+  }
+};
---
+++ evita/dom/jslib/windows_externs.js
@@ -0,0 +1,11 @@
+// Copyright (C) 2014 by Project Vogue.
+// Written by Yoshifumi "VOGUE" INOUE. (yosi@msn.com)
+
+/**
+ * @const @type{{
+ *  activate: function(!Window, !Document),
+ *  newEditorWindow: function(!Document),
+ *  newTextWindow: function(!Window, !Document)
+ * }}
+ */
+var windows;

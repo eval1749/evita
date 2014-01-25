a1bdd523141c753b20c0dbfc2fc487fbcbe3b819
Eval <eval1749@gmail.com>
2014-01-25 09:34:05 +0900

    Cleanup: Fix JsConsole externs.

--- evita/dom/js_console_externs.js
+++ evita/dom/js_console_externs.js
@@ -2,7 +2,44 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/**
-
 /** @const @type {function(*, number=, number=) : string} */
 JsConsole.stringify;
+
+/** @type {function(Window)} */
+JsConsole.prototype.activate;
+
+/** @type {function()} */
+JsConsole.prototype.backwardHistory;
+
+/** @type {!Document} */
+JsConsole.prototype.document;
+
+/** @type {function(string)} */
+JsConsole.prototype.emit;
+
+/** @type {function()} */
+JsConsole.prototype.emitPrompt;
+
+/** @type {function()} */
+JsConsole.prototype.evalLastLine;
+
+/** @type {function()} */
+JsConsole.prototype.forwardHistory;
+
+/** @type {Array.<string>} */
+JsConsole.prototype.history;
+
+/** @type {number} */
+JsConsole.prototype.history_index;
+
+/** @type {number} */
+JsConsole.prototype.line_number;
+
+/** @type {function()} */
+JsConsole.prototype.newWindow;
+
+/** @type {!Range} */
+JsConsole.prototype.range;
+
+/** @type {function()} */
+JsConsole.prototype.useHistory;

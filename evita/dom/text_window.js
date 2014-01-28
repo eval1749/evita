714c805e574ac9b8d9eca9e6b1e372a4cae67ceb
Eval <eval1749@gmail.com>
2014-01-29 01:56:22 +0900

    [evita] commands: Handle mouse event on |TextWindow| in JavaScript.

--- evita/dom/text_window.js
+++ evita/dom/text_window.js
@@ -5,3 +5,30 @@
 global.TextWindow.prototype.clone = function() {
   return new TextWindow(this.selection.range);
 };
+
+(function() {
+  /** @param {!TextWindow} window */
+  function selectWord(window) {
+    var selection = window.selection;
+    selection.startOf(Unit.WORD);
+    selection.endOf(Unit.WORD, Alter.EXTEND);
+    selection.startIsActive = false;
+  }
+
+  /**
+   * Default event handler.
+   * @this {!TextWindow}
+   * @param {!Event} event.
+   */
+  TextWindow.handleEvent = function(event) {
+    switch (event.type) {
+      case Event.Names.MOUSEDOWN:
+        if (/** @type {MouseEvent} */(event).ctrlKey)
+          selectWord(this);
+        break;
+      case Event.Names.DBLCLICK:
+        selectWord(this);
+        break;
+    }
+  };
+})();

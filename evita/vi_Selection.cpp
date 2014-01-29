e6b0b20f828ae376da57231e89a66f16cde4a1d1
Eval <eval1749@gmail.com>
2014-01-29 21:13:27 +0900

    Cleanup: Fix coding style in "evita/vi_Selection.{cpp,h}"

--- evita/vi_Selection.cpp
+++ evita/vi_Selection.cpp
@@ -1,15 +1,6 @@
 #include "precomp.h"
-//////////////////////////////////////////////////////////////////////////////
-//
-// evcl - listener - winapp - winmain
-// listener/winapp/winmain.cpp
-//
 // Copyright (C) 1996-2007 by Project Vogue.
 // Written by Yoshifumi "VOGUE" INOUE. (yosi@msn.com)
-//
-// @(#)$Id: //proj/evcl3/mainline/listener/winapp/vi_Selection.cpp#4 $
-//
-#define DEBUG_GOAL 0
 #include "evita/vi_Selection.h"
 
 #include "base/strings/string16.h"
@@ -17,143 +8,87 @@
 #include "evita/dom/buffer.h"
 #include "evita/vi_TextEditWindow.h"
 
-//////////////////////////////////////////////////////////////////////
-//
-// Selection ctor
-//
 Selection::Selection(TextEditWindow* pWindow, Buffer* pBuffer) :
     m_fStartIsActive(false),
     m_lRestoreLineNum(0),
     m_pBuffer(pBuffer),
     m_ptGoal(-1, -1),
     m_pWindow(pWindow),
-    Range(pBuffer, Kind_Selection)
-{
+    Range(pBuffer, Kind_Selection) {
     pBuffer->InternalAddRange(this);
-} // Selection::Selection
+}
 
 Selection::Selection(const text::Range& range)
     : Selection(nullptr, static_cast<Buffer*>(range.GetBuffer())) {
   SetRange(&range);
 }
 
-//////////////////////////////////////////////////////////////////////
-//
-// Selection dtor
-//
-Selection::~Selection()
-{
-    #if DEBUG_DESTROY
-        DEBUG_PRINTF("%p\n", this);
-    #endif
-} // Selection::~Selection
+Selection::~Selection() {
+}
 
+void Selection::Blink(Posn lPosn, Count nMillisecond) {
+  bool fReadOnly = GetBuffer()->IsReadOnly();
+  GetBuffer()->SetReadOnly(true);
+  GetWindow()->Blink(lPosn, nMillisecond);
+  GetBuffer()->SetReadOnly(fReadOnly);
+}
 
-//////////////////////////////////////////////////////////////////////
-//
-// Selection::Blink
-//
-void Selection::Blink(Posn lPosn, Count nMillisecond)
-{
-    bool fReadOnly = GetBuffer()->IsReadOnly();
-    GetBuffer()->SetReadOnly(true);
-    GetWindow()->Blink(lPosn, nMillisecond);
-    GetBuffer()->SetReadOnly(fReadOnly);
-} // Selection::Blink
-
-
-//////////////////////////////////////////////////////////////////////
-//
-// Selection::Collapse
-//
-void Selection::Collapse(CollapseWhich eCollapse)
-{
-    forgetGoal();
-    Range::Collapse(eCollapse);
-    m_fStartIsActive = eCollapse == Collapse_Start;
-} // Selection::Collapse
+void Selection::Collapse(CollapseWhich eCollapse) {
+  forgetGoal();
+  Range::Collapse(eCollapse);
+  m_fStartIsActive = eCollapse == Collapse_Start;
+}
 
 Selection* Selection::Create(const text::Range& range) {
   return new(range.GetBuffer()->GetHeap()) Selection(range);
 }
 
-//////////////////////////////////////////////////////////////////////
-//
-// Selection::forgetGoal
-//
-void Selection::forgetGoal()
-{
+void Selection::forgetGoal() {
     m_ptGoal.x = m_ptGoal.y = -1;
-} // Selection::forgetGoal
+}
 
+SelectionType Selection::GetType() const {
+  return GetStart() == GetEnd() ? Selection_None : Selection_Normal;
+}
 
-//////////////////////////////////////////////////////////////////////
-//
-// Selection::GetType
-//
-SelectionType Selection::GetType() const
-{
-    return GetStart() == GetEnd() ?
-        Selection_None :
-        Selection_Normal;
-} // Selection::GetType
+Count Selection::moveAux(Unit eUnit, Count n, bool fExtend) {
+  Posn lActive;
+  if (fExtend) {
+      // Extend from active position.
+      lActive = GetActivePosn();
+  } else if (GetEnd() == GetStart()) {
+      // Move insertion position.
+      lActive = GetStart();
+  } else {
+    // Move from specified direction end.
+    lActive = n > 0 ? GetEnd() : GetStart();
 
-//////////////////////////////////////////////////////////////////////
-//
-// Selection::moveAux
-//
-Count Selection::moveAux(Unit eUnit, Count n, bool fExtend)
-{
-    Posn lActive;
-    if (fExtend)
-    {
-        // Extend from active position.
-        lActive = GetActivePosn();
+    // Note: We make selection collapsed instead of moving end.
+    if (eUnit == Unit_Char) {
+        if (!n)
+          return 0;
+        MoveTo(lActive, false);
+        // Note: We count one for degenerating selection.
+        return 1;
     }
-    else if (GetEnd() == GetStart())
-    {
-        // Move insertion position.
-        lActive = GetStart();
-    }
-    else
-    {
-        // Move from specified direction end.
-        lActive = n > 0 ? GetEnd() : GetStart();
+  }
 
-        // Note: We make selection collapsed instead of moving end.
-        if (eUnit == Unit_Char)
-        {
-            if (n == 0) return 0;
-            MoveTo(lActive, false);
-            // Note: We count one for degenerating selection.
-            return 1;
-        }
-    } // if
+  auto const k = GetWindow()->ComputeMotion(eUnit, n, m_ptGoal, &lActive);
+  MoveTo(lActive, fExtend);
+  return k;
+}
 
-    Count k = GetWindow()->ComputeMotion(eUnit, n, m_ptGoal, &lActive);
-    MoveTo(lActive, fExtend);
-    return k;
-} // Selection::moveAux
-
-
-//////////////////////////////////////////////////////////////////////
-//
-// Selection::MoveDown
 //  Unit        | Key Combination
 //  ------------+----------------
 //  Line        | Down
 //  Paragraph   | Ctrl+Down
 /// Screen      | PageDown
 //  Window      | Ctrl+PageDown
-Count Selection::MoveDown(Unit eUnit, Count n, bool fExtend)
-{
-    updateGoal();
-    return moveAux(eUnit, n, fExtend);
-} // Selection::MoveDown
+Count Selection::MoveDown(Unit eUnit, Count n, bool fExtend) {
+  updateGoal();
+  return moveAux(eUnit, n, fExtend);
+}
 
-//////////////////////////////////////////////////////////////////////
-//
-// Selection::MoveRight
 //  Unit        | Key Combination
 //  ------------+----------------
 //  Char        | Right
@@ -161,108 +96,70 @@ Count Selection::MoveDown(Unit eUnit, Count n, bool fExtend)
 //
 // Note: If selection isn't nondegenerate and fExtend is false, selection
 // becomes degenerate.
-Count Selection::MoveRight(Unit eUnit, Count n, bool fExtend)
-{
-    forgetGoal();
-    return moveAux(eUnit, n, fExtend);
-} // Selection::MoveRight
+Count Selection::MoveRight(Unit eUnit, Count n, bool fExtend) {
+  forgetGoal();
+  return moveAux(eUnit, n, fExtend);
+}
 
+void Selection::MoveTo(Posn lPosn, bool fExtend) {
+  if (!fExtend) {
+    Range::SetRange(lPosn, lPosn);
+  } else {
+    Range::SetRange(lPosn, m_fStartIsActive ? GetEnd() : GetStart());
+    m_fStartIsActive = lPosn <= GetStart();
+  }
+}
 
-//////////////////////////////////////////////////////////////////////
-//
-// Selection::MoveTo
-//
-// Description:
-//  Sets start and end by specified position.
-//
-//  This method is used for implementing mouse click.
-//
-void Selection::MoveTo(Posn lPosn, bool fExtend)
-{
-    if (! fExtend)
-    {
-        Range::SetRange(lPosn, lPosn);
-    }
-    else
-    {
-        Range::SetRange(lPosn, m_fStartIsActive ? GetEnd() : GetStart());
-        m_fStartIsActive = lPosn <= GetStart();
-    }
-} // Selection::MoveTo
-
-
-//////////////////////////////////////////////////////////////////////
-//
-// Selection::MoveUp
-//
 //  Unit        | Key Combination
 //  ------------+----------------
 //  Line        | Up
 //  Paragraph   | Ctrl+Up
 /// Screen      | PageUp
 //  Window      | Ctrl+PageUp
-Count Selection::MoveUp(Unit eUnit, Count n, bool fExtend)
-{
-    updateGoal();
-    return moveAux(eUnit, -n, fExtend);
-} // Selection::MoveUp
+Count Selection::MoveUp(Unit eUnit, Count n, bool fExtend) {
+  updateGoal();
+  return moveAux(eUnit, -n, fExtend);
+}
 
+void Selection::RestoreForReload() {
+  SetRange(0, 0);
+  Move(Unit_Line, m_lRestoreLineNum);
+}
 
-//////////////////////////////////////////////////////////////////////
-//
-// Selection::RestoreForReload
-//
-void Selection::RestoreForReload()
-{
-    SetRange(0, 0);
-    Move(Unit_Line, m_lRestoreLineNum);
-} // Selection::RestoreForReload
+void Selection::PrepareForReload() {
+  Information oInfo;
+  GetInformation(&oInfo);
+  m_lRestoreLineNum = oInfo.m_lLineNum;
+}
 
+void Selection::SetText(const char16* pwsz) {
+  SetText(pwsz, ::lstrlenW(pwsz));
+}
 
-//////////////////////////////////////////////////////////////////////
-//
-// Selection::PrepareForReload
-//
-void Selection::PrepareForReload()
-{
-    Information oInfo;
-    GetInformation(&oInfo);
-    m_lRestoreLineNum = oInfo.m_lLineNum;
-} // Selection::PrepareForReload
+void Selection::SetText(const char16* pwch, int cwch) {
+  forgetGoal();
+  Range::SetText(base::string16(pwch, static_cast<size_t>(cwch)));
+}
 
+void Selection::SetEnd(Posn p) {
+  forgetGoal();
+  Range::SetEnd(p);
+}
 
-//////////////////////////////////////////////////////////////////////
-//
-// Selection::SetText
-//
-void Selection::SetText(const char16* pwsz)
-{
-    SetText(pwsz, ::lstrlenW(pwsz));
-} // Selection::SetText
+void Selection::SetRange(Posn s, Posn e) {
+  forgetGoal();
+  Range::SetRange(s, e);
+}
 
+void Selection::SetRange(const Range* p) {
+  forgetGoal();
+  Range::SetRange(p);
+}
 
-//////////////////////////////////////////////////////////////////////
-//
-// Selection::SetText
-//
-void Selection::SetText(const char16* pwch, int cwch)
-{
-    forgetGoal();
-    Range::SetText(base::string16(pwch, static_cast<size_t>(cwch)));
-} // Selection::SetText
-
-
-void Selection::SetEnd(Posn p)
-    { forgetGoal(); Range::SetEnd(p); }
-
-void Selection::SetRange(Posn s, Posn e)
-    { forgetGoal(); Range::SetRange(s, e); }
-
-void Selection::SetRange(const Range* p)
-    { forgetGoal(); Range::SetRange(p); }
-
-void Selection::SetStart(Posn p)
-    { forgetGoal(); Range::SetStart(p); }
+void Selection::SetStart(Posn p) {
+  forgetGoal();
+  Range::SetStart(p);
+}
 
 void Selection::SetStartIsActive(bool new_start_is_active) {
   // TODO(yosi) We should remove |forgotGoal()| call, once we implement new
@@ -271,47 +168,24 @@ void Selection::SetStartIsActive(bool new_start_is_active) {
   m_fStartIsActive = new_start_is_active;
 }
 
-//////////////////////////////////////////////////////////////////////
-//
-// Selection::TypeChar
-//
-void Selection::TypeChar(char16 wch, Count k)
-{
-    Buffer* pBuffer = GetBuffer();
-    if (Selection_None == GetType())
-    {
-        pBuffer->Insert(GetStart(), wch, k);
-    }
-    else
-    {
-        text::UndoBlock oUndo(pBuffer, L"Selection.TypeChar");
-        SetText(NULL, 0);
-        pBuffer->Insert(GetStart(), wch, k);
-    } // if
+void Selection::TypeChar(char16 wch, Count k) {
+  auto const pBuffer = GetBuffer();
+  if (Selection_None == GetType()) {
+    pBuffer->Insert(GetStart(), wch, k);
+  } else {
+    text::UndoBlock oUndo(pBuffer, L"Selection.TypeChar");
+    SetText(nullptr, 0);
+    pBuffer->Insert(GetStart(), wch, k);
+  }
+  MoveRight(Unit_Char, k);
+}
 
-    MoveRight(Unit_Char, k);
-} // Selection::TypeChar
+bool Selection::updateGoal() {
+  if (m_ptGoal.x >= 0)
+    return false;
 
-//////////////////////////////////////////////////////////////////////
-//
-// Selection::updateGoal
-//
-bool Selection::updateGoal()
-{
-    if (m_ptGoal.x >= 0)
-    {
-        return false;
-    }
-
-    const auto rect = GetWindow()->MapPosnToPoint(GetActivePosn());
-    if (rect)
-      m_ptGoal = rect.left_top();
-
-    #if DEBUG_GOAL
-        DEBUG_PRINTF("%d %p (%d,%d)\n",
-            ::GetTickCount(), this, static_cast<int>(m_ptGoal.x * 100),
-            static_cast<int>(m_ptGoal.y * 100));
-    #endif // DEBUG_GOAL
-
-    return true;
-} // Selection::updateGoal
+  const auto rect = GetWindow()->MapPosnToPoint(GetActivePosn());
+  if (rect)
+    m_ptGoal = rect.left_top();
+  return true;
+}
--- evita/vi_Selection.h
+++ evita/vi_Selection.h
@@ -1,13 +1,5 @@
-//////////////////////////////////////////////////////////////////////////////
-//
-// evcl - listener - edit buffer
-// listener/winapp/ed_buffer.h
-//
-// Copyright (C) 1996-2007 by Project Vogue.
+// Copyright (C) 1996-2014 by Project Vogue.
 // Written by Yoshifumi "VOGUE" INOUE. (yosi@msn.com)
-//
-// @(#)$Id: //proj/evcl3/mainline/listener/winapp/vi_Selection.h#2 $
-//
 #if !defined(INCLUDE_listener_winapp_visual_selection_h)
 #define INCLUDE_listener_winapp_visual_selection_h
 
@@ -17,12 +9,10 @@
 #include "evita/ed_Style.h"
 #include "evita/gfx_base.h"
 
-enum SelectionType
-{
-    Selection_None,
-    Selection_Normal,
-}; // SelectionType
-
+enum SelectionType {
+  Selection_None,
+  Selection_Normal,
+};
 
 class TextEditWindow;
 
@@ -36,91 +26,90 @@ using Buffer = dom::Buffer;
 //
 // Selection
 //
-//  m_lRestoreLineNum
-//   A line number which selection belongs before reloading. We try to
-//   move selection to this line after reloading.
+// m_lRestoreLineNum
+// A line number which selection belongs before reloading. We try to
+// move selection to this line after reloading.
 //
-class Selection : public text::Range
-{
-    private: typedef text::Range  Range;
+class Selection : public text::Range {
+  private: typedef text::Range Range;
 
-    private: Color              m_crColor;
-    private: Color              m_crBackground;
-    private: bool               m_fStartIsActive;
-    private: Count              m_lRestoreLineNum;  // for reloading
-    private: Buffer*            m_pBuffer;
-    private: gfx::PointF        m_ptGoal; // Goal point of vertical motion
-    private: TextEditWindow*    m_pWindow;
+  private: Color m_crColor;
+  private: Color m_crBackground;
+  private: bool m_fStartIsActive;
+  private: Count m_lRestoreLineNum; // for reloading
+  private: Buffer* m_pBuffer;
+  private: gfx::PointF m_ptGoal; // Goal point of vertical motion
+  private: TextEditWindow* m_pWindow;
 
-    // ctor
-    private : explicit Selection(const text::Range& range);
-    public: Selection(TextEditWindow*, Buffer*);
-    public: ~Selection();
+  private : explicit Selection(const text::Range& range);
+  public: Selection(TextEditWindow*, Buffer*);
+  public: ~Selection();
 
-    public: void set_window(TextEditWindow* window) {
-        m_pWindow = window;
-    }
+  public: void set_window(TextEditWindow* window) {
+    m_pWindow = window;
+  }
 
-    // [B]
-    public: void Blink(Posn, Count);
+  // [B]
+  public: void Blink(Posn, Count);
 
-    // [C]
-    public:  void Collapse(CollapseWhich = Collapse_Start);
-    public: static Selection* Create(const text::Range& range);
+  // [C]
+  public: void Collapse(CollapseWhich = Collapse_Start);
+  public: static Selection* Create(const text::Range& range);
 
-    // [G]
-    public: Posn GetActivePosn() const
-        { return m_fStartIsActive ? GetStart() : GetEnd(); }
+  // [G]
+  public: Posn GetActivePosn() const {
+    return m_fStartIsActive ? GetStart() : GetEnd();
+  }
 
-    public: Color           GetBackground() const { return m_crBackground; }
-    public: Buffer*         GetBuffer()     const { return m_pBuffer; }
-    public: Color           GetColor()      const { return m_crColor; }
-    public: SelectionType   GetType()       const;
-    public: TextEditWindow* GetWindow()     const { return m_pWindow; }
+  public: Color GetBackground() const { return m_crBackground; }
+  public: Buffer* GetBuffer() const { return m_pBuffer; }
+  public: Color GetColor() const { return m_crColor; }
+  public: SelectionType GetType() const;
+  public: TextEditWindow* GetWindow() const { return m_pWindow; }
 
-    // [I]
-    public:    bool IsStartActive() const { return m_fStartIsActive; }
+  // [I]
+  public: bool IsStartActive() const { return m_fStartIsActive; }
 
-    // [M]
-    public:  Count MoveDown (Unit, Count, bool = false);
-    public:  Count MoveRight(Unit, Count, bool = false);
-    public:  void  MoveTo(Posn, bool = false);
-    public:  Count MoveUp(Unit, Count, bool = false);
+  // [M]
+  public: Count MoveDown(Unit, Count, bool = false);
+  public: Count MoveRight(Unit, Count, bool = false);
+  public: void MoveTo(Posn, bool = false);
+  public: Count MoveUp(Unit, Count, bool = false);
 
-    // [P]
-    public: void PrepareForReload();
+  // [P]
+  public: void PrepareForReload();
 
-    // [R]
-    public: void RestoreForReload();
+  // [R]
+  public: void RestoreForReload();
 
-    // [S]
-    public: Color SetBackground(Color cr) { return m_crBackground = cr; }
-    public: Color SetColor(Color cr) { return m_crColor = cr; }
-    public: void  SetEnd(Posn);
-    public: void  SetRange(Posn, Posn);
-    public: void  SetRange(const Range*);
-    public: void  SetText(const char16*);
-    public: void  SetText(const char16*, int);
-    public: void  SetStart(Posn);
-    public: void SetStartIsActive(bool new_start_is_active);
+  // [S]
+  public: Color SetBackground(Color cr) { return m_crBackground = cr; }
+  public: Color SetColor(Color cr) { return m_crColor = cr; }
+  public: void SetEnd(Posn);
+  public: void SetRange(Posn, Posn);
+  public: void SetRange(const Range*);
+  public: void SetText(const char16*);
+  public: void SetText(const char16*, int);
+  public: void SetStart(Posn);
+  public: void SetStartIsActive(bool new_start_is_active);
 
-    // [T]
-    public: void TypeChar(char16, Count = 1);
-    private: void TypeText(const char16*, Count);
+  // [T]
+  public: void TypeChar(char16, Count = 1);
+  private: void TypeText(const char16*, Count);
 
-    ////////////////////////////////////////////////////////////
-    //
-    // Methods for implementation
-    //
+  ////////////////////////////////////////////////////////////
+  //
+  // Methods for implementation
+  //
 
-    // [F]
-    private: void  forgetGoal();
+  // [F]
+  private: void forgetGoal();
 
-    // [M]
-    private: Count moveAux(Unit, Count, bool);
+  // [M]
+  private: Count moveAux(Unit, Count, bool);
 
-    // [U]
-    private: bool updateGoal();
+  // [U]
+  private: bool updateGoal();
 }; // Selection
 
 #endif //!defined(INCLUDE_listener_winapp_visual_selection_h)

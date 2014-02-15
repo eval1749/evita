02c3dd9668605ed849eb932486bb338c4529582d
Eval <eval1749@gmail.com>
2014-02-15 10:49:28 +0900

    Cleanup: Move |LocalCharSink_| to "evita/vi_Page.cpp" from "evita/ed_util.h".

--- evita/vi_Page.cpp
+++ evita/vi_Page.cpp
@@ -21,7 +21,6 @@
 #include <vector>
 
 #include "evita/ed_interval.h"
-#include "evita/ed_util.h"
 #include "evita/gfx_base.h"
 #include "evita/dom/buffer.h"
 #include "evita/vi_Selection.h"
@@ -637,6 +636,88 @@ class EnumCI {
     }
   }
 };
+
+//////////////////////////////////////////////////////////////////////
+//
+// LocalCharSink
+//
+template<int t_N = 80>
+class LocalCharSink_
+{
+    HANDLE  m_hObjHeap;
+    char16* m_p;
+    char16* m_pwch;
+    uint    m_cAlloc;
+    char16  m_rgwch[t_N];
+
+    // CharSink
+    public: LocalCharSink_(HANDLE hHeap) :
+        m_hObjHeap(hHeap),
+        m_pwch(m_rgwch),
+        m_p(m_rgwch),
+        m_cAlloc(lengthof(m_rgwch)) {}
+
+    // [A]
+    public: void Add(char16 wch)
+        { Add(&wch, 1); }
+
+    public: void Add(const char16* pwch, uint cwch)
+    {
+        if (m_p + cwch >= m_pwch + m_cAlloc)
+        {
+            alloc(130, cwch);
+        }
+
+        ::CopyMemory(m_p, pwch, sizeof(char16) * cwch);
+        m_p += cwch;
+    } // Add
+
+    private: void alloc(uint n, uint k)
+    {
+        uint cwch = static_cast<uint>(m_p - m_pwch);
+
+        uint cAlloc = (cwch + k) * n / 100;
+
+        char16* pwch = reinterpret_cast<char16*>(
+            ::HeapAlloc(m_hObjHeap, 0, sizeof(char16) * cAlloc) );
+
+        ::CopyMemory(pwch, m_pwch, sizeof(char16) * cwch);
+
+        if (m_pwch != m_rgwch) ::HeapFree(m_hObjHeap, 0, m_pwch);
+
+        m_cAlloc = cAlloc;
+        m_pwch = pwch;
+        m_p = m_pwch + cwch;
+    } // alloc
+
+    // [G]
+    public: uint GetLength() const
+        { return static_cast<uint>(m_p - m_pwch); }
+
+    public: const char16* GetStart() const
+        { return m_pwch; }
+
+    // [F]
+    public: char16* Fix()
+    {
+        if (m_p == m_pwch) return NULL;
+
+        if (m_pwch == m_rgwch)
+        {
+            alloc(100, 0);
+        } // if
+
+        return m_pwch;
+    } // Fix
+
+    // [R]
+    public: void Reset()
+    {
+        m_cAlloc = lengthof(m_rgwch);
+        m_pwch   = m_rgwch;
+        m_p      = m_pwch;
+    } // Reset
+}; // LocalCharSink_
 
 //////////////////////////////////////////////////////////////////////
 //

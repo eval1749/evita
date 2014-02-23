3fd50091f6c370a68ad8c1c5f4d2a59ef7964ad4
Eval <eval1749@gmail.com>
2014-02-23 22:12:51 +0900

    Cleanup: Remove unused functions:  - Selection::GetBackgroundColor  - Selection::GetColor  - Selection::SetBackgroundColor  - Selection::SetColor

--- evita/vi_Selection.h
+++ evita/vi_Selection.h
@@ -15,17 +15,7 @@ namespace text {
 class Range;
 }
 
-//////////////////////////////////////////////////////////////////////
-//
-// Selection
-//
-// m_lRestoreLineNum
-// A line number which selection belongs before reloading. We try to
-// move selection to this line after reloading.
-//
 class Selection {
-  private: Color m_crColor;
-  private: Color m_crBackground;
   private: bool m_fStartIsActive;
   private: dom::Buffer* m_pBuffer;
   private: text::Range* m_pRange;
@@ -36,18 +26,14 @@ class Selection {
   public: text::Range* range() const { return m_pRange; }
 
   // [G]
-  public: Color GetBackground() const { return m_crBackground; }
   public: dom::Buffer* GetBuffer() const { return m_pBuffer; }
   public: Posn GetEnd() const;
-  public: Color GetColor() const { return m_crColor; }
   public: Posn GetStart() const;
 
   // [I]
   public: bool IsStartActive() const { return m_fStartIsActive; }
 
   // [S]
-  public: Color SetBackground(Color cr) { return m_crBackground = cr; }
-  public: Color SetColor(Color cr) { return m_crColor = cr; }
   public: void SetStartIsActive(bool new_start_is_active);
 
   DISALLOW_COPY_AND_ASSIGN(Selection);

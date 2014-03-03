6038d3c39c0cbc64707a6acd7a22f7b107eb249a
Eval <eval1749@gmail.com>
2014-03-03 12:50:18 +0900

    [evita] dom/forms: Fix compilation error for |Form.prototype.get|.

--- evita/dom/forms/form.js
+++ evita/dom/forms/form.js
@@ -6,7 +6,7 @@
  * @param {number} control_id
  * @return {!FormControl}
  */
-Form.prototype.get = function(control_id) {
+global.Form.prototype.get = function(control_id) {
   var control = this.control(control_id);
   if (!control)
     throw 'No such control ' + control_id + ' in form.';

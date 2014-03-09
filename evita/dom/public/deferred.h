a55b9109adf0225e3d24252c73426c75e1b90ad5
Eval <eval1749@gmail.com>
2014-03-10 02:04:11 +0900

    [evita] domapi: Introduce |domapi::Deferred| for replacement callback for |Promise|. This patch is a preparation of introducing |Editor.checkSpelling|.

---
+++ evita/dom/public/deferred.h
@@ -0,0 +1,19 @@
+// Copyright (c) 2014 Project Vogue. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#if !defined(INCLUDE_evita_dom_public_deferred_h)
+#define INCLUDE_evita_dom_public_deferred_h
+
+#include "base/callback.h"
+
+namespace domapi {
+
+template<typename ResolveType, typename RejectType = int>
+struct Deferred {
+  base::Callback<void(RejectType)> reject;
+  base::Callback<void(ResolveType)> resolve;
+};
+}  // namespace domapi
+
+#endif //!defined(INCLUDE_evita_dom_public_deferred_h)

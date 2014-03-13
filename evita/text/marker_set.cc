ec34a23de418d72881eea40c7c6923735ea971ff
Eval <eval1749@gmail.com>
2014-03-13 18:27:16 +0900

    Revert "[evita] text: Re-factor |text::MarkerSet::InsertMarker|."
    This reverts commit 644bc7207cd6464c04f2bef4ea7e4ba1c936dced.

--- evita/text/marker_set.cc
+++ evita/text/marker_set.cc
@@ -84,131 +84,34 @@ Marker MarkerSet::GetMarkerAt(Posn offset) {
 
 void MarkerSet::InsertMarker(Posn start, Posn end, int type) {
   DCHECK_LT(start, end);
+  RemoveMarker(start, end);
 
-  auto offset = start;
-  while (offset < end) {
-    auto iterator = lower_bound(offset);
-    if (iterator == markers_.end() || (*iterator)->start_ >= end)
-      break;
+  auto const after = lower_bound(end);
+  auto const can_merge_after = after != markers_.end() &&
+      (*after)->type_ == type && (*after)->start_ == end;
+  auto const before = lower_bound(start);
+  auto const can_merge_before = before != markers_.end() &&
+      (*before)->type_ == type && (*before)->end_ == start;
 
-    auto const present = *iterator;
-    if (present->end_ >= end) {
-      if (present->type_ == type) {
-        if (present->start_ > start) {
-          // present: ----nnnn--
-          // insert:  --nnnn----
-          NotifyChange(start, present->start_);
-          present->start_ = start;
-        }
-        return;
-      }
-      if (present->start_ > start) {
-        // present: ----pppp--
-        // insert:  --nnnn----
-        // after:   --nnnn----
-        present->start_ = end;
-        break;
-      }
-      // present: --pppppppp--
-      // insert:  ----nnnn----
-      // after:   --ppnnnnpp--
-      SplitMarker(present, offset, end);
-      break;
-    }
-
-   ++iterator;
-   if (iterator == markers_.end() || (*iterator)->start_ >= end) {
-     // |present| is the last marker.
-     if (present->type_ == type) {
-        if (present->start_ > offset) {
-          NotifyChange(offset, present->start_);
-          present->start_ = offset;
-        }
-        NotifyChange(present->end_, end);
-        present->end_ = end;
-        return;
-     }
-     if (present->start_ >= offset) {
-       NotifyChange(offset, end);
-       present->start_ = offset;
-       present->end_ = end;
-       present->type_ = type;
-       return;
-     }
-     // present: --pppp----
-     // insert:  ----nnnn--
-     // after:   --pp????--
-     present->end_ = offset;
-     break;
-   }
-
-    auto const following = *iterator;
-    if (following->end_ <= end && following->type_ != type) {
-      NotifyChange(following->start_, following->end_);
-      following->type_ = type;
-    }
-
-    if (present->type_ == type) {
-      // present:   ---nnnn-----
-      // following: -------ffff--
-      // insert:    --nnnnnnn----
-      // after:     --nnnnnffff--
-
-      // present:   --nnnnnn------
-      // following: --------ffff--
-      // insert:    ------nnnn----
-      // after:     --nnnnnnffff--
-      if (present->start_ > offset)
-        present->start_ = offset;
-      present->end_ = following->start_;
-      TryMerge(present, following);
-      offset = present->end_;
-      continue;
-    }
-
-    if (present->start_ >= offset) {
-      // present:   ----pppp--------
-      // following: ----------ffff--
-      // insert:    --nnnnnnnnnn??--
-      // after:     --nnnnnnnnnn??--
-      NotifyChange(offset, following->start_);
-      present->start_ = offset;
-      present->end_ = following->start_;
-      present->type_ = type;
-      TryMerge(present, following);
-      offset = present->end_;
-      continue;
-    }
-
-    present->end_ = offset;
-    if (following->type_ == type) {
-      // present:   --pppp--------
-      // insert:    ------nnnn----
-      // following: --------nnnn--
-      // after:     --ppppnnnnnn--
-      if (offset != following->start_) {
-        NotifyChange(offset, following->start_);
-        following->start_ = present->end_;
-      }
-      offset = following->end_;
-    } else if (following->end_ <= end) {
-      NotifyChange(offset, following->end_);
-      following->start_ = offset;
-      offset = following->end_;
-    } else {
-      following->start_ = end;
-      break;
-    }
+  ChangeScope change_scope(&markers_);
+  NotifyChange(start, end);
+  if (can_merge_after && can_merge_before) {
+    (*before)->end_ = (*after)->end_;
+    change_scope.Remove(*after);
+    return;
   }
-
-  if (offset < end) {
-    NotifyChange(offset, end);
-    markers_.insert(new Marker(offset, end, type));
+  if (can_merge_after) {
+    (*after)->start_ = start;
+    return;
   }
+  if (can_merge_before) {
+    (*before)->end_ = end;
+    return;
+  }
+  markers_.insert(new Marker(start, end, type));
 }
 
 void MarkerSet::NotifyChange(Posn start, Posn end) {
-  DCHECK_LT(start, end);
   FOR_EACH_OBSERVER(MarkerSetObserver, observers_,
       DidChangeMarker(start, end));
 }
@@ -280,40 +183,6 @@ void MarkerSet::RemoveMarker(Posn start, Posn end) {
 
 void MarkerSet::RemoveObserver(MarkerSetObserver* observer) {
   observers_.RemoveObserver(observer);
-}
-
-// Split specified marker into atmost two markers by excluding range start,
-// end(exclusive).
-void MarkerSet::SplitMarker(Marker* marker, Posn start, Posn end) {
-  DCHECK_LT(start, end);
-  DCHECK_LE(marker->start_, start);
-  DCHECK_GE(marker->end_, end);
-
-  if (marker->start_ == start && marker->end_ == end) {
-    markers_.erase(marker);
-    delete marker;
-    return;
-  }
-
-  if (marker->start_ == start) {
-    marker->start_ = end;
-    return;
-  }
-
-  auto const marker_end = marker->end_;
-  marker->end_ = start;
-  if (end < marker_end)
-    markers_.insert(new Marker(end, marker_end, marker->type_));
-}
-
-void MarkerSet::TryMerge(Marker* marker, Marker* following) {
-  if (!following || marker->type_ != following->type_ ||
-      marker->end_ != following->start_) {
-    return;
-  }
-  markers_.erase(following);
-  marker->end_ = following->end_;
-  delete following;
 }
 
 // BufferMutationObserver

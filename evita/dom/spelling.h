62982abf67a64eb0c37b353766bdee0b5d5435bf
Eval <eval1749@gmail.com>
2014-03-14 10:25:07 +0900

    Build: Add missing commit "evita/dom/spelling.h"

---
+++ evita/dom/spelling.h
@@ -0,0 +1,19 @@
+// Copyright (c) 1996-2014 Project Vogue. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#if !defined(INCLUDE_evita_dom_spelling_h)
+#define INCLUDE_evita_dom_spelling_h
+
+namespace dom {
+
+enum class Spelling {
+  None,
+  Corrected,
+  Misspelled,
+  BadGrammar,
+};
+
+}  // namespace dom
+
+#endif // !defined(INCLUDE_evita_dom_spelling_h)

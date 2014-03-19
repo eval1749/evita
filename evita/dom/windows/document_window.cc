1a9c4965113fdd4d6867858bb5167b28990dc7ed
Eval <eval1749@gmail.com>
2014-03-19 18:51:30 +0900

    [evita] dom/windows: Get rid of tab data regitration from |dom::DocumentWindow|.

--- evita/dom/windows/document_window.cc
+++ evita/dom/windows/document_window.cc
@@ -8,8 +8,6 @@
 #include "evita/dom/events/document_event_init.h"
 #include "evita/dom/public/tab_data.h"
 #include "evita/dom/text/document.h"
-#include "evita/dom/script_controller.h"
-#include "evita/dom/view_delegate.h"
 #include "evita/dom/windows/selection.h"
 #include "evita/v8_glue/converter.h"
 #include "evita/v8_glue/wrapper_info.h"
@@ -48,18 +46,6 @@ DEFINE_SCRIPTABLE_OBJECT(DocumentWindow, DocumentWindowClass);
 
 DocumentWindow::DocumentWindow(Selection* selection)
     : selection_(selection) {
-  // TODO(yosi) We should not set |TabData| in |dom::DocumentWindow|
-  // constructor, but befor |dom::Window::Realize| to make sure tab strip
-  // to have tab data.
-  auto const document = selection->document();
-  domapi::TabData tab_data;
-  tab_data.title = document->name();
-  tab_data.tooltip = document->name();
-  tab_data.state = domapi::TabData::State::Loading;
-  // TODO(yosi) We should not use magic value -2 for tab_data.icon.
-  tab_data.icon = -2;
-  ScriptController::instance()->view_delegate()->SetTabData(
-      window_id(), tab_data);
 }
 
 DocumentWindow::~DocumentWindow() {

8bfcf98e405decdc19ca25a3bdc89f5148a3cb46
Eval <eval1749@gmail.com>
2014-04-06 13:30:00 +0900

    [evita] dom/tests: Run V8 micro trasks in |EXPECT_SCRIPT_VALID(script)| to execute |Promise| callbacks. Thsi patch follows up revision "7e5cb80c376a".

--- evita/dom/abstract_dom_test.cc
+++ evita/dom/abstract_dom_test.cc
@@ -164,6 +164,7 @@ bool AbstractDomTest::RunScript(const base::StringPiece& script_text,
     UnhandledException(runner_.get(), try_catch);
     return false;
   }
+  v8::V8::RunMicrotasks(isolate);
   return true;
 }
 

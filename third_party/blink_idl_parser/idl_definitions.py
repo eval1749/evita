6bb0813e5a1e3775795b62d4dd1ab9150bf93286
Eval <eval1749@gmail.com>
2014-04-23 22:48:30 +0900

    [blink_idl_parser] Make |IdlDictinaryMember.default_value| to have numberic value and null value.

--- third_party/blink_idl_parser/idl_definitions.py
+++ third_party/blink_idl_parser/idl_definitions.py
@@ -228,7 +228,8 @@ class IdlDictionaryMember(object):
             if child_class == 'Type':
                 self.idl_type = type_node_to_type(child)
             elif child_class == 'Default':
-                self.default_value = child.GetProperty('VALUE')
+                self.default_value = \
+                    child.GetProperty('VALUE') or child.GetProperty('NAME')
             elif child_class == 'ExtAttributes':
                 self.extended_attributes = ext_attributes_node_to_extended_attributes(child)
             else:

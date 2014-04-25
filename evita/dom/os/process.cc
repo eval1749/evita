45f81feb8721ea422272c3eb32abf0c6bcd53899
Eval <eval1749@gmail.com>
2014-04-25 15:33:12 +0900

    [evita] dom/os: Introduce "evita/dom/os/Process.idl".

--- evita/dom/os/process.cc
+++ evita/dom/os/process.cc
@@ -4,18 +4,9 @@
 
 #include "evita/dom/os/process.h"
 
-#include "base/memory/ref_counted.h"
-#include "base/strings/string16.h"
-#include "evita/dom/converter.h"
 #include "evita/dom/promise_resolver.h"
 #include "evita/dom/public/io_delegate.h"
 #include "evita/dom/script_host.h"
-#include "evita/v8_glue/converter.h"
-#include "evita/v8_glue/function_template_builder.h"
-#include "evita/v8_glue/optional.h"
-#include "evita/v8_glue/runner.h"
-#include "gin/array_buffer.h"
-#include "v8_strings.h"
 
 namespace gin {
 template<>
@@ -30,63 +21,22 @@ struct Converter<domapi::ProcessId> {
 
 namespace dom {
 
-namespace {
-
-//////////////////////////////////////////////////////////////////////
-//
-// ProcessClass
-//
-class ProcessClass :
-  public v8_glue::DerivedWrapperInfo<Process, AbstractFile> {
-
-  public: ProcessClass(const char* name)
-      : BaseClass(name) {
-  }
-  public: ~ProcessClass() = default;
-
-  private: static Process* NewProcess() {
-    ScriptHost::instance()->ThrowError(
-        "Cannot create an instance of Process.");
-    return nullptr;
-  }
-
-  private: static v8::Handle<v8::Promise> OpenProcess(
-      const base::string16& command_line);
-
-  // v8_glue::WrapperInfo
-  protected: virtual v8::Handle<v8::FunctionTemplate>
-      CreateConstructorTemplate(v8::Isolate* isolate) override {
-    auto const templ = v8_glue::CreateConstructorTemplate(isolate,
-        &ProcessClass::NewProcess);
-  return v8_glue::FunctionTemplateBuilder(isolate, templ)
-      .SetMethod("open", &ProcessClass::OpenProcess)
-      .Build();
-  }
-
-  DISALLOW_COPY_AND_ASSIGN(ProcessClass);
-};
-
-v8::Handle<v8::Promise> ProcessClass::OpenProcess(
-    const base::string16& command_line) {
-  return PromiseResolver::FastCall(base::Bind(
-      &domapi::IoDelegate::OpenProcess,
-      base::Unretained(ScriptHost::instance()->io_delegate()),
-      command_line));
-}
-
-}  // namespace
-
 //////////////////////////////////////////////////////////////////////
 //
 // Process
 //
-DEFINE_SCRIPTABLE_OBJECT(Process, ProcessClass);
-
 Process::Process(domapi::IoContextId context_id)
     : ScriptableBase(context_id) {
 }
 
 Process::~Process() {
+}
+
+v8::Handle<v8::Promise> Process::Open(const base::string16& command_line) {
+  return PromiseResolver::FastCall(base::Bind(
+      &domapi::IoDelegate::OpenProcess,
+      base::Unretained(ScriptHost::instance()->io_delegate()),
+      command_line));
 }
 
 }  // namespace dom
--- evita/dom/os/process.h
+++ evita/dom/os/process.h
@@ -5,15 +5,24 @@
 #if !defined(INCLUDE_evita_dom_os_process_h)
 #define INCLUDE_evita_dom_os_process_h
 
+#include "evita/dom/converter.h"
 #include "evita/dom/os/abstract_file.h"
 
 namespace dom {
 
+namespace bindings {
+class ProcessClass;
+}
+
 class Process : public v8_glue::Scriptable<Process, AbstractFile> {
   DECLARE_SCRIPTABLE_OBJECT(Process);
+  friend class bindings::ProcessClass;
 
   public: Process(domapi::IoContextId context_id);
   public: virtual ~Process();
+
+  private: static v8::Handle<v8::Promise> Open(
+      const base::string16& command_line);
 
   DISALLOW_COPY_AND_ASSIGN(Process);
 };

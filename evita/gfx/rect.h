a233137e2b06d1c7ff17a69fac72b0c033b70c96
Eval <eval1749@gmail.com>
2014-05-11 00:27:31 +0900

    [evita] gfx: Introduce |gfx::Point|, |gfx::Rect| and |gfx::Size|.

---
+++ evita/gfx/rect.h
@@ -0,0 +1,16 @@
+// Copyright (c) 1996-2014 Project Vogue. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#if !defined(INCLUDE_evita_gfx_rect_h)
+#define INCLUDE_evita_gfx_rect_h
+
+#include "common/win/rect.h"
+
+namespace gfx {
+using common::win::Point;
+using common::win::Rect;
+using common::win::Size;
+} // namespace gfx
+
+#endif //!defined(INCLUDE_evita_gfx_rect_h)

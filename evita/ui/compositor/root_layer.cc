60853c960c0e3de2743e4ffd40d8536ec7881486
Eval <eval1749@gmail.com>
2014-07-28 01:22:11 +0900

    [evita] ui: Add |ui::RootLayer| for direction composition.

---
+++ evita/ui/compositor/root_layer.cc
@@ -0,0 +1,26 @@
+// Copyright (c) 1996-2014 Project Vogue. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "evita/ui/compositor/root_layer.h"
+
+#include <dcomp.h>
+
+#include "evita/gfx/rect.h"
+#include "evita/ui/compositor/compositor.h"
+#include "evita/ui/widget.h"
+
+namespace ui {
+
+RootLayer::RootLayer(Widget* widget) {
+  COM_VERIFY(Compositor::instance()->device()->
+      CreateTargetForHwnd(widget->AssociatedHwnd(), false,
+                          &composition_target_));
+  composition_target_->SetRoot(visual());
+  SetBounds(gfx::RectF(widget->bounds()));
+}
+
+RootLayer::~RootLayer() {
+}
+
+}  // namespace ui

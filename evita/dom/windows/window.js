19e67acc66424ddb058253311a49cc5e64503f78
Eval <eval1749@gmail.com>
2014-08-30 14:47:43 +0900

    [evita] dom: Tidying up "evita/dom/windows/window.js" and "evita/dom/windows/Window.idl".

--- evita/dom/windows/window.js
+++ evita/dom/windows/window.js
@@ -2,39 +2,44 @@
 // Written by Yoshifumi "VOGUE" INOUE. (yosi@msn.com)
 
 (function() {
+  /** @type {string} */
+  Window.prototype.status_;
+
   /**
    * Default event handler.
    * @this {!Window}
    * @param {!Event} event
    */
-  Window.handleEvent = function(event) {
+  function handleEvent(event) {
     commander.handleEvent(event);
   }
+
+  /**
+   * @this {!Window}
+   * @return {string}
+   */
+  function statusGet() {
+    return this.status;
+  }
+
+  /**
+   * @this {!Window}
+   */
+  function statusSet(newStatus) {
+    if (this.status_ == newStatus)
+      return;
+    this.status_ = newStatus;
+    Editor.messageBox(this, this.status_, MessageBox.ICONINFORMATION);
+  }
+
+  Object.defineProperties(Window.prototype, {
+    clientHeight: {value: 0, writable: true},
+    clientLeft: {value: 0, writable: true},
+    clientTop: {value: 0, writable: true},
+    clientWidth: {value: 0, writable: true},
+    focusTick_: {value: 0, writable: true},
+    handleEvent: {value: handleEvent},
+    status: {get: statusGet, set: statusSet},
+    status_: {value: '', writable: true},
+  });
 })();
-
-[
-  'clientHeight',
-  'clientLeft',
-  'clientTop',
-  'clientWidth',
-  'focusTick_'
-].forEach(function(name) {
-  Object.defineProperty(Window.prototype, name, {value: 0, writable: true});
-});
-
-Object.defineProperty(Window.prototype, 'status', (function() {
-  var status = '';
-  return {
-    /** @return {string} */
-    get: function() {
-      return status;
-    },
-    /** @param {string} new_status */
-    set: function(new_status) {
-      if (status == new_status)
-        return;
-      status = new_status;
-      Editor.messageBox(this, status, MessageBox.ICONINFORMATION);
-    }
-  };
-})());

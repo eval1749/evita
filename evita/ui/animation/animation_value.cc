003d9f844797eaf0a16a343c741b823679b4252f
Eval <eval1749@gmail.com>
2014-09-07 09:06:50 +0900

    [evita] ui/animation: Introduce |AnimationPoint| and |AnimationSize| classes.

--- evita/ui/animation/animation_value.cc
+++ evita/ui/animation/animation_value.cc
@@ -10,6 +10,10 @@
 
 namespace ui {
 
+//////////////////////////////////////////////////////////////////////
+//
+// AnimationFloat
+//
 AnimationFloat::AnimationFloat(base::Time start_time, base::TimeDelta duration,
                                float start_value, float end_value)
     : duration_(duration), end_value_(end_value), start_time_(start_time),
@@ -25,6 +29,53 @@ float AnimationFloat::Compute(base::Time current_time) const {
       delta / duration_.InMillisecondsF(), 1.0));
   DCHECK_GE(factor, 0.0f);
   return start_value_ + (end_value_ - start_value_) * factor;
+}
+
+//////////////////////////////////////////////////////////////////////
+//
+// AnimationPoint
+//
+AnimationPoint::AnimationPoint(base::Time start_time, base::TimeDelta duration,
+                               const gfx::PointF& start_value,
+                               const gfx::PointF& end_value)
+    : duration_(duration), end_value_(end_value), start_time_(start_time),
+      start_value_(start_value) {
+}
+
+AnimationPoint::~AnimationPoint() {
+}
+
+gfx::PointF AnimationPoint::Compute(base::Time current_time) const {
+  auto const delta = (current_time - start_time_).InMillisecondsF();
+  auto const factor = static_cast<float>(std::min(
+      delta / duration_.InMillisecondsF(), 1.0));
+  DCHECK_GE(factor, 0.0f);
+  return gfx::PointF(start_value_.x + (end_value_.x - start_value_.x) * factor,
+                     start_value_.y + (end_value_.y - start_value_.y) * factor);
+}
+
+//////////////////////////////////////////////////////////////////////
+//
+// AnimationSize
+//
+AnimationSize::AnimationSize(base::Time start_time, base::TimeDelta duration,
+                               const gfx::SizeF& start_value,
+                               const gfx::SizeF& end_value)
+    : duration_(duration), end_value_(end_value), start_time_(start_time),
+      start_value_(start_value) {
+}
+
+AnimationSize::~AnimationSize() {
+}
+
+gfx::SizeF AnimationSize::Compute(base::Time current_time) const {
+  auto const delta = (current_time - start_time_).InMillisecondsF();
+  auto const factor = static_cast<float>(std::min(
+      delta / duration_.InMillisecondsF(), 1.0));
+  DCHECK_GE(factor, 0.0f);
+  return gfx::SizeF(
+    start_value_.width + (end_value_.width - start_value_.width) * factor,
+    start_value_.height + (end_value_.height - start_value_.height) * factor);
 }
 
 }  // namespace ui
--- evita/ui/animation/animation_value.h
+++ evita/ui/animation/animation_value.h
@@ -8,24 +8,76 @@
 #include "base/basictypes.h"
 #include "base/time/time.h"
 #include "common/memory/singleton.h"
+#include "evita/gfx/point_f.h"
+#include "evita/gfx/size_f.h"
 
 #include <unordered_set>
 
 namespace ui {
 
+//////////////////////////////////////////////////////////////////////
+//
+// AnimationFloat
+//
 class AnimationFloat final {
-  private: float end_value_;
+  private: const float end_value_;
   private: base::TimeDelta duration_;
   private: base::Time start_time_;
-  private: float start_value_;
+  private: const float start_value_;
 
   public: AnimationFloat(base::Time start_time, base::TimeDelta duration,
                          float start_value, float end_value);
   public: ~AnimationFloat();
 
+  public: float end_value() const { return end_value_; }
+
   public: float Compute(base::Time current_time) const;
 
   DISALLOW_COPY_AND_ASSIGN(AnimationFloat);
+};
+
+//////////////////////////////////////////////////////////////////////
+//
+// AnimationPoint
+//
+class AnimationPoint {
+  private: const gfx::PointF end_value_;
+  private: base::TimeDelta duration_;
+  private: base::Time start_time_;
+  private: const gfx::PointF start_value_;
+
+  public: AnimationPoint(base::Time start_time, base::TimeDelta duration,
+                         const gfx::PointF& start_value,
+                         const gfx::PointF& end_value);
+  public: ~AnimationPoint();
+
+  public: gfx::PointF end_value() const { return end_value_; }
+
+  public: gfx::PointF Compute(base::Time current_time) const;
+
+  DISALLOW_COPY_AND_ASSIGN(AnimationPoint);
+};
+
+//////////////////////////////////////////////////////////////////////
+//
+// AnimationSize
+//
+class AnimationSize {
+  private: const gfx::SizeF end_value_;
+  private: base::TimeDelta duration_;
+  private: base::Time start_time_;
+  private: const gfx::SizeF start_value_;
+
+  public: AnimationSize(base::Time start_time, base::TimeDelta duration,
+                         const gfx::SizeF& start_value,
+                         const gfx::SizeF& end_value);
+  public: ~AnimationSize();
+
+  public: gfx::SizeF end_value() const { return end_value_; }
+
+  public: gfx::SizeF Compute(base::Time current_time) const;
+
+  DISALLOW_COPY_AND_ASSIGN(AnimationSize);
 };
 
 }   // namespace ui

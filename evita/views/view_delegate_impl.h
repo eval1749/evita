41c94d753745a13b071016a8cbd3fdfcc6f40e5a
Eval <eval1749@gmail.com>
2014-12-18 17:16:01 +0900

    evita: views: use one of |virtual|, |override| or |final| in "evita/views/view_delegate_impl.h"

--- evita/views/view_delegate_impl.h
+++ evita/views/view_delegate_impl.h
@@ -9,85 +9,75 @@
 
 namespace views {
 
-class ViewDelegateImpl : public dom::ViewDelegate {
+class ViewDelegateImpl final : public dom::ViewDelegate {
   private: domapi::ViewEventHandler* event_handler_;
 
   public: ViewDelegateImpl();
-  public: virtual ~ViewDelegateImpl();
+  public: ~ViewDelegateImpl() final;
 
   public: domapi::ViewEventHandler* event_handler() const {
     return event_handler_;
   }
 
   // ViewDelegate
-  private: virtual void AddWindow(dom::WindowId parent_id,
-                                  dom::WindowId child_id) override;
-  private: virtual void ChangeParentWindow(dom::WindowId window_id,
-      dom::WindowId new_parent_id) override;
-  private: virtual text::Posn ComputeOnTextWindow(dom::WindowId window_id,
-      const dom::TextWindowCompute& data) override;
-  private: virtual void CreateEditorWindow(
-      const dom::EditorWindow* window) override;
-  private: virtual void CreateFormWindow(
-      dom::WindowId window_id, dom::Form* form,
-      const domapi::PopupWindowInit& init) override;
-  private: virtual void CreateTableWindow(
-      dom::WindowId window_id, dom::Document* document) override;
-  private: virtual void CreateTextWindow(
-      dom::WindowId window_id, text::Selection* selection) override;
-  private: virtual void DestroyWindow(dom::WindowId window_id) override;
-  private: virtual void DidStartScriptHost(
-      domapi::ScriptHostState state) override;
-  private: virtual void FocusWindow(dom::WindowId window_id) override;
-  private: virtual void GetFileNameForLoad(
-      dom::WindowId window_id, const base::string16& dir_path,
-      const GetFileNameForLoadResolver& resolver) override;
-  private: virtual void GetFileNameForSave(
-      dom::WindowId window_id, const base::string16& dir_path,
-      const GetFileNameForSaveResolver& resolver) override;
-  private: virtual base::string16 GetMetrics(
-      const base::string16& name) override;
-  private: virtual domapi::SwitchValue GetSwitch(
-      const base::string16& name) override;
-  private: virtual std::vector<base::string16> GetSwitchNames() override;
-  private: virtual std::vector<int> GetTableRowStates(
-      dom::WindowId window_id,
-      const std::vector<base::string16>& keys) override;
-  private: virtual void HideWindow(dom::WindowId window_id) override;
-  private: virtual domapi::FloatRect HitTestTextPosition(
-      dom::WindowId window_id, text::Posn position);
-  private: virtual void MakeSelectionVisible(dom::WindowId window_id) override;
-  private: virtual text::Posn MapPointToPosition(
-      domapi::EventTargetId event_target_id, float x, float y) override;
-  private: virtual void MessageBox(dom::WindowId window_id,
+  private: void AddWindow(dom::WindowId parent_id,
+                          dom::WindowId child_id) final;
+  private: void ChangeParentWindow(dom::WindowId window_id,
+                                   dom::WindowId new_parent_id) final;
+  private: text::Posn ComputeOnTextWindow(dom::WindowId window_id,
+      const dom::TextWindowCompute& data) final;
+  private: void CreateEditorWindow(
+      const dom::EditorWindow* window) final;
+  private: void CreateFormWindow(dom::WindowId window_id, dom::Form* form,
+                                 const domapi::PopupWindowInit& init) final;
+  private: void CreateTableWindow(dom::WindowId window_id,
+                                  dom::Document* document) final;
+  private: void CreateTextWindow(dom::WindowId window_id,
+                                 text::Selection* selection) final;
+  private: void DestroyWindow(dom::WindowId window_id) final;
+  private: void DidStartScriptHost(domapi::ScriptHostState state) final;
+  private: void FocusWindow(dom::WindowId window_id) final;
+  private: void GetFileNameForLoad(dom::WindowId window_id,
+                                   const base::string16& dir_path,
+      const GetFileNameForLoadResolver& resolver) final;
+  private: void GetFileNameForSave(dom::WindowId window_id,
+                                   const base::string16& dir_path,
+      const GetFileNameForSaveResolver& resolver) final;
+  private: base::string16 GetMetrics(const base::string16& name) final;
+  private: domapi::SwitchValue GetSwitch(const base::string16& name) final;
+  private: std::vector<base::string16> GetSwitchNames() final;
+  private: std::vector<int> GetTableRowStates(dom::WindowId window_id,
+      const std::vector<base::string16>& keys) final;
+  private: void HideWindow(dom::WindowId window_id) final;
+  private: domapi::FloatRect HitTestTextPosition(dom::WindowId window_id,
+                                                 text::Posn position);
+  private: void MakeSelectionVisible(dom::WindowId window_id) final;
+  private: text::Posn MapPointToPosition(domapi::EventTargetId event_target_id,
+                                         float x, float y) final;
+  private: void MessageBox(dom::WindowId window_id,
       const base::string16& message, const base::string16& title, int flags,
-      const MessageBoxResolver& resolver) override;
-  public: virtual void Reconvert(dom::WindowId window_id,
-                                 const base::string16& text) override;
-  private: virtual void RealizeWindow(dom::WindowId window_id) override;
-  private: virtual void RegisterViewEventHandler(
-      domapi::ViewEventHandler* event_handler) override;
-  private: virtual void ReleaseCapture(
-      domapi::EventTargetId event_target_id) override;
-  private: virtual void ScrollTextWindow(dom::WindowId window_id,
-                                         int direction) override;
-  private: virtual void SetCapture(
-      domapi::EventTargetId event_target_id) override;
-  private: virtual void SetStatusBar(dom::WindowId window_id,
-      const std::vector<base::string16>& texts) override;
-  private: virtual void SetSwitch(
-      const base::string16& name,
-      const domapi::SwitchValue& new_value) override;
-  private: virtual void SetTabData(dom::WindowId window_id,
-                                   const domapi::TabData& tab_data) override;
-  private: virtual void SetTextWindowZoom(dom::WindowId window_id,
-                                         float zoom) override;
-  private: virtual void ShowWindow(dom::WindowId window_id) override;
-  private: virtual void SplitHorizontally(dom::WindowId left_window,
-      dom::WindowId new_right_window) override;
-  private: virtual void SplitVertically(dom::WindowId above_window,
-      dom::WindowId new_below_window) override;
-  private: virtual void UpdateWindow(dom::WindowId window_id) override;
+      const MessageBoxResolver& resolver) final;
+  public: void Reconvert(dom::WindowId window_id,
+                         const base::string16& text) final;
+  private: void RealizeWindow(dom::WindowId window_id) final;
+  private: void RegisterViewEventHandler(
+      domapi::ViewEventHandler* event_handler) final;
+  private: void ReleaseCapture(domapi::EventTargetId event_target_id) final;
+  private: void ScrollTextWindow(dom::WindowId window_id, int direction) final;
+  private: void SetCapture(domapi::EventTargetId event_target_id) final;
+  private: void SetStatusBar(dom::WindowId window_id,
+      const std::vector<base::string16>& texts) final;
+  private: void SetSwitch(const base::string16& name,
+                          const domapi::SwitchValue& new_value) final;
+  private: void SetTabData(dom::WindowId window_id,
+                                   const domapi::TabData& tab_data) final;
+  private: void SetTextWindowZoom(dom::WindowId window_id, float zoom) final;
+  private: void ShowWindow(dom::WindowId window_id) final;
+  private: void SplitHorizontally(dom::WindowId left_window,
+                                  dom::WindowId new_right_window) final;
+  private: void SplitVertically(dom::WindowId above_window,
+                                dom::WindowId new_below_window) final;
+  private: void UpdateWindow(dom::WindowId window_id) final;
 
   DISALLOW_COPY_AND_ASSIGN(ViewDelegateImpl);
 };

0ad0c1d4c7b5f0cec98b437472f67c60cc8ae3c3
eval1749 <eval1749@gmail.com>
2015-04-25 18:51:10 +0900

    build: Make "gn_evita.cmd" to use proper configuration.

--- evita/build/gn_evita.cmd
+++ evita/build/gn_evita.cmd
@@ -12,6 +12,6 @@ echo //
 : v8 should have  -DICU_UTIL_DATA_IMPL=ICU_UTIL_DATA_SHARE
 
 if exist src cd src
-call gn gen %outdir%\Debug --args="is_component_build=true"
-call gn gen %outdir%\Release --args="is_component_build=false is_debug=true"
+call gn gen %outdir%\Debug --args="is_component_build=true is_debug=true"
+call gn gen %outdir%\Release --args="is_component_build=false is_debug=false"
 endlocal

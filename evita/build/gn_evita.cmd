ec010f57019e357caf89a87464ee789f882f14eb
Eval <eval1749@gmail.com>
2015-09-12 15:22:09 +0900

    Print environment variables for GN file generation

--- evita/build/gn_evita.cmd
+++ evita/build/gn_evita.cmd
@@ -8,6 +8,9 @@ echo //
 echo // Current directory is %CD%
 echo // Generating *.ninja into %outdir%
 echo //
+echo // DEPOT_TOOLS_WIN_TOOLCHAIN=%DEPOT_TOOLS_WIN_TOOLCHAIN%
+echo // GYP_MSVS_VERSION=%GYP_MSVS_VERSION%
+echo //
 
 : v8 should have  -DICU_UTIL_DATA_IMPL=ICU_UTIL_DATA_SHARE
 
@@ -15,3 +18,4 @@ if exist src cd src
 call gn gen %outdir%\Debug --args="is_component_build=true is_debug=true is_win_fastlink=true"
 call gn gen %outdir%\Release --args="is_component_build=false is_debug=false is_win_fastlink=true"
 endlocal
+exit /b

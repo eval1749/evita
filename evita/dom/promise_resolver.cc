d38cecfb087be53432644a8993ffd7a022c033cb
Eval <eval1749@gmail.com>
2015-10-17 22:54:12 +0900

    Don't run micro tasks at PromiseResolver::DoResolve() and DoReject().

--- evita/dom/promise_resolver.cc
+++ evita/dom/promise_resolver.cc
@@ -27,8 +27,6 @@ void PromiseResolver::DoReject(v8::Handle<v8::Value> value) {
   auto const isolate = runner_->isolate();
   auto const resolver = resolver_.NewLocal(isolate);
   resolver->Reject(value);
-  // We run micro tasks to process result from |Promise| rather than idle time.
-  ScheduleRunMicrotasks();
 }
 
 void PromiseResolver::DoResolve(v8::Handle<v8::Value> value) {
@@ -36,8 +34,6 @@ void PromiseResolver::DoResolve(v8::Handle<v8::Value> value) {
   auto const isolate = runner_->isolate();
   auto const resolver = resolver_.NewLocal(isolate);
   resolver->Resolve(value);
-  // We run micro tasks to process result from |Promise| rather than idle time.
-  ScheduleRunMicrotasks();
 }
 
 v8::Local<v8::Promise> PromiseResolver::GetPromise(v8::Isolate* isolate) const {

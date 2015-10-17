26499440041b7a2afdfe8bda6223d11e8d943ada
Eval <eval1749@gmail.com>
2015-10-17 22:30:38 +0900

    Make dom::EventTarget::InvokeEventListeners() not to acquire DOM lock.

--- evita/dom/events/event_target.cc
+++ evita/dom/events/event_target.cc
@@ -209,7 +209,7 @@ void EventTarget::InvokeEventListeners(v8_glue::Runner* runner, Event* event) {
         GetCallee(isolate, listener->callback.NewLocal(isolate));
     if (callee.IsEmpty())
       continue;
-    DOM_AUTO_LOCK_SCOPE();
+    ASSERT_DOM_LOCKED();
     runner->Call(callee, GetWrapper(isolate), event->GetWrapper(isolate));
   }
 }

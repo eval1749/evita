a1f6ccb02f05db24cf0a67eba68489a3183ec9db
Eval <eval1749@gmail.com>
2015-12-05 21:35:43 +0900

    Make LexersTest to use lexer class directly rather than getting lexer from document mode.

--- evita/dom/lexers/lexers_unittest.cc
+++ evita/dom/lexers/lexers_unittest.cc
@@ -24,12 +24,11 @@ void LexersTest::SetUp() {
   AbstractDomTest::SetUp();
 
   EXPECT_SCRIPT_VALID(
-      "function doColor(fileName, lines) {"
-      "  var doc = Document.new(fileName);"
+      "function applySyntax(lexerClass, lines) {"
+      "  var doc = Document.new(lexerClass.name);"
       "  var range = new Range(doc);"
       "  range.text = lines.join('\\n');"
-      "  doc.mode = Mode.chooseModeByFileName(fileName);"
-      "  doc.doColor_(doc.length);"
+      "  (new lexerClass(doc)).doColor(doc.length);"
       "  var result = '';"
       "  var lastSyntax = '';"
       "  var count = 0;"
@@ -62,7 +61,7 @@ void LexersTest::SetUp() {
 
 TEST_F(LexersTest, ConfigLexer) {
   EXPECT_SCRIPT_EQ("c5 n23",
-                   "doColor('foo.mk', ["
+                   "applySyntax(ConfigLexer, ["
                    "'# foo',"
                    "'all: object',"
                    "'  echo foo',"
@@ -72,7 +71,7 @@ TEST_F(LexersTest, ConfigLexer) {
 TEST_F(LexersTest, CppLexer) {
   EXPECT_SCRIPT_EQ(
       "c9 n1 c6 n1 k8 n1 o1 n3 o1 n1 k4 n4 o1 n1 s4 o1 n3 k4 n4 o1 n1 s4 o1",
-      "doColor('foo.cc', ["
+      "applySyntax(CppLexer, ["
       "'/* foo */',"
       "'// bar',"
       "'#include <foo>',"
@@ -82,7 +81,7 @@ TEST_F(LexersTest, CppLexer) {
 
   EXPECT_SCRIPT_EQ(
       "n1 k15 o1 n1 o1 n1 c5 n10 o1 n1 o1 n1 c6 n8",
-      "doColor('extended_keywords.cc', ['"
+      "applySyntax(CppLexer, ['"
       " std::unique_ptr<T>"
       " std::vector_ptr<T>"  // not extended keyword
       " base::string16"      // chromium specific == not extended keyword
@@ -91,7 +90,7 @@ TEST_F(LexersTest, CppLexer) {
 
 TEST_F(LexersTest, IdlLexer) {
   EXPECT_SCRIPT_EQ("c9 n1 c6 n1 k9 n4 o1 n1 s4 o1 n3 k9 n4 o1 n1 s4 o1",
-                   "doColor('foo.idl', ["
+                   "applySyntax(IdlLexer, ["
                    "'/* foo */',"
                    "'// bar',"
                    "'attribute a1 = \\'s1\\';',"
@@ -102,7 +101,7 @@ TEST_F(LexersTest, IdlLexer) {
 TEST_F(LexersTest, HtmlLexer) {
   EXPECT_SCRIPT_EQ(
       "k1 e6 k1 n1 k3 n3 o1 n1 s5 o1 n1 k2 e6 k1 n1 k1 e3 k1 n3 k2 e3 k1",
-      "doColor('foo.html', ["
+      "applySyntax(HtmlLexer, ["
       "'<script>',"
       "'var x = \"foo\";',"
       "'</script>',"
@@ -112,15 +111,15 @@ TEST_F(LexersTest, HtmlLexer) {
 
 TEST_F(LexersTest, JavaLexer) {
   EXPECT_SCRIPT_EQ("c9 n1 c6 n1 k5 n4 o1 n1 s4 o1 n3 k10 n4 o1 n1 s4 o1",
-                   "doColor('foo.java', ["
+                   "applySyntax(JavaLexer, ["
                    "'/* foo */',"
                    "'// bar',"
                    "'class a1 = \\'s1\\';',"
                    "'  instanceof a2 = \"s2\";',"
                    "])");
 
-  EXPECT_SCRIPT_EQ("n11 k4 o1 n4 k4 o1 n5 o2 n4 o1 n8 o2 n4 K10",
-                   "doColor('extended_java.js', ['"
+  EXPECT_SCRIPT_EQ("n1 k9 n1 k4 o1 n4 k10 o2 n4 k9 o2 n4 o1 n9",
+                   "applySyntax(JavaLexer, ['"
                    " @Override"
                    " this.foo"
                    " this.clone()"
@@ -131,7 +130,7 @@ TEST_F(LexersTest, JavaLexer) {
 
 TEST_F(LexersTest, JavaScriptLexer) {
   EXPECT_SCRIPT_EQ("c9 n1 c6 n1 k2 n1 o1 n3 o1 n1 s4 o1 n3 k3 n4 o1 n1 s4 o1",
-                   "doColor('foo.js', ["
+                   "applySyntax(JavaScriptLexer, ["
                    "'/* foo */',"
                    "'// bar',"
                    "'if (a1 = \\'s1\\')',"
@@ -139,7 +138,7 @@ TEST_F(LexersTest, JavaScriptLexer) {
                    "])");
 
   EXPECT_SCRIPT_EQ("n1 k8 o1 n1 o1 n1 k4 o1 n4 k4 K7 n4 K7 n4 K10 n4 K9 o2",
-                   "doColor('extended_keywords.js', ['"
+                   "applySyntax(JavaScriptLexer, ['"
                    " Math.sin(1)"
                    " this.foo"
                    " this.length"
@@ -151,7 +150,7 @@ TEST_F(LexersTest, JavaScriptLexer) {
 
 TEST_F(LexersTest, PythonLexer) {
   EXPECT_SCRIPT_EQ("c5 n1 c5 n1 k2 n4 o2 n1 s4 o1 n8 o1 n1 s4",
-                   "doColor('foo.py', ["
+                   "applySyntax(PythonLexer, ["
                    "'# bar',"
                    "'# foo',"
                    "'if a1 == \\'s1\\':',"
@@ -162,36 +161,36 @@ TEST_F(LexersTest, PythonLexer) {
 TEST_F(LexersTest, XmlLexer) {
   EXPECT_SCRIPT_EQ(
       "c16 n1 k1 e7 n1 a5 n2 v3 n2 a5 n2 v3 n1 k1 n4 &5 n4 k2 e7 k1",
-      "doColor('foo.xml', ["
+      "applySyntax(XmlLexer, ["
       "'<!-- comment -->',"
       "'<element attr1=\\'123\\' attr2=\"456\">',"
       "'foo&amp;bar',"
       "'</element>',"
       "])");
 
-  EXPECT_SCRIPT_EQ("k1 e2 k2", "doColor('foo.xml', ['<br/>'])");
+  EXPECT_SCRIPT_EQ("k1 e2 k2", "applySyntax(XmlLexer, ['<br/>'])");
 
   EXPECT_SCRIPT_EQ("k2 e3 n1 a7 n2 v3 n1 k2",
-                   "doColor('foo.xml', ['<?xml version=\"1.0\"?>'])");
+                   "applySyntax(XmlLexer, ['<?xml version=\"1.0\"?>'])");
 
   // attribute variations
   //  <input autofocus maxlength=10 />
   EXPECT_SCRIPT_EQ("k1 e5 n1 a3 n1 a3 n1 v2 k1",
-                   "doColor('foo.xml', ['<input abc xyz=10>'])");
+                   "applySyntax(XmlLexer, ['<input abc xyz=10>'])");
 
   // Ampersand
   EXPECT_SCRIPT_EQ("n5 &5 n1 &6 n1 &8 n4",
-                   "doColor('foo.html', ['& && &amp; &#123; &#xBEEF; foo'])");
+                   "applySyntax(XmlLexer, ['& && &amp; &#123; &#xBEEF; foo'])");
 
   // Malformed:  no space between attributes.
   //  <abc def="123"ghi>
   EXPECT_SCRIPT_EQ("k1 e3 n1 a3 n2 v3 n1 a3 k1",
-                   "doColor('foo.html', ['<abc def=\"123\"ghi>'])");
+                   "applySyntax(XmlLexer, ['<abc def=\"123\"ghi>'])");
 
   // Malformed: no ending quote
   //  <abc def="123>ghi
   EXPECT_SCRIPT_EQ("k1 e3 n1 a3 n2 v3 k1 n3",
-                   "doColor('foo.html', ['<abc def=\"123>ghi'])");
+                   "applySyntax(XmlLexer, ['<abc def=\"123>ghi'])");
 }
 
 }  // namespace dom

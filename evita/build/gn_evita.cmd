dec6fd68a46096970a092cca190d272eb0bae93c
eval1749 <eval1749@gmail.com>
2015-12-17 21:43:22 +0900

    Introduce official build configuration

--- evita/build/gn_evita.cmd
+++ evita/build/gn_evita.cmd
@@ -16,6 +16,7 @@ echo //
 
 if exist src cd src
 call gn gen %outdir%\Debug --args="is_component_build=true is_debug=true is_win_fastlink=true"
-call gn gen %outdir%\Release --args="is_component_build=false is_debug=false is_win_fastlink=true is_official_build=true"
+call gn gen %outdir%\Release --args="is_component_build=false is_debug=false is_win_fastlink=true"
+call gn gen %outdir%\Official --args="is_component_build=false is_debug=false is_win_fastlink=true is_official_build=true"
 endlocal
 exit /b

f57a774c1d7f4b8f1196bf256dff97b79de89ca5
eval1749 <eval1749@gmail.com>
2015-12-24 13:19:44 +0900

    Move |base.OrderedSet| test to JavaScript

--- evita/dom/base/ordered_set_test.cc
+++ evita/dom/base/ordered_set_test.cc
@@ -18,54 +18,7 @@ class OrderedSetTest : public AbstractDomTest {
 };
 
 TEST_F(OrderedSetTest, all) {
-  EXPECT_SCRIPT_VALID(
-      "var set = new base.OrderedSet(function(a, b) { return a < b; });"
-      "for (var data = 0; data < 30; data += 5) {"
-      "  set.add(data);"
-      "}");
-
-  EXPECT_SCRIPT_EQ("6", "set.size");
-  EXPECT_SCRIPT_EQ("null", "set.find(-1)");
-  EXPECT_SCRIPT_EQ("0", "set.find(0).data");
-  EXPECT_SCRIPT_EQ("null", "set.find(1)");
-  EXPECT_SCRIPT_EQ("5", "set.find(5).data");
-  EXPECT_SCRIPT_EQ("25", "set.find(25).data");
-  EXPECT_SCRIPT_EQ("null", "set.find(26)");
-  EXPECT_SCRIPT_EQ("null", "set.find(30)");
-
-  EXPECT_SCRIPT_EQ("0", "set.lowerBound(-1).data");
-  EXPECT_SCRIPT_EQ("0", "set.lowerBound(0).data");
-  EXPECT_SCRIPT_EQ("5", "set.lowerBound(1).data");
-  EXPECT_SCRIPT_EQ("5", "set.lowerBound(5).data");
-  EXPECT_SCRIPT_EQ("25", "set.lowerBound(25).data");
-  EXPECT_SCRIPT_EQ("null", "set.lowerBound(26)");
-  EXPECT_SCRIPT_EQ("null", "set.lowerBound(30)");
-
-  EXPECT_SCRIPT_EQ("0", "set.minimum");
-  EXPECT_SCRIPT_EQ("25", "set.maximum");
-
-  EXPECT_SCRIPT_VALID(
-      "var result = [];"
-      "set.forEach(data => result.push(data));");
-  EXPECT_SCRIPT_EQ("0 5 10 15 20 25", "result.join(' ')");
-
-  EXPECT_SCRIPT_EQ("false", "set.remove(-1);");
-  EXPECT_SCRIPT_EQ("false", "set.remove(1);");
-  EXPECT_SCRIPT_EQ("false", "set.remove(11);");
-  EXPECT_SCRIPT_EQ("false", "set.remove(31);");
-  EXPECT_SCRIPT_EQ("true", "set.remove(10);");
-  EXPECT_SCRIPT_EQ("5", "set.size");
-  EXPECT_SCRIPT_EQ("null", "set.find(10)");
-  EXPECT_SCRIPT_EQ("15", "set.lowerBound(10).data");
-
-  EXPECT_SCRIPT_VALID("set.remove(0);");
-  EXPECT_SCRIPT_EQ("5", "set.minimum");
-
-  EXPECT_SCRIPT_VALID("set.remove(25);");
-  EXPECT_SCRIPT_EQ("20", "set.maximum");
-
-  EXPECT_SCRIPT_VALID("set.clear()");
-  EXPECT_SCRIPT_EQ("0", "set.size");
+  RunFile({"base", "ordered_set_test.js"});
 }
 
 }  // namespace dom
---
+++ evita/dom/base/ordered_set_test.js
@@ -0,0 +1,52 @@
+// Copyright (c) 2015 Project Vogue. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+testing.test('OrderedSet', function(t) {
+  const set = new base.OrderedSet(function(a, b) { return a < b; });
+  for (let data = 0; data < 30; data += 5) {
+    set.add(data);
+  }
+
+  t.expect(set.size).toEqual(6);
+  t.expect(set.find(-1)).toEqual(null);
+  t.expect(set.find(0).data).toEqual(0);
+  t.expect(set.find(1)).toEqual(null);
+  t.expect(set.find(5).data).toEqual(5);
+  t.expect(set.find(25).data).toEqual(25);
+  t.expect(set.find(26)).toEqual(null);
+  t.expect(set.find(30)).toEqual(null);
+
+  t.expect(set.lowerBound(-1).data).toEqual(0);
+  t.expect(set.lowerBound(0).data).toEqual(0);
+  t.expect(set.lowerBound(1).data).toEqual(5);
+  t.expect(set.lowerBound(5).data).toEqual(5);
+  t.expect(set.lowerBound(25).data).toEqual(25);
+  t.expect(set.lowerBound(26)).toEqual(null);
+  t.expect(set.lowerBound(30)).toEqual(null);
+
+  t.expect(set.minimum).toEqual(0);
+  t.expect(set.maximum).toEqual(25);
+
+  const result = [];
+  set.forEach(data => result.push(data));
+  t.expect(result.join(' ')).toEqual('0 5 10 15 20 25');
+
+  t.expect(set.remove(-1)).toEqual(false);
+  t.expect(set.remove(1)).toEqual(false);
+  t.expect(set.remove(11)).toEqual(false);
+  t.expect(set.remove(31)).toEqual(false);
+  t.expect(set.remove(10)).toEqual(true);
+  t.expect(set.size).toEqual(5);
+  t.expect(set.find(10)).toEqual(null);
+  t.expect(set.lowerBound(10).data).toEqual(15);
+
+  set.remove(0);
+  t.expect(set.minimum).toEqual(5);
+
+  set.remove(25);
+  t.expect(set.maximum).toEqual(20);
+
+  set.clear();
+  t.expect(set.size).toEqual(0);
+});

d76043e451986ab689fabad592819584b0e1cbc4
eval1749 <eval1749@gmail.com>
2015-12-31 20:13:41 +0900

    Introduce |visuals::Box::DescendantsOrSelf| for ease of iterating over descendants

---
+++ evita/visuals/model/descendants_or_self.cc
@@ -0,0 +1,64 @@
+// Copyright (c) 2015 Project Vogue. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "evita/visuals/model/descendants_or_self.h"
+
+#include "base/logging.h"
+#include "evita/visuals/model/container_box.h"
+#include "evita/visuals/model/traversal.h"
+
+namespace visuals {
+
+//////////////////////////////////////////////////////////////////////
+//
+// Box::DescendantsOrSelf
+//
+Box::DescendantsOrSelf::DescendantsOrSelf(const Box& box) : box_(&box) {}
+Box::DescendantsOrSelf::~DescendantsOrSelf() {}
+
+Box::DescendantsOrSelf::Iterator Box::DescendantsOrSelf::begin() const {
+  return Iterator(const_cast<Box*>(box_));
+}
+
+Box::DescendantsOrSelf::Iterator Box::DescendantsOrSelf::end() const {
+  return Iterator(nullptr);
+}
+
+//////////////////////////////////////////////////////////////////////
+//
+// Box::DescendantsOrSelf::Iterator
+//
+Box::DescendantsOrSelf::Iterator::Iterator(Box* box) : box_(box) {}
+
+Box::DescendantsOrSelf::Iterator::Iterator(const Iterator& other)
+    : box_(other.box_) {}
+
+Box::DescendantsOrSelf::Iterator::~Iterator() {}
+
+Box* Box::DescendantsOrSelf::Iterator::operator*() const {
+  DCHECK(box_);
+  return box_;
+}
+
+Box* Box::DescendantsOrSelf::Iterator::operator->() const {
+  DCHECK(box_);
+  return box_;
+}
+
+Box::DescendantsOrSelf::Iterator& Box::DescendantsOrSelf::Iterator::
+operator++() {
+  DCHECK(box_);
+  box_ = BoxTraversal::NextOf(*box_);
+  return *this;
+}
+
+bool Box::DescendantsOrSelf::Iterator::operator==(const Iterator& other) const {
+  return box_ == other.box_;
+}
+
+bool Box::DescendantsOrSelf::Iterator::operator!=(const Iterator& other) const {
+  return !operator==(other);
+}
+
+}  // namespace visuals
---
+++ evita/visuals/model/descendants_or_self_test.cc
@@ -0,0 +1,48 @@
+// Copyright (c) 2015 Project Vogue. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <vector>
+
+#include "evita/visuals/model/descendants_or_self.h"
+
+#include "evita/visuals/model/block_box.h"
+#include "evita/visuals/model/box_builder.h"
+#include "evita/visuals/model/text_box.h"
+#include "evita/visuals/model/traversal.h"
+#include "gtest/gtest.h"
+
+namespace visuals {
+
+namespace {
+std::unique_ptr<Box> NewText(const base::string16& text) {
+  return std::move(BoxBuilder::New<TextBox>(text).Finish());
+}
+}  // namespace
+
+TEST(BoxDescendantsOrSelfTest, Basic) {
+  const auto& block = BoxBuilder::New<BlockBox>()
+                          .Append(NewText(L"a"))
+                          .Append(BoxBuilder::New<BlockBox>()
+                                      .Append(NewText(L"b"))
+                                      .Append(NewText(L"c"))
+                                      .Finish())
+                          .Append(NewText(L"d"))
+                          .Finish();
+  std::vector<Box*> visited;
+  for (const auto& runner : Box::DescendantsOrSelf(*block))
+    visited.push_back(runner);
+  EXPECT_EQ(6, visited.size());
+  EXPECT_EQ(block.get(), visited.front());
+  EXPECT_EQ(BoxTraversal::LastChildOf(*block), visited.back());
+}
+
+TEST(BoxDescendantsOrSelfTest, NoChild) {
+  const auto& block = BoxBuilder::New<BlockBox>().Finish();
+  std::vector<Box*> visited;
+  for (const auto& runner : Box::DescendantsOrSelf(*block))
+    visited.push_back(runner);
+  EXPECT_EQ(1, visited.size());
+  EXPECT_EQ(block.get(), visited.front());
+}
+}  // namespace visuals

431bc5d9ad3d9cb36e1ae4a997afab67ca08f610
eval1749 <eval1749@gmail.com>
2016-01-03 02:42:21 +0900

    Introduce |visulas::StyleEdtior| class

---
+++ evita/visuals/css/style_editor.cc
@@ -0,0 +1,43 @@
+// Copyright (c) 2015 Project Vogue. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "evita/visuals/css/style_editor.h"
+
+#include "evita/visuals/css/style.h"
+#include "evita/visuals/css/properties.h"
+#include "evita/visuals/css/values.h"
+
+namespace visuals {
+namespace css {
+
+//////////////////////////////////////////////////////////////////////
+//
+// StyleEditor
+//
+StyleEditor::StyleEditor() {}
+StyleEditor::~StyleEditor() {}
+
+void StyleEditor::SetHeight(Style* style, float height) {
+  SetHeight(style, css::Height(css::Length(height)));
+}
+
+void StyleEditor::SetWidth(Style* style, float width) {
+  SetWidth(style, css::Width(css::Length(width)));
+}
+
+#define V(Name, name, type, text)                        \
+  void StyleEditor::Set##Name(Style* style, type name) { \
+    style->contains_.insert(PropertyId::Name);           \
+    style->name##_ = name;                               \
+  }                                                      \
+                                                         \
+  void StyleEditor::Unset##Name(Style* style) {          \
+    style->contains_.erase(PropertyId::Name);            \
+  }
+
+FOR_EACH_VISUAL_CSS_PROPERTY(V)
+#undef V
+
+}  // namespace css
+}  // namespace visuals
---
+++ evita/visuals/css/style_editor.h
@@ -0,0 +1,46 @@
+// Copyright (c) 2015 Project Vogue. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef EVITA_VISUALS_CSS_STYLE_EDITOR_H_
+#define EVITA_VISUALS_CSS_STYLE_EDITOR_H_
+
+#include <memory>
+
+#include "base/macros.h"
+#include "evita/visuals/css/properties_forward.h"
+#include "evita/visuals/css/values_forward.h"
+
+namespace visuals {
+namespace css {
+
+class Style;
+
+//////////////////////////////////////////////////////////////////////
+//
+// StyleEditor
+//
+class StyleEditor final {
+ public:
+  StyleEditor();
+  ~StyleEditor();
+
+  // Shorthand functions
+  void SetHeight(Style* style, float height);
+  void SetWidth(Style* style, float width);
+
+#define V(Name, name, type, text)          \
+  void Set##Name(Style* style, type name); \
+  void Unset##Name(Style* style);
+
+  FOR_EACH_VISUAL_CSS_PROPERTY(V)
+#undef V
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(StyleEditor);
+};
+
+}  // namespace css
+}  // namespace visuals
+
+#endif  // EVITA_VISUALS_CSS_STYLE_EDITOR_H_

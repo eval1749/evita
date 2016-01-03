0e9a446673508ac534ba5428e979ae839b18ea4c
eval1749 <eval1749@gmail.com>
2016-01-04 02:34:27 +0900

    Introduce |visulas::CompiledStyleSheet|

---
+++ evita/visuals/style/compiled_style_sheet.cc
@@ -0,0 +1,88 @@
+// Copyright (c) 2016 Project Vogue. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "evita/visuals/style/compiled_style_sheet.h"
+
+#include "base/strings/string16.h"
+#include "evita/visuals/css/rule.h"
+#include "evita/visuals/css/style.h"
+#include "evita/visuals/css/style_editor.h"
+#include "evita/visuals/css/style_sheet.h"
+#include "evita/visuals/dom/element.h"
+
+namespace visuals {
+
+//////////////////////////////////////////////////////////////////////
+//
+// StyleSheet
+//
+CompiledStyleSheet::CompiledStyleSheet(const css::StyleSheet& style_sheet)
+    : style_sheet_(style_sheet) {
+  for (const auto& rule : style_sheet_.rules())
+    CompileRule(*rule);
+  style_sheet_.AddObserver(this);
+}
+
+CompiledStyleSheet::~CompiledStyleSheet() {
+  style_sheet_.RemoveObserver(this);
+}
+
+void CompiledStyleSheet::AddObserver(css::StyleSheetObserver* observer) const {
+  observers_.AddObserver(observer);
+}
+
+void CompiledStyleSheet::CompileRule(const css::Rule& rule) {
+  if (rule.selector()[0] == '#') {
+    const auto& id = rule.selector().substr(1);
+    id_map_.emplace(id, &rule.style());
+    return;
+  }
+
+  if (rule.selector()[0] == '.') {
+    const auto& class_name = rule.selector().substr(1);
+    class_name_map_.emplace(class_name, &rule.style());
+    return;
+  }
+  const auto& tag_name = rule.selector();
+  tag_name_map_.emplace(tag_name, &rule.style());
+}
+
+std::unique_ptr<css::Style> CompiledStyleSheet::Match(
+    const Element& element) const {
+  auto matched = false;
+  css::Style style;
+  if (!element.id().empty()) {
+    const auto& id_it = id_map_.find(element.id());
+    if (id_it != id_map_.end()) {
+      matched = true;
+      style = *id_it->second;
+    }
+  }
+  // TODO(eval1749): NYI selector match to element.classList
+  const auto& tag_name_it = tag_name_map_.find(element.id());
+  if (tag_name_it != tag_name_map_.end()) {
+    css::StyleEditor().Merge(&style, *tag_name_it->second);
+    matched = true;
+  }
+  if (!matched)
+    return std::unique_ptr<css::Style>();
+  return std::make_unique<css::Style>(style);
+}
+
+void CompiledStyleSheet::RemoveObserver(
+    css::StyleSheetObserver* observer) const {
+  observers_.RemoveObserver(observer);
+}
+
+// css::StyleSheetObserver
+void CompiledStyleSheet::DidAddRule(const css::Rule& new_rule) {
+  FOR_EACH_OBSERVER(css::StyleSheetObserver, observers_, DidAddRule(new_rule));
+}
+
+void CompiledStyleSheet::DidRemoveRule(const css::Rule& old_rule) {
+  FOR_EACH_OBSERVER(css::StyleSheetObserver, observers_,
+                    DidRemoveRule(old_rule));
+}
+
+}  // namespace visuals
---
+++ evita/visuals/style/compiled_style_sheet.h
@@ -0,0 +1,57 @@
+// Copyright (c) 2016 Project Vogue. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef EVITA_VISUALS_STYLE_COMPILED_STYLE_SHEET_H_
+#define EVITA_VISUALS_STYLE_COMPILED_STYLE_SHEET_H_
+
+#include <memory>
+#include <unordered_map>
+
+#include "base/macros.h"
+#include "base/observer_list.h"
+#include "base/strings/string16.h"
+#include "evita/visuals/css/style_sheet_observer.h"
+
+namespace visuals {
+
+class Element;
+
+namespace css {
+class Style;
+class StyleSheet;
+}
+
+//////////////////////////////////////////////////////////////////////
+//
+// CompiledStyleSheet
+//
+class CompiledStyleSheet final : public css::StyleSheetObserver {
+ public:
+  explicit CompiledStyleSheet(const css::StyleSheet& style_sheet);
+  ~CompiledStyleSheet();
+
+  void AddObserver(css::StyleSheetObserver* observer) const;
+  std::unique_ptr<css::Style> Match(const Element& element) const;
+  void RemoveObserver(css::StyleSheetObserver* observer) const;
+
+ private:
+  void CompileRule(const css::Rule& rule);
+
+  // css::StyleSheetObserver
+  void DidAddRule(const css::Rule& new_rule);
+  void DidRemoveRule(const css::Rule& old_rule);
+
+  mutable base::ObserverList<css::StyleSheetObserver> observers_;
+  const css::StyleSheet& style_sheet_;
+
+  std::unordered_map<base::string16, const css::Style*> class_name_map_;
+  std::unordered_map<base::string16, const css::Style*> id_map_;
+  std::unordered_map<base::string16, const css::Style*> tag_name_map_;
+
+  DISALLOW_COPY_AND_ASSIGN(CompiledStyleSheet);
+};
+
+}  // namespace visuals
+
+#endif  // EVITA_VISUALS_STYLE_COMPILED_STYLE_SHEET_H_

fece35e3eedd4ad45330572b663a8f1236ffe0d9
eval1749 <eval1749@gmail.com>
2016-01-11 18:22:55 +0900

    Set "font-family" and "font-size" CSS property in |BoxFinderText| This patch is a follow-up patch of 045d8eb8f939c7893ee64c49308e71538feea209

--- evita/visuals/layout/box_finder_test.cc
+++ evita/visuals/layout/box_finder_test.cc
@@ -2,6 +2,9 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "evita/visuals/css/style.h"
+#include "evita/visuals/css/style_builder.h"
+#include "evita/visuals/css/values.h"
 #include "evita/visuals/dom/document.h"
 #include "evita/visuals/dom/element.h"
 #include "evita/visuals/dom/node_tree_builder.h"
@@ -18,6 +21,10 @@
 namespace visuals {
 
 TEST(BoxFinderTest, Basic) {
+  const auto& style = css::StyleBuilder()
+                          .SetFontFamily(css::FontFamily(css::String(L"Arial")))
+                          .SetFontSize(css::FontSize(css::Length(10)))
+                          .Build();
   const auto document = NodeTreeBuilder()
                             .Begin(L"block")
                             .AddText(L"foo")
@@ -36,6 +43,8 @@ TEST(BoxFinderTest, Basic) {
   const auto main = root->first_child()->as<ContainerBox>();
   const auto text_box1 = main->first_child();
   const auto text_box2 = main->last_child();
+  BoxEditor().SetStyle(text_box1, *style);
+  BoxEditor().SetStyle(text_box2, *style);
   BoxEditor().SetViewportSize(root.get(), FloatSize(640, 480));
   Layouter().Layout(root.get());
 

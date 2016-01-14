662fd99184d9880d18c3ad2f0945c636a7e208b7
eval1749 <eval1749@gmail.com>
2016-01-14 15:43:23 +0900

    Move code from "node_handle_style.cc" to "node_handle.cc"

--- evita/dom/visuals/node_handle.cc
+++ evita/dom/visuals/node_handle.cc
@@ -5,12 +5,17 @@
 #include "evita/dom/visuals/node_handle.h"
 
 #include "evita/dom/script_host.h"
+#include "evita/dom/visuals/css_style.h"
+#include "evita/v8_glue/runner.h"
+#include "evita/visuals/css/style.h"
 #include "evita/visuals/dom/document.h"
 #include "evita/visuals/dom/element.h"
 #include "evita/visuals/dom/node_editor.h"
 #include "evita/visuals/dom/text.h"
 
 namespace dom {
+
+using Element = visuals::Element;
 
 visuals::ContainerNode* AsContainerNode(NodeHandle* node) {
   const auto container = node->value()->as<visuals::ContainerNode>();
@@ -73,6 +78,21 @@ void NodeHandle::AppendChild(NodeHandle* parent, NodeHandle* node) {
   visuals::NodeEditor().AppendChild(container, node->value());
 }
 
+v8::Local<v8::Map> NodeHandle::GetInlineStyle(NodeHandle* handle) {
+  const auto& runner = ScriptHost::instance()->runner();
+  const auto& context = runner->context();
+  const auto& element = handle->value()->as<Element>();
+  if (!element) {
+    ScriptHost::instance()->ThrowError("Requires element object");
+    return v8::Local<v8::Map>();
+  }
+  v8_glue::Runner::EscapableHandleScope runner_scope(runner);
+  const auto& style = element->inline_style();
+  if (!style)
+    return runner_scope.Escape(v8::Map::New(context->GetIsolate()));
+  return runner_scope.Escape(CSSStyle::ConvertToV8(context, *style));
+}
+
 void NodeHandle::InsertBefore(NodeHandle* parent,
                               NodeHandle* node,
                               NodeHandle* child) {
@@ -97,6 +117,19 @@ void NodeHandle::RemoveClass(NodeHandle* element_handle,
     return;
   }
   visuals::NodeEditor().RemoveClass(element, class_name);
+}
+
+void NodeHandle::SetInlineStyle(NodeHandle* handle,
+                                v8::Local<v8::Map> raw_style) {
+  const auto& runner = ScriptHost::instance()->runner();
+  const auto& context = runner->context();
+  const auto element = handle->value()->as<Element>();
+  if (!element) {
+    ScriptHost::instance()->ThrowError("Requires element object");
+    return;
+  }
+  auto style = CSSStyle::ConvertFromV8(context, raw_style);
+  visuals::NodeEditor().SetInlineStyle(element, *style);
 }
 
 }  // namespace dom

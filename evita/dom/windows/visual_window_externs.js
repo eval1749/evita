dc4cfeb21e4afd1ef77a65ebc047837a2df5477d
eval1749 <eval1749@gmail.com>
2016-01-14 18:03:22 +0900

    Introduce JS externs for |VisualWindow.newWindow()|

---
+++ evita/dom/windows/visual_window_externs.js
@@ -0,0 +1,18 @@
+// Copyright (c) 2016 Project Vogue. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @this {!VisualWindow}
+ * @param {number} point_x
+ * @param {number} point_y
+ * @return {Node}
+ */
+VisualWindow.prototype.hitTest = function (point_x, point_y) {};
+
+/**
+ * @param {!Document} document
+ * @param {!CSSStyleSheet} styleSheet
+ * @return {!VisualWindow}
+ */
+VisualWindow.newWindow = function(document, styleSheet) {};

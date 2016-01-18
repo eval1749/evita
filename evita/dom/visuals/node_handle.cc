1e42bcf813b8a47f986fb4c258c7eb7cc39a5bda
eval1749 <eval1749@gmail.com>
2016-01-19 00:54:44 +0900

    Cleanup "evita/dom/visuals/node_handle.cc"

--- evita/dom/visuals/node_handle.cc
+++ evita/dom/visuals/node_handle.cc
@@ -21,13 +21,33 @@ namespace dom {
 
 using Element = visuals::Element;
 
-visuals::ContainerNode* AsContainerNode(NodeHandle* node) {
-  const auto container = node->value()->as<visuals::ContainerNode>();
+namespace {
+
+visuals::ContainerNode* AsContainerNode(NodeHandle* node_handle) {
+  const auto container = node_handle->value()->as<visuals::ContainerNode>();
   if (container)
     return container;
   ScriptHost::instance()->ThrowError("Requires container node");
   return nullptr;
 }
+
+visuals::Document* AsDocument(NodeHandle* node_handle) {
+  const auto container = node_handle->value()->as<visuals::Document>();
+  if (container)
+    return container;
+  ScriptHost::instance()->ThrowError("Requires document node");
+  return nullptr;
+}
+
+visuals::Element* AsElement(NodeHandle* node_handle) {
+  const auto container = node_handle->value()->as<visuals::Element>();
+  if (container)
+    return container;
+  ScriptHost::instance()->ThrowError("Requires element node");
+  return nullptr;
+}
+
+}  // namespace
 
 //////////////////////////////////////////////////////////////////////
 //
@@ -40,28 +60,27 @@ int NodeHandle::id() const {
   return value_->sequence_id();
 }
 
+// static
 void NodeHandle::AddClass(NodeHandle* element_handle,
                           const base::string16& class_name) {
-  const auto element = element_handle->value()->as<visuals::Element>();
-  if (!element) {
-    ScriptHost::instance()->ThrowError("Requires element node");
+  const auto element = AsElement(element_handle);
+  if (!element)
     return;
-  }
   visuals::NodeEditor().AddClass(element, class_name);
 }
 
+// static
 NodeHandle* NodeHandle::CreateDocument() {
   return new NodeHandle(new visuals::Document());
 }
 
-NodeHandle* NodeHandle::CreateElement(NodeHandle* document_node,
+// static
+NodeHandle* NodeHandle::CreateElement(NodeHandle* document_handle,
                                       const base::string16& tag_name,
                                       const base::string16& id) {
-  const auto document = document_node->value()->as<visuals::Document>();
-  if (!document) {
-    ScriptHost::instance()->ThrowError("Requires document object");
+  const auto document = AsDocument(document_handle);
+  if (!document)
     return nullptr;
-  }
   return new NodeHandle(new visuals::Element(document, tag_name, id));
 }
 
@@ -69,26 +88,24 @@ NodeHandle* NodeHandle::CreateElement(NodeHandle* document_node,
 NodeHandle* NodeHandle::CreateShape(
     NodeHandle* document_handle,
     const gin::ArrayBufferView& array_buffer_view) {
-  const auto document = document_handle->value()->as<visuals::Document>();
-  if (!document) {
-    ScriptHost::instance()->ThrowError("Requires document node");
+  const auto document = AsDocument(document_handle);
+  if (!document)
     return nullptr;
-  }
   const auto& data = visuals::ShapeData(array_buffer_view.bytes(),
                                         array_buffer_view.num_bytes());
   return new NodeHandle(new visuals::Shape(document, data));
 }
 
-NodeHandle* NodeHandle::CreateText(NodeHandle* document_node,
+// static
+NodeHandle* NodeHandle::CreateText(NodeHandle* document_handle,
                                    const base::string16& data) {
-  const auto document = document_node->value()->as<visuals::Document>();
-  if (!document) {
-    ScriptHost::instance()->ThrowError("Requires document node");
+  const auto document = AsDocument(document_handle);
+  if (!document)
     return nullptr;
-  }
   return new NodeHandle(new visuals::Text(document, data));
 }
 
+// static
 void NodeHandle::AppendChild(NodeHandle* parent, NodeHandle* node) {
   const auto container = AsContainerNode(parent);
   if (!container)
@@ -96,14 +113,13 @@ void NodeHandle::AppendChild(NodeHandle* parent, NodeHandle* node) {
   visuals::NodeEditor().AppendChild(container, node->value());
 }
 
-v8::Local<v8::Map> NodeHandle::GetInlineStyle(NodeHandle* handle) {
+// static
+v8::Local<v8::Map> NodeHandle::GetInlineStyle(NodeHandle* element_handle) {
   const auto& runner = ScriptHost::instance()->runner();
   const auto& context = runner->context();
-  const auto& element = handle->value()->as<Element>();
-  if (!element) {
-    ScriptHost::instance()->ThrowError("Requires element object");
+  const auto element = AsElement(element_handle);
+  if (!element)
     return v8::Local<v8::Map>();
-  }
   v8_glue::Runner::EscapableHandleScope runner_scope(runner);
   const auto& style = element->inline_style();
   if (!style)
@@ -111,6 +127,7 @@ v8::Local<v8::Map> NodeHandle::GetInlineStyle(NodeHandle* handle) {
   return runner_scope.Escape(CSSStyle::ConvertToV8(context, *style));
 }
 
+// static
 void NodeHandle::InsertBefore(NodeHandle* parent,
                               NodeHandle* node,
                               NodeHandle* child) {
@@ -120,6 +137,7 @@ void NodeHandle::InsertBefore(NodeHandle* parent,
   visuals::NodeEditor().InsertBefore(container, node->value(), child->value());
 }
 
+// static
 void NodeHandle::RemoveChild(NodeHandle* parent, NodeHandle* child) {
   const auto container = AsContainerNode(parent);
   if (!container)
@@ -127,16 +145,16 @@ void NodeHandle::RemoveChild(NodeHandle* parent, NodeHandle* child) {
   visuals::NodeEditor().RemoveChild(container, child->value());
 }
 
+// static
 void NodeHandle::RemoveClass(NodeHandle* element_handle,
                              const base::string16& class_name) {
-  const auto element = element_handle->value()->as<visuals::Element>();
-  if (!element) {
-    ScriptHost::instance()->ThrowError("Requires element node");
+  const auto element = AsElement(element_handle);
+  if (!element)
     return;
-  }
   visuals::NodeEditor().RemoveClass(element, class_name);
 }
 
+// static
 void NodeHandle::ReplaceChild(NodeHandle* parent,
                               NodeHandle* node,
                               NodeHandle* child) {
@@ -146,15 +164,14 @@ void NodeHandle::ReplaceChild(NodeHandle* parent,
   visuals::NodeEditor().ReplaceChild(container, node->value(), child->value());
 }
 
-void NodeHandle::SetInlineStyle(NodeHandle* handle,
+// static
+void NodeHandle::SetInlineStyle(NodeHandle* element_handle,
                                 v8::Local<v8::Map> raw_style) {
   const auto& runner = ScriptHost::instance()->runner();
   const auto& context = runner->context();
-  const auto element = handle->value()->as<Element>();
-  if (!element) {
-    ScriptHost::instance()->ThrowError("Requires element object");
+  const auto element = AsElement(element_handle);
+  if (!element)
     return;
-  }
   auto style = CSSStyle::ConvertFromV8(context, raw_style);
   visuals::NodeEditor().SetInlineStyle(element, *style);
 }

0a589eafa247d23291d6ee3f4d35f5a2d014ff4e
eval1749 <eval1749@gmail.com>
2016-01-19 00:13:53 +0900

    Make "run_all_test.cmd" to detect "FATAL" error

--- evita/tools/run_all_tests.cmd
+++ evita/tools/run_all_tests.cmd
@@ -22,12 +22,12 @@ for %%x in (%tests%) do (
   set test_exe=%exedir%\evita_%%x_tests.exe
   if not exist !test_exe! goto no_test_exe
 
+  set test_log=%TEMP%\evita_%%x_tests.log
   !test_exe! ^
       --single-process-tests ^
-      --test-launcher-retry-limit=1 >> %test_log% 2>&1
+      --test-launcher-retry-limit=1 > %test_log% 2>&1
+  grep -E -e "PASSED|CRASHED|FAIL|Fatal|FATAL" %test_log%
 )
-
-grep -E -e "CRASHED|FAIL|Fatal" %test_log%
 
 set end=%TIME%
 

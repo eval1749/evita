30da90e9e0571fd5535501811502c4e5217596da
eval1749 <eval1749@gmail.com>
2016-02-05 17:08:35 +0900

    Add missing include directive "base/macros.h" to "scoped_persistent.h"

--- evita/ginx/scoped_persistent.h
+++ evita/ginx/scoped_persistent.h
@@ -1,12 +1,19 @@
-// Copyright (C) 2013 by Project Vogue.
-// Written by Yoshifumi "VOGUE" INOUE. (yosi@msn.com)
+// Copyright (c) 2016 Project Vogue. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 #ifndef EVITA_GINX_SCOPED_PERSISTENT_H_
 #define EVITA_GINX_SCOPED_PERSISTENT_H_
 
+#include "base/macros.h"
 #include "evita/ginx/v8.h"
 
 namespace ginx {
 
+//////////////////////////////////////////////////////////////////////
+//
+// ScopedPersistent
+//
 template <typename ObjectType>
 class ScopedPersistent final {
  public:

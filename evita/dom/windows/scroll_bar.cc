84248d71765f81fc435d9fcd10e553c305955b1a
eval1749 <eval1749@gmail.com>
2016-02-08 02:48:46 +0900

    Introduce |dom::ScrollBar| as replacement of |views::ScrollBar| This patch is a preparation of moving text formatting to script thread.

---
+++ evita/dom/windows/scroll_bar.cc
@@ -0,0 +1,177 @@
+// Copyright (c) 2016 Project Vogue. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <cmath>
+
+#include "evita/dom/windows/scroll_bar.h"
+
+#include "evita/dom/events/mouse_event.h"
+#include "evita/dom/public/scroll_bar_orientation.h"
+#include "evita/dom/public/scroll_bar_part.h"
+#include "evita/dom/public/scroll_bar_state.h"
+#include "evita/dom/public/view_events.h"
+#include "evita/dom/windows/window.h"
+#include "evita/gfx/canvas.h"
+#include "evita/gfx/color_f.h"
+#include "evita/layout/scroll_bar.h"
+#include "evita/ui/controls/scroll_bar_observer.h"
+#include "evita/visuals/display/public/display_item_list.h"
+
+namespace dom {
+
+using DisplayItemList = visuals::DisplayItemList;
+using FloatPoint = domapi::FloatPoint;
+using FloatRect = domapi::FloatRect;
+using FloatSize = domapi::FloatSize;
+using MouseButton = domapi::MouseButton;
+using ScrollBarPart = domapi::ScrollBarPart;
+
+namespace {
+
+gfx::PointF ToPointF(const FloatPoint& point) {
+  return gfx::PointF(point.x(), point.y());
+}
+
+gfx::SizeF ToSizeF(const FloatSize& size) {
+  return gfx::SizeF(size.width(), size.height());
+}
+
+gfx::RectF ToRectF(const FloatRect& rect) {
+  return gfx::RectF(ToPointF(rect.origin()), ToSizeF(rect.size()));
+}
+
+}  // namespace
+
+//////////////////////////////////////////////////////////////////////
+//
+// ScrollBar
+//
+ScrollBar::ScrollBar(ScrollBarOrientation orientation,
+                     Window* window,
+                     ui::ScrollBarObserver* observer)
+    : active_part_(ScrollBarPart::None),
+      hovered_part_(ScrollBarPart::None),
+      layout_(new layout::ScrollBar(orientation)),
+      observer_(observer),
+      window_(window) {}
+
+ScrollBar::~ScrollBar() {}
+
+bool ScrollBar::HandleMouseEvent(const MouseEvent& event) {
+  switch (event.event_type) {
+    case domapi::EventType::MouseDown:
+      return HandleMousePressed(event);
+    case domapi::EventType::MouseMove:
+      return HandleMouseMoved(event);
+    case domapi::EventType::MouseUp:
+      return HandleMouseReleased(event);
+  }
+  return false;
+}
+
+bool ScrollBar::HandleMouseMoved(const MouseEvent& event) {
+  const auto& point = FloatPoint(event.client_x, event.client_y);
+  const auto part = HitTestPoint(point);
+  if (active_part_ != ScrollBarPart::Thumb) {
+    UpdateHoveredPart(part);
+    return part != ScrollBarPart::None;
+  }
+  const auto delta = layout_->IsVertical() ? point.y() - last_drag_point_.y()
+                                           : point.x() - last_drag_point_.x();
+  last_drag_point_ = point;
+  if (delta < 0)
+    observer_->DidClickLineUp();
+  else if (delta > 0)
+    observer_->DidClickLineDown();
+  return true;
+  return true;
+}
+
+bool ScrollBar::HandleMousePressed(const MouseEvent& event) {
+  if (event.button != MouseButton::Left)
+    return false;
+  const auto& point = FloatPoint(event.client_x, event.client_y);
+  const auto part = HitTestPoint(point);
+  if (part == ScrollBarPart::None)
+    return false;
+  static_cast<ViewEventTarget*>(window_)->SetCapture();
+  active_part_ = part;
+  switch (part) {
+    case ScrollBarPart::BackwardButton:
+      layout_->SetState(ScrollBarPart::BackwardButton, ScrollBarState::Pressed);
+      observer_->DidClickLineUp();
+      break;
+    case ScrollBarPart::BackwardTrack:
+      layout_->SetState(ScrollBarPart::Thumb, ScrollBarState::Active);
+      observer_->DidClickPageUp();
+      break;
+    case ScrollBarPart::ForwardButton:
+      layout_->SetState(ScrollBarPart::ForwardButton, ScrollBarState::Pressed);
+      observer_->DidClickLineDown();
+      break;
+    case ScrollBarPart::ForwardTrack:
+      layout_->SetState(ScrollBarPart::Thumb, ScrollBarState::Active);
+      observer_->DidClickPageDown();
+      break;
+    case ScrollBarPart::Thumb:
+      layout_->SetState(ScrollBarPart::Thumb, ScrollBarState::Pressed);
+      last_drag_point_ = point;
+      break;
+  }
+  return true;
+}
+
+bool ScrollBar::HandleMouseReleased(const MouseEvent& event) {
+  if (event.button != MouseButton::Left)
+    return false;
+  if (active_part_ == ScrollBarPart::None)
+    return false;
+  static_cast<ViewEventTarget*>(window_)->ReleaseCapture();
+  layout_->SetState(active_part_, ScrollBarState::Normal);
+  layout_->SetState(ScrollBarPart::Thumb, ScrollBarState::Normal);
+  active_part_ = ScrollBarPart::None;
+  return true;
+}
+
+ScrollBarPart ScrollBar::HitTestPoint(const FloatPoint& point) const {
+  return layout_->HitTestPoint(ToPointF(point));
+}
+
+std::unique_ptr<DisplayItemList> ScrollBar::Paint() const {
+  return layout_->Paint();
+}
+
+void ScrollBar::SetBounds(const FloatRect& new_bounds) {
+  return layout_->SetBounds(ToRectF(new_bounds));
+}
+
+void ScrollBar::SetData(const ScrollBarData& new_data) {
+  if (data_ == new_data)
+    return;
+  data_ = new_data;
+  return layout_->SetData(new_data);
+}
+
+void ScrollBar::SetDisabled(bool new_disabled) {
+  if (disabled_ == new_disabled)
+    return;
+  disabled_ = new_disabled;
+  layout_->SetState(ScrollBarPart::BackwardButton, ScrollBarState::Disabled);
+  layout_->SetState(ScrollBarPart::BackwardTrack, ScrollBarState::Disabled);
+  layout_->SetState(ScrollBarPart::ForwardButton, ScrollBarState::Disabled);
+  layout_->SetState(ScrollBarPart::ForwardTrack, ScrollBarState::Disabled);
+  layout_->SetState(ScrollBarPart::Thumb, ScrollBarState::Disabled);
+}
+
+void ScrollBar::UpdateHoveredPart(ScrollBarPart part) {
+  if (hovered_part_ == part)
+    return;
+  if (hovered_part_ != ScrollBarPart::None)
+    layout_->SetState(hovered_part_, ScrollBarState::Normal);
+  hovered_part_ = part;
+  if (hovered_part_ != ScrollBarPart::None)
+    layout_->SetState(hovered_part_, ScrollBarState::Hovered);
+}
+
+}  // namespace dom
---
+++ evita/dom/windows/scroll_bar.h
@@ -0,0 +1,88 @@
+// Copyright (c) 2016 Project Vogue. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef EVITA_DOM_WINDOWS_SCROLL_BAR_H_
+#define EVITA_DOM_WINDOWS_SCROLL_BAR_H_
+
+#include <memory>
+
+#include "base/macros.h"
+#include "evita/base/float_range.h"
+#include "evita/dom/public/float_point.h"
+#include "evita/dom/public/float_rect.h"
+#include "evita/dom/public/scroll_bar_data.h"
+
+namespace domapi {
+struct MouseEvent;
+enum class ScrollBarOrientation;
+enum class ScrollBarPart;
+enum class ScrollBarState;
+}
+
+namespace layout {
+class ScrollBar;
+}
+
+namespace ui {
+class ScrollBarObserver;
+}
+
+namespace visuals {
+class DisplayItemList;
+}
+
+namespace dom {
+
+class Window;
+
+//////////////////////////////////////////////////////////////////////
+//
+// ScrollBar
+//
+class ScrollBar final {
+  using DisplayItemList = visuals::DisplayItemList;
+  using FloatPoint = domapi::FloatPoint;
+  using FloatRect = domapi::FloatRect;
+  using MouseEvent = domapi::MouseEvent;
+  using ScrollBarData = domapi::ScrollBarData;
+  using ScrollBarOrientation = domapi::ScrollBarOrientation;
+  using ScrollBarPart = domapi::ScrollBarPart;
+  using ScrollBarState = domapi::ScrollBarState;
+
+ public:
+  ScrollBar(ScrollBarOrientation orientation,
+            Window* window,
+            ui::ScrollBarObserver* observer);
+  ~ScrollBar();
+
+  // Returns true if event handled.
+  bool HandleMouseEvent(const MouseEvent& event);
+  ScrollBarPart HitTestPoint(const FloatPoint& point) const;
+  std::unique_ptr<DisplayItemList> Paint() const;
+  void SetBounds(const FloatRect& bounds);
+  void SetData(const ScrollBarData& new_data);
+  void SetDisabled(bool new_disabled);
+
+ private:
+  bool HandleMouseMoved(const MouseEvent& event);
+  bool HandleMousePressed(const MouseEvent& event);
+  bool HandleMouseReleased(const MouseEvent& event);
+  void UpdateHoveredPart(ScrollBarPart part);
+
+  ScrollBarPart active_part_;
+  ScrollBarData data_;
+  ScrollBarPart hovered_part_;
+  bool disabled_ = false;
+  FloatPoint last_drag_point_;
+  std::unique_ptr<layout::ScrollBar> layout_;
+  ui::ScrollBarObserver* const observer_;
+  // |window_| is used for capturing mouse pointer.
+  Window* const window_;
+
+  DISALLOW_COPY_AND_ASSIGN(ScrollBar);
+};
+
+}  // namespace dom
+
+#endif  // EVITA_DOM_WINDOWS_SCROLL_BAR_H_

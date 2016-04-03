de17e016d6931607a3e248d30189c115e34f3e26
eval1749 <eval1749@gmail.com>
2016-04-03 18:31:22 +0900

    Introduce alphabet minimization in DFA builder

--- evita/dom/components/syntax_highlighter/scripts/pattern_dfa_builder_test.py
+++ evita/dom/components/syntax_highlighter/scripts/pattern_dfa_builder_test.py
@@ -24,14 +24,24 @@ class NamedPattern(object):
         return self._source
 
 
+def alphabets_to_string(alphabets):
+    result = []
+    for alphabet in alphabets:
+        if alphabet == 0:
+            continue
+        result.append('%d:[%s]' % (
+            alphabet, char_codes_to_string(alphabets.char_codes_of(alphabet))))
+    return '{%s}' % ', '.join(result)
+
+
 def build(*patterns):
     builder = NfaBuilder()
     for pattern in patterns:
         builder.build(pattern.name, PatternParser(pattern.source).parse())
     dfa_builder = DfaBuilder()
-    nodes = dfa_builder.build(builder.finalize())
+    graph = dfa_builder.build(builder.finalize())
     result = []
-    for node in nodes:
+    for node in graph.nodes:
         node_name = group_name_of(node)
         if len(node.transitions.mapping) == 0:
             result.append('%d:%s' % (node.index, node_name.upper()))
@@ -43,59 +53,30 @@ def build(*patterns):
         result.append('%s%s:(%s)' %
                       (node.index, node_name,
                        transition_map_to_string(node.transitions.mapping)))
-    return ' '.join(result)
+    return (' '.join(result), alphabets_to_string(graph.alphabets))
 
 
-def transition_map_to_string(transition_map):
-    edge_map = dict()
-    for char_code in transition_map.keys():
-        for node in transition_map[char_code]:
-            if node in edge_map:
-                edge_map[node].append(char_code)
-                continue
-            edge_map[node] = [char_code]
+def char_codes_to_string(char_codes):
     result = []
-    for node in sorted(edge_map.keys()):
-        char_set = edge_map[node]
-        if len(char_set) == 128:
-            result.append('.->%d' % node.index)
+    min_code = max_code = -1
+    for char_code in sorted(char_codes):
+        if min_code == -1:
+            min_code = max_code = char_code
             continue
-        complement = ''
-        if 0 in char_set:
-            complement = '^'
-            ranges = char_set_to_string(complement_of(char_set))
-        else:
-            ranges = char_set_to_string(char_set)
-        result.append('[%s%s]->%d' % (complement, ''.join(ranges), node.index))
-    return ', '.join(result)
-
-
-def char_set_to_string(char_set):
-    ranges = []
-    from_char_code = -1
-    to_char_code = -1
-    for char_code in sorted(char_set):
-        if from_char_code == -1:
-            from_char_code = char_code
-            to_char_code = char_code
+        if max_code + 1 == char_code:
+            max_code = char_code
             continue
-        if to_char_code + 1 == char_code:
-            to_char_code = char_code
-            continue
-        if from_char_code == to_char_code:
-            ranges.append(chr(from_char_code))
+        if min_code == max_code:
+            result.append(chr(min_code))
         else:
-            ranges.append('%s-%s' % (chr(from_char_code),
-                                     chr(to_char_code)))
-        from_char_code = char_code
-        to_char_code = char_code
-    if from_char_code != -1:
-        if from_char_code == to_char_code:
-            ranges.append(chr(from_char_code))
+            result.append('%s-%s' % (chr(min_code), chr(max_code)))
+        min_code = max_code = char_code
+    if min_code != -1:
+        if min_code == max_code:
+            result.append(chr(min_code))
         else:
-            ranges.append('%s-%s' % (chr(from_char_code),
-                                     chr(to_char_code)))
-    return ranges
+            result.append('%s-%s' % (chr(min_code), chr(max_code)))
+    return ''.join(result)
 
 
 def complement_of(numbers):
@@ -116,58 +97,100 @@ def group_name_of(node):
     return str(names.pop())
 
 
+def transition_map_to_string(transition_map):
+    edge_map = dict()
+    for char_code in transition_map.keys():
+        for node in transition_map[char_code]:
+            if node in edge_map:
+                edge_map[node].append(char_code)
+                continue
+            edge_map[node] = [char_code]
+    result = []
+    for node in sorted(edge_map.keys()):
+        char_codes = edge_map[node]
+        if len(char_codes) == 128:
+            result.append('.->%d' % node.index)
+            continue
+        if 0 in char_codes:
+            result.append('[^%s]->%d' % (
+                char_codes_to_string(complement_of(char_codes)), node.index))
+            continue
+        result.append('[%s]->%d' % (char_codes_to_string(char_codes),
+                                    node.index))
+    return ', '.join(result)
+
+
 class DfaBuilderTest(unittest.TestCase):
 
+    def test_alphabet(self):
+        # TODO(eval1749): We should merge alphabet[3] and alphabet[5]
+        self.assertEqual(('0:foo:([a]->1) '
+                          '1:foo:([0-9a-z]->2) '
+                          '2:foo:([x]->2, [^0-9a-z]->3, [0-9a-wy-z]->4) '
+                          '3:FOO '
+                          '4:FOO:([x]->2, [^0-9a-z]->3, [0-9a-wy-z]->4)',
+                          '{1:[0-9], 2:[a], 3:[b-w], 4:[x], 5:[y-z]}'),
+                         build(NamedPattern('foo', 'a[a-z0-9]+[^x]')))
+
     def test_basic(self):
-        self.assertEqual('0:name:([a]->1) 1:NAME',
+        self.assertEqual(('0:name:([a]->1) 1:NAME', '{1:[a]}'),
                          build(NamedPattern('name', 'a')))
-        self.assertEqual('0:name:([a]->1) 1:NAME:([a]->1)',
+        self.assertEqual(('0:name:([a]->1) 1:NAME:([a]->1)', '{1:[a]}'),
                          build(NamedPattern('name', 'a+')))
-        self.assertEqual('0:([a]->1, [5]->2) 1:NAME 2:NUMBER',
+        self.assertEqual(('0:([a]->1, [5]->2) 1:NAME 2:NUMBER',
+                          '{1:[5], 2:[a]}'),
                          build(NamedPattern('name', 'a'),
                                NamedPattern('number', '5')))
-        self.assertEqual('0:([a]->1, [9]->2) '
-                         '1:NAME:([a]->1) '
-                         '2:NUMBER:([9]->2)',
+        self.assertEqual(('0:([a]->1, [9]->2) '
+                          '1:NAME:([a]->1) '
+                          '2:NUMBER:([9]->2)',
+                          '{1:[9], 2:[a]}'),
                          build(NamedPattern('name', 'a+'),
                                NamedPattern('number', '9+')))
-        self.assertEqual('0:name:([0-9A-Z_a-z]->1) 1:NAME:([0-9A-Z_a-z]->1)',
+        self.assertEqual(('0:name:([0-9A-Z_a-z]->1) '
+                          '1:NAME:([0-9A-Z_a-z]->1)',
+                          '{1:[0-9], 2:[A-Z], 3:[_], 4:[a-z]}'),
                          build(NamedPattern('name', '\\w+')))
 
     def test_double_quote(self):
         # Double quote string with "/" escape
-        self.assertEqual('0:string:(["]->2) '
-                         '1:string:(.->2) '
-                         '2:string:([/]->1, [^"/]->2, ["]->3) '
-                         '3:STRING',
+        self.assertEqual(('0:string:(["]->2) '
+                          '1:string:(.->2) '
+                          '2:string:([/]->1, [^"/]->2, ["]->3) '
+                          '3:STRING',
+                          '{1:["], 2:[/]}'),
                          build(NamedPattern('string', '"([^/"]|/.)*"')))
 
     def test_block_comment(self):
-        self.assertEqual('0:comment:([/]->1) '
-                         '1:comment:([*]->2) '
-                         '2:comment:([^*]->2, [*]->3) '
-                         '3:comment:([^*/]->2, [*]->3, [/]->4) '
-                         '4:COMMENT',
+        self.assertEqual(('0:comment:([/]->1) '
+                          '1:comment:([*]->2) '
+                          '2:comment:([^*]->2, [*]->3) '
+                          '3:comment:([^*/]->2, [*]->3, [/]->4) '
+                          '4:COMMENT',
+                          '{1:[*], 2:[/]}'),
                          build(NamedPattern('comment',
                                             '/[*][^*]*[*]+([^*/][^*]*[*]+)*/')))
 
     def test_block_comment_greedy(self):
-        self.assertEqual('0:comment:([/]->1) 1:comment:([*]->2) '
-                         '2:comment:([^*]->2, [*]->3) '
-                         '3:comment:([^*/]->2, [*]->3, [/]->4) '
-                         '4:COMMENT:([^*]->2, [*]->3)',
+        self.assertEqual(('0:comment:([/]->1) 1:comment:([*]->2) '
+                          '2:comment:([^*]->2, [*]->3) '
+                          '3:comment:([^*/]->2, [*]->3, [/]->4) '
+                          '4:COMMENT:([^*]->2, [*]->3)',
+                          '{1:[*], 2:[/]}'),
                          build(NamedPattern('comment', '/[*].*[*]/')))
 
     def test_block_comment_lazy(self):
         # TODO(eval1749)-> Can we convert lazy quantifier to DFA?
-        self.assertEqual('0:comment:([/]->1) 1:comment:([*]->2) '
-                         '2:comment:([^*]->2, [*]->3) '
-                         '3:comment:([^*/]->2, [*]->3, [/]->4) '
-                         '4:COMMENT:([^*]->2, [*]->3)',
+        self.assertEqual(('0:comment:([/]->1) 1:comment:([*]->2) '
+                          '2:comment:([^*]->2, [*]->3) '
+                          '3:comment:([^*/]->2, [*]->3, [/]->4) '
+                          '4:COMMENT:([^*]->2, [*]->3)',
+                          '{1:[*], 2:[/]}'),
                          build(NamedPattern('comment', '/[*].*?[*]/')))
 
     def test_repeat_any(self):
-        self.assertEqual('0:name:([^a]->0, [a]->1) 1:NAME:([^a]->0, [a]->1)',
+        self.assertEqual(('0:name:([^a]->0, [a]->1) 1:NAME:([^a]->0, [a]->1)',
+                          '{1:[a]}'),
                          build(NamedPattern('name', '.*a')))
 
 

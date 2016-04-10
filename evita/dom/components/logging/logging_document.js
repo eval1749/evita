d28f3006869bff14c59c575b9b1d3a9a24b020ed
eval1749 <eval1749@gmail.com>
2016-04-10 16:20:40 +0900

    Introduce text document DVLOG output.

---
+++ evita/dom/components/logging/logging_document.js
@@ -0,0 +1,69 @@
+// Copyright (c) 2016 Project Vogue. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+goog.scope(function() {
+
+/**
+ * @param {string} name
+ * @return {!TextDocument}
+ */
+function ensureDocument(name) {
+  const present = TextDocument.find(name);
+  if (present)
+    return present;
+  const document = TextDocument.new(name);
+  SpellChecker.disable(document);
+
+  // Bind "Ctrl+L" to clear document.
+  document.bindKey('Ctrl+L', function() {
+    // TODO(eval1749): We should use |TextDocument.prototype.splice()|.
+    const range = this.selection.range;
+    range.collapseTo(0);
+    range.end = range.document.length;
+    range.text = '';
+  });
+
+  return document;
+}
+
+/** @type {TextRange} */
+let loggingRange = null;
+
+/** @return {!TextRange} */
+function ensureLoggingRange() {
+  if (loggingRange)
+    return loggingRange;
+  const document = ensureDocument('*logging*');
+  loggingRange = new TextRange(document);
+  return loggingRange;
+}
+
+/**
+ * @param {*} thing
+ * @return {string}
+ */
+function safeToString(thing) {
+  if (thing === null)
+    return 'null';
+  if (thing === undefined)
+    return 'undefined';
+  return thing.toString();
+}
+
+// Install DVLOG output handler.
+global.DVLOG.output = function(location, args) {
+  // TODO(eval1749): We should use |TextDocument.prototype.splice()|.
+  const range = ensureLoggingRange();
+  const document = range.document;
+  range.collapseTo(document.length);
+  range.text = `${location.fileName}(${location.lineNumber}):` +
+      ` ${location.functionName}: `;
+  range.collapseTo(document.length);
+  range.text = args.map(arg => safeToString(arg)).join(' ');
+  range.collapseTo(document.length);
+  range.text = '\n';
+  document.clearUndo();
+};
+
+});

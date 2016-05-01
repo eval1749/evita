cfe6c2cc9a3fc8564c6402c18cb1a7778ca47b2f
eval1749 <eval1749@gmail.com>
2016-05-01 19:05:46 +0900

    Utilize |goog.scope()| in |windows.TextDocumentState| to improve type checking

--- evita/dom/windows/text_document_state.js
+++ evita/dom/windows/text_document_state.js
@@ -2,172 +2,186 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-$define(global, 'windows', function($export) {
+goog.scope(function() {
+
+/**
+ * @typedef {function(!TextDocument, !TextDocumentState)}
+ */
+var TextDocumentStateCallback;
+
+/**
+ * @type {!Array<!TextDocumentStateCallback>}
+ */
+const observers = [];
+
+/**
+ * @type {!Map<!TextDocument, !TextDocumentState>}
+ */
+const stateMap = new Map();
+
+//////////////////////////////////////////////////////////////////////
+//
+// TextDocumentState
+//
+class TextDocumentState {
   /**
-   * @type {!Array<!TextDocumentStateCallback>}
+   * @param {!TextDocument} document
    */
-  const observers = [];
-
-  /**
-   * @type {!Map<!TextDocument, !TextDocumentState>}
-   */
-  const stateMap = new Map();
-
-  //////////////////////////////////////////////////////////////////////
-  //
-  // TextDocumentState
-  //
-  class TextDocumentState {
-    /**
-     * @param {!TextDocument} document
-     */
-    constructor(document) {
-      this.document_ = document;
-      this.fileName = document.fileName;
-      // TODO(eval1749): We should specify |TextDocumentState.prototype.icon|
-      // from
-      // IconSet.
-      this.icon = -2;
-      this.lastWriteTime = document.lastWriteTime;
-      this.modified = document.modified;
-      this.name = document.name;
-      this.state = 0;
-    }
-
-    /**
-     * @private
-     * Set initial tab data for |TextWindow|.
-     * @param {!TextDocumentEvent} event
-     */
-    didAttachWindow(event) {
-      if (event.view === null)
-        return;
-      TabData.update(event.view, this);
-    }
-
-    /**
-     * @private
-     * @param {!TextDocumentEvent} event
-     */
-    didLoadSaveTextDocument(event) {
-      const document = this.document_;
-      this.fileName = document.fileName;
-      this.lastWriteTime = document.lastWriteTime;
-      this.modified = document.modified;
-      this.state = 0;
-      this.notifyChanged();
-    }
-
-    /**
-     * @private
-     * @param {!Array.<!TextMutationRecord>} mutations
-     * @param {!TextMutationObserver} observer
-     */
-    mutationCallback(mutations, observer) {
-      if (this.modified === this.document_.modified)
-        return;
-      this.modified = this.document_.modified;
-      this.notifyChanged();
-    }
-
-    /**
-     * @private
-     */
-    notifyChanged() {
-      observers.forEach(observer => observer(this.document_, this));
-    }
-
-    /**
-     * @private
-     */
-    start() {
-      const document = this.document_;
-      document.addEventListener(
-          Event.Names.ATTACH, this.didAttachWindow.bind(this));
-      document.addEventListener(
-          Event.Names.BEFORELOAD, this.willLoadTextDocument.bind(this));
-      document.addEventListener(
-          Event.Names.LOAD, this.didLoadSaveTextDocument.bind(this));
-      document.addEventListener(
-          Event.Names.SAVE, this.didLoadSaveTextDocument.bind(this));
-      const mutationObserver =
-          new TextMutationObserver(this.mutationCallback.bind(this));
-      mutationObserver.observe(document, {summary: true});
-      this.notifyChanged();
-    }
-
-    /**
-     * @private
-     * @param {!TextDocumentEvent} event
-     */
-    willLoadTextDocument(event) {
-      this.state = 1;
-      this.notifyChanged();
-    }
-
-    /**
-     * @param {!TextDocumentStateCallback} callback
-     */
-    static addObserver(callback) { controller.addObserver(callback); }
-
-    static controller() { return controller; }
-
-    /**
-     * For debugging only
-     * @param {!TextDocument} document
-     * @return {TextDocumentState}
-     */
-    static get(document) { return controller.get(document); }
+  constructor(document) {
+    this.document_ = document;
+    this.fileName = document.fileName;
+    // TODO(eval1749): We should specify |TextDocumentState.prototype.icon|
+    // from
+    // IconSet.
+    this.icon = -2;
+    this.lastWriteTime = document.lastWriteTime;
+    this.modified = document.modified;
+    this.name = document.name;
+    this.state = 0;
   }
 
-  //////////////////////////////////////////////////////////////////////
-  //
-  // Controller
-  //
-  class Controller extends SimpleTextDocumentSetObserver {
-    constructor() {
-      super();
-      this.stateMap_ = new Map();
-      TextDocument.addObserver(this);
-    }
-
-    /**
-     * @param {!TextDocumentStateCallback} callback
-     */
-    addObserver(callback) { observers.push(callback); }
-
-    /**
-     * @override
-     * @param {!TextDocument} document
-     */
-    didAddTextDocument(document) {
-      const state = new TextDocumentState(document);
-      stateMap.set(document, state);
-      state.start();
-    }
-
-    /**
-     * For debugging only
-     * @param {!TextDocument} document
-     * @return {TextDocumentState}
-     */
-    get(document) { return stateMap.get(document) || null; }
-
-    /**
-     * @override
-     * @param {!TextDocument} document
-     */
-    didRemoveTextDocument(document) { stateMap.delete(document); }
+  /**
+   * @private
+   * Set initial tab data for |TextWindow|.
+   * @param {!TextDocumentEvent} event
+   */
+  didAttachWindow(event) {
+    if (event.view === null)
+      return;
+    TabData.update(event.view, this);
   }
 
-  /** @const @type {!Controller} */
-  const controller = new Controller()
+  /**
+   * @private
+   * @param {!TextDocumentEvent} event
+   */
+  didLoadSaveTextDocument(event) {
+    const document = this.document_;
+    this.fileName = document.fileName;
+    this.lastWriteTime = document.lastWriteTime;
+    this.modified = document.modified;
+    this.state = 0;
+    this.notifyChanged();
+  }
 
-                         Object.defineProperties(TextDocumentState, {
-                           controller_: {value: controller},
-                         });
+  /**
+   * @private
+   * @param {!Array.<!TextMutationRecord>} mutations
+   * @param {!TextMutationObserver} observer
+   */
+  mutationCallback(mutations, observer) {
+    if (this.modified === this.document_.modified)
+      return;
+    this.modified = this.document_.modified;
+    this.notifyChanged();
+  }
 
-  $export({TextDocumentState});
+  /**
+   * @private
+   */
+  notifyChanged() {
+    observers.forEach(observer => observer(this.document_, this));
+  }
+
+  /**
+   * @private
+   */
+  start() {
+    const document = this.document_;
+    document.addEventListener(
+        Event.Names.ATTACH, this.didAttachWindow.bind(this));
+    document.addEventListener(
+        Event.Names.BEFORELOAD, this.willLoadTextDocument.bind(this));
+    document.addEventListener(
+        Event.Names.LOAD, this.didLoadSaveTextDocument.bind(this));
+    document.addEventListener(
+        Event.Names.SAVE, this.didLoadSaveTextDocument.bind(this));
+    const mutationObserver =
+        new TextMutationObserver(this.mutationCallback.bind(this));
+    mutationObserver.observe(document, {summary: true});
+    this.notifyChanged();
+  }
+
+  /**
+   * @private
+   * @param {!TextDocumentEvent} event
+   */
+  willLoadTextDocument(event) {
+    this.state = 1;
+    this.notifyChanged();
+  }
+
+  /**
+   * @param {!TextDocumentStateCallback} callback
+   */
+  static addObserver(callback) { controller.addObserver(callback); }
+
+  static controller() { return controller; }
+
+  /**
+   * For debugging only
+   * @param {!TextDocument} document
+   * @return {TextDocumentState}
+   */
+  static get(document) { return controller.get(document); }
+}
+
+//////////////////////////////////////////////////////////////////////
+//
+// Controller
+//
+class Controller extends SimpleTextDocumentSetObserver {
+  constructor() {
+    super();
+    this.stateMap_ = new Map();
+    TextDocument.addObserver(this);
+  }
+
+  /**
+   * @param {!TextDocumentStateCallback} callback
+   */
+  addObserver(callback) { observers.push(callback); }
+
+  /**
+   * @override
+   * @param {!TextDocument} document
+   */
+  didAddTextDocument(document) {
+    const state = new TextDocumentState(document);
+    stateMap.set(document, state);
+    state.start();
+  }
+
+  /**
+   * For debugging only
+   * @param {!TextDocument} document
+   * @return {TextDocumentState}
+   */
+  get(document) { return stateMap.get(document) || null; }
+
+  /**
+   * @override
+   * @param {!TextDocument} document
+   */
+  didRemoveTextDocument(document) { stateMap.delete(document); }
+}
+
+/** @const @type {!Controller} */
+const controller = new Controller();
+
+/** @constructor */
+windows.TextDocumentState = TextDocumentState;
+
+/**
+ * @typedef {TextDocumentStateCallback}
+ */
+windows.TextDocumentStateCallback;
 });
 
-global.TextDocumentState = windows.TextDocumentState;
+/** @constructor */
+var TextDocumentState = windows.TextDocumentState;
+
+/**
+ * @typedef {windows.TextDocumentStateCallback}
+ */
+var TextDocumentStateCallback;

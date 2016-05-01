8873b8cf99e1e599aaa75f2fcc84d690bdf0b4c4
eval1749 <eval1749@gmail.com>
2016-05-01 19:04:09 +0900

    Utilize |goog.scope()| in |text.TextPosition| to improve type checking

--- evita/dom/text/text_position.js
+++ evita/dom/text/text_position.js
@@ -2,216 +2,210 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-(function() {
-  /** @const @type {!Bracket.Detail} */
-  var NOT_BRACKET = new Bracket.Detail(Bracket.Type.NONE, 0);
+goog.scope(function() {
+/** @const @type {!Bracket.Detail} */
+var NOT_BRACKET = new Bracket.Detail(Bracket.Type.NONE, 0);
 
-  /** @const @type {number} */
-  var MAX_BRACKET_NESTING_LEVEL = 10;
+/** @const @type {number} */
+var MAX_BRACKET_NESTING_LEVEL = 10;
 
-  class BracketMatchData {
-    /**
-     * @param {!Bracket.Detail} data
-     * @param {!TextOffset} offset
-     */
-    constructor(data, offset) {
-      this.data = data;
-      this.offset = offset;
-    }
-  }
-
+class BracketMatchData {
   /**
-   * @param {!TextPosition} position
-   * @return {!Bracket.Detail}
+   * @param {!Bracket.Detail} data
+   * @param {!TextOffset} offset
    */
-  function bracketDataOf(position) {
-    var charCode = position.charCode();
+  constructor(data, offset) {
+    this.data = data;
+    this.offset = offset;
+  }
+}
+
+/**
+ * @param {!TextPosition} position
+ * @return {!Bracket.Detail}
+ */
+function bracketDataOf(position) {
+  var charCode = position.charCode();
+  // TODO(eval1749): We should get character syntax from mime type
+  // information.
+  return Bracket.DATA[charCode] || NOT_BRACKET;
+}
+
+/**
+ * @param {string} syntax1
+ * @param {string} syntax2
+ * @return {boolean}
+ */
+function syntaxEquals(syntax1, syntax2) {
+  if (syntax1 === syntax2)
+    return true;
+  return syntax1 === 'normal' || syntax2 == 'normal';
+}
+
+/**
+ * @param {!TextPosition} position
+ * Note: We use syntax coloring information for preventing matching
+ * parenthesis among statement, string and comment.
+ * See also |moveForwardBracket()|.
+ */
+function moveBackwardBracket(position) {
+  /** @type {Array.<BracketMatchData>} */ var bracketStack = [];
+  // reset when we reached at bracket.
+  /**  @type {string} */ var bracketCharSyntax = '?';
+  /** @type {number} */ var startOffset = position.offset;
+
+  while (position.offset) {
+    position.move(Unit.CHARACTER, -1);
     // TODO(eval1749): We should get character syntax from mime type
     // information.
-    return Bracket.DATA[charCode] || NOT_BRACKET;
-  }
+    var bracket = bracketDataOf(position);
+    if (bracket.type == Bracket.Type.NONE)
+      continue;
+    if (bracketCharSyntax == '?')
+      bracketCharSyntax = position.charSyntax();
+    else if (!syntaxEquals(position.charSyntax(), bracketCharSyntax))
+      continue;
 
-  /**
-   * @param {string} syntax1
-   * @param {string} syntax2
-   * @return {boolean}
-   */
-  function syntaxEquals(syntax1, syntax2) {
-    if (syntax1 === syntax2)
-      return true;
-    return syntax1 === 'normal' || syntax2 == 'normal';
-  }
+    var currentOffset = position.offset;
+    position.moveWhile(function() {
+      return syntaxEquals(this.charSyntax(), bracketCharSyntax) &&
+          bracketDataOf(this).type == Bracket.Type.ESCAPE;
+    }, Count.BACKWARD);
+    if ((currentOffset - position.offset) & 1)
+      continue;
 
-  /**
-   * @param {!TextPosition} position
-   * Note: We use syntax coloring information for preventing matching
-   * parenthesis among statement, string and comment.
-   * See also |moveForwardBracket()|.
-   */
-  function moveBackwardBracket(position) {
-    /** @type {Array.<BracketMatchData>} */ var bracketStack = [];
-    // reset when we reached at bracket.
-    /**  @type {string} */ var bracketCharSyntax = '?';
-    /** @type {number} */ var startOffset = position.offset;
+    var nextOffset = position.offset;
+    position.offset = currentOffset;
 
-    while (position.offset) {
-      position.move(Unit.CHARACTER, -1);
-      // TODO(eval1749): We should get character syntax from mime type
-      // information.
-      var bracket = bracketDataOf(position);
-      if (bracket.type == Bracket.Type.NONE)
-        continue;
-      if (bracketCharSyntax == '?')
-        bracketCharSyntax = position.charSyntax();
-      else if (!syntaxEquals(position.charSyntax(), bracketCharSyntax))
-        continue;
-
-      var currentOffset = position.offset;
-      position.moveWhile(function() {
-        return syntaxEquals(this.charSyntax(), bracketCharSyntax) &&
-            bracketDataOf(this).type == Bracket.Type.ESCAPE;
-      }, Count.BACKWARD);
-      if ((currentOffset - position.offset) & 1)
-        continue;
-
-      var nextOffset = position.offset;
-      position.offset = currentOffset;
-
-      if (bracket.type == Bracket.Type.LEFT) {
-        if (!bracketStack.length) {
-          // We reach at left bracket.
-          return;
-        }
-        var lastBracket = bracketStack.pop();
-        if (lastBracket.data.pair != position.charCode()) {
-          // We reach at mismatched left bracket.
-          break;
-        }
-
-        if (!bracketStack.length) {
-          // We reach at matched left bracket.
-          return;
-        }
-        position.offset = nextOffset;
-      } else if (bracket.type == Bracket.Type.RIGHT) {
-        if (!bracketStack.length) {
-          if (position.offset != startOffset - 1) {
-            // We found right bracket.
-            position.move(Unit.CHARACTER);
-            return;
-          }
-        }
-        bracketStack.push(new BracketMatchData(bracket, position.offset));
-        position.offset = nextOffset;
+    if (bracket.type == Bracket.Type.LEFT) {
+      if (!bracketStack.length) {
+        // We reach at left bracket.
+        return;
       }
-    }
-    position.offset = startOffset;
-  }
+      var lastBracket = bracketStack.pop();
+      if (lastBracket.data.pair != position.charCode()) {
+        // We reach at mismatched left bracket.
+        break;
+      }
 
-  /**
-   * @param {!TextPosition} position
-   * Note: We use syntax coloring information for preventing matching
-   * parenthesis among statement, string and comment.
-   * See also |moveBackwardBracket()|.
-   */
-  function moveForwardBracket(position) {
-    /** @type {Array.<BracketMatchData>} */ var bracketStack = [];
-    // reset when we reached at racket.
-    /** @type {string} */ var bracketCharSyntax = '?';
-    /** @type {number} */ var startOffset = position.offset;
-
-    for (; position.offset < position.document.length;
-         position.move(Unit.CHARACTER)) {
-      var bracket = bracketDataOf(position);
-      if (bracket.type == Bracket.Type.NONE)
-        continue;
-      if (bracketCharSyntax == '?')
-        bracketCharSyntax = position.charSyntax();
-      else if (!syntaxEquals(position.charSyntax(), bracketCharSyntax))
-        continue;
-      switch (bracket.type) {
-        case Bracket.Type.ESCAPE: {
-          var currentOffset = position.offset;
-          position.moveWhile(function() {
-            return bracketDataOf(this).type == Bracket.Type.ESCAPE &&
-                syntaxEquals(this.charSyntax(), bracketCharSyntax);
-          }, Count.FORWARD);
-          if (!((position.offset - currentOffset) & 1))
-            position.move(Unit.CHARACTER, -1);
-          break;
+      if (!bracketStack.length) {
+        // We reach at matched left bracket.
+        return;
+      }
+      position.offset = nextOffset;
+    } else if (bracket.type == Bracket.Type.RIGHT) {
+      if (!bracketStack.length) {
+        if (position.offset != startOffset - 1) {
+          // We found right bracket.
+          position.move(Unit.CHARACTER);
+          return;
         }
-        case Bracket.Type.LEFT:
-          if (!bracketStack.length) {
-            if (position.offset != startOffset) {
-              // We reach left bracket.
-              return;
-            }
-            bracketStack.push(new BracketMatchData(bracket, position.offset));
-            break;
-          }
+      }
+      bracketStack.push(new BracketMatchData(bracket, position.offset));
+      position.offset = nextOffset;
+    }
+  }
+  position.offset = startOffset;
+}
 
-          if (bracketStack.length == MAX_BRACKET_NESTING_LEVEL) {
-            // We found too many left bracket.
+/**
+ * @param {!TextPosition} position
+ * Note: We use syntax coloring information for preventing matching
+ * parenthesis among statement, string and comment.
+ * See also |moveBackwardBracket()|.
+ */
+function moveForwardBracket(position) {
+  /** @type {Array.<BracketMatchData>} */ var bracketStack = [];
+  // reset when we reached at racket.
+  /** @type {string} */ var bracketCharSyntax = '?';
+  /** @type {number} */ var startOffset = position.offset;
+
+  for (; position.offset < position.document.length;
+       position.move(Unit.CHARACTER)) {
+    var bracket = bracketDataOf(position);
+    if (bracket.type == Bracket.Type.NONE)
+      continue;
+    if (bracketCharSyntax == '?')
+      bracketCharSyntax = position.charSyntax();
+    else if (!syntaxEquals(position.charSyntax(), bracketCharSyntax))
+      continue;
+    switch (bracket.type) {
+      case Bracket.Type.ESCAPE: {
+        var currentOffset = position.offset;
+        position.moveWhile(function() {
+          return bracketDataOf(this).type == Bracket.Type.ESCAPE &&
+              syntaxEquals(this.charSyntax(), bracketCharSyntax);
+        }, Count.FORWARD);
+        if (!((position.offset - currentOffset) & 1))
+          position.move(Unit.CHARACTER, -1);
+        break;
+      }
+      case Bracket.Type.LEFT:
+        if (!bracketStack.length) {
+          if (position.offset != startOffset) {
+            // We reach left bracket.
             return;
           }
           bracketStack.push(new BracketMatchData(bracket, position.offset));
           break;
-        case Bracket.Type.RIGHT:
-          if (!bracketStack.length) {
-            // We reach right bracket.
-            position.move(Unit.CHARACTER);
-            return;
-          }
-          var lastBracket = bracketStack.pop();
-          if (lastBracket.data.pair != position.charCode()) {
-            // We reach mismatched right bracket.
-            return;
-          }
+        }
 
-          if (!bracketStack.length) {
-            // We reach matched right bracket.
-            position.move(Unit.CHARACTER);
-            return;
-          }
-          break;
-      }
+        if (bracketStack.length == MAX_BRACKET_NESTING_LEVEL) {
+          // We found too many left bracket.
+          return;
+        }
+        bracketStack.push(new BracketMatchData(bracket, position.offset));
+        break;
+      case Bracket.Type.RIGHT:
+        if (!bracketStack.length) {
+          // We reach right bracket.
+          position.move(Unit.CHARACTER);
+          return;
+        }
+        var lastBracket = bracketStack.pop();
+        if (lastBracket.data.pair != position.charCode()) {
+          // We reach mismatched right bracket.
+          return;
+        }
+
+        if (!bracketStack.length) {
+          // We reach matched right bracket.
+          position.move(Unit.CHARACTER);
+          return;
+        }
+        break;
     }
-    if (bracketStack.length)
-      position.offset = bracketStack.pop().offset;
-    else
-      position.offset = startOffset;
   }
+  if (bracketStack.length)
+    position.offset = bracketStack.pop().offset;
+  else
+    position.offset = startOffset;
+}
 
+class TextPosition {
   /**
-   * @constructor
    * @param {!TextDocument} document
    * @param {!number} offset
    */
-  global.TextPosition = (function() {
-    function TextPosition(document, offset) {
-      if (offset < 0 || offset > document.length)
-        throw new RangeError('Invalid offset ' + offset + ' for ' + document);
-      this.document = document;
-      this.offset = offset;
-    }
-    return TextPosition;
-  })();
+  constructor(document, offset) {
+    if (offset < 0 || offset > document.length)
+      throw new RangeError('Invalid offset ' + offset + ' for ' + document);
+    this.document = document;
+    this.offset = offset;
+  }
 
   /** * @return {number} */
-  global.TextPosition.prototype.charCode =
-      function() { return this.document.charCodeAt(this.offset); }
+  charCode() { return this.document.charCodeAt(this.offset); }
 
-      /** * @return {string} */
-      global.TextPosition.prototype.charSyntax =
-          function() { return this.document.syntaxAt(this.offset); }
+  /** * @return {string} */
+  charSyntax() { return this.document.syntaxAt(this.offset); }
 
-          /**
-           * @this {!TextPosition}
-           * @param {!Unit} unit
-           * @param {number=} count, default is one.
-           */
-          global.TextPosition.prototype.move =
-              function(unit, count = 1) {
+  /**
+   * @this {!TextPosition}
+   * @param {!Unit} unit
+   * @param {number=} count, default is one.
+   */
+  move(unit, count = 1) {
     if (unit != Unit.BRACKET)
       this.offset = this.document.computeMotion_(unit, count, this.offset);
     else if (count > 0)
@@ -221,14 +215,13 @@
     return this;
   }
 
-              /**
-               * @this {!TextPosition}
-               * @param {function() : boolean} callback
-               * @param {number=} count, default is one
-               * @return {!TextPosition}
-               */
-              global.TextPosition.prototype.moveWhile = function(
-                  callback, count = 1) {
+  /**
+   * @this {!TextPosition}
+   * @param {function() : boolean} callback
+   * @param {number=} count, default is one
+   * @return {!TextPosition}
+   */
+  moveWhile(callback, count = 1) {
     if (count < 0) {
       while (count && this.offset) {
         --this.offset;
@@ -248,5 +241,12 @@
       }
     }
     return this;
-  };
-})();
+  }
+}
+
+/** @constructor */
+text.TextPosition = TextPosition;
+});
+
+/** @constructor */
+var TextPosition = text.TextPosition;

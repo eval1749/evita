1bd8972bdf6a3c30ebe407c1059f8a42d603df2c
eval1749 <eval1749@gmail.com>
2016-05-01 18:56:28 +0900

    Make ES6-ish for |TextDocumentErro|, |TextDocumentNotReady| and |TextDocumentReadOnly|

--- evita/dom/errors.js
+++ evita/dom/errors.js
@@ -2,61 +2,53 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-global.TextDocumentError = (function() {
+goog.scope(function() {
+
+class TextDocumentError extends Error {
   /**
    * @param {!TextDocument} document
    * @param {string=} opt_message
    */
-  function TextDocumentError(document, opt_message) {
-    if (arguments.length >= 2) {
-      Error.call(this, opt_message);
-      // TODO(eval1749): Not sure why |Error.call(this, opt_message)| doesn't
-      // set
-      // |message| property.
-      this.message = opt_message;
-    } else {
-      Error.call(this);
-    }
+  constructor(document, opt_message) {
+    super(opt_message);
     this.document = document;
+    this.name = this.constructor.name;
   }
+}
 
-  TextDocumentError.prototype = Object.create(
-      /** @type {!Object} */ (Error.prototype),
-      {document: {configurable: false, enumerable: false, writable: true}});
-  TextDocumentError.prototype.constructor = TextDocumentError;
-  TextDocumentError.prototype.name = TextDocumentError.name;
-  return TextDocumentError;
-})();
-
-
-global.TextDocumentNotReady = (function() {
+class TextDocumentNotReady extends TextDocumentError {
   /**
    * @param {!TextDocument} document
    * @param {string=} opt_message
    */
-  function TextDocumentNotReady(document, opt_message) {
-    TextDocumentError.apply(this, arguments);
-  }
-  TextDocumentNotReady.prototype = /** @type {!TextDocumentNotReady} */ (
-      Object.create(
-          /** @type {!Object} */ (TextDocumentError.prototype), {}));
-  TextDocumentNotReady.prototype.constructor = TextDocumentNotReady;
-  TextDocumentNotReady.prototype.name = TextDocumentNotReady.name;
-  return TextDocumentNotReady;
-})();
+  constructor(document, opt_message) { super(document, opt_message); }
+}
 
-global.TextDocumentReadOnly = (function() {
+class TextDocumentReadOnly extends TextDocumentError {
   /**
    * @param {!TextDocument} document
    * @param {string=} opt_message
    */
-  function TextDocumentReadOnly(document, opt_message) {
-    TextDocumentError.apply(this, arguments);
-  }
-  TextDocumentReadOnly.prototype = /** @type {!TextDocumentReadOnly} */ (
-      Object.create(
-          /** @type {!Object} */ (TextDocumentError.prototype), {}));
-  TextDocumentReadOnly.prototype.constructor = TextDocumentReadOnly;
-  TextDocumentReadOnly.prototype.name = TextDocumentReadOnly.name;
-  return TextDocumentReadOnly;
-})();
+  constructor(document, opt_message) { super(document, opt_message); }
+}
+
+/** @constructor */
+text.TextDocumentError = TextDocumentError;
+
+/** @constructor */
+text.TextDocumentNotReady = TextDocumentNotReady;
+
+/** @constructor */
+text.TextDocumentReadOnly = TextDocumentReadOnly;
+});
+
+// TODO(eval1749): We should not export |TextDocumentError|,
+// |TextDocumentNotReady|, and |TextDocumentReadOnly| to |global|.
+/** @constructor */
+var TextDocumentError = text.TextDocumentError;
+
+/** @constructor */
+var TextDocumentNotReady = text.TextDocumentNotReady;
+
+/** @constructor */
+var TextDocumentReadOnly = text.TextDocumentReadOnly;

b06fe1e241f90878c265e4178a4c3cf710b8573a
eval1749 <eval1749@gmail.com>
2016-05-01 22:55:28 +0900

    Utilize |goog.scope()| in "//evita/dom/jslib/editors.js"

--- evita/dom/jslib/editors.js
+++ evita/dom/jslib/editors.js
@@ -2,49 +2,53 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-global.editors = {};
+goog.provide('editors');
 
-(function() {
-  function loadUserScript() {
-    var context = {
-      dirs: [
-        Os.getenv('HOME'), Os.getenv('USERPROFILE'),
-        Os.getenv('HOMEDRIVE') + Os.getenv('HOMEPATH')
-      ],
-      scriptPath: ''
-    };
-    function tryLoad() {
-      if (!context.dirs.length)
-        return Promise.reject('none');
-      context.scriptPath = FilePath.join(context.dirs.shift(), 'evitarc.js');
-      return repl.load(context.scriptPath, {verbose: true});
-    }
-    return tryLoad().catch(function(reason) {
-      if (!(reason instanceof Os.File.Error))
-        return;
-      tryLoad();
-    });
+goog.scope(function() {
+
+const Initializer = core.Initializer;
+
+function loadUserScript() {
+  var context = {
+    dirs: [
+      Os.getenv('HOME'), Os.getenv('USERPROFILE'),
+      Os.getenv('HOMEDRIVE') + Os.getenv('HOMEPATH')
+    ],
+    scriptPath: ''
+  };
+  function tryLoad() {
+    if (!context.dirs.length)
+      return Promise.reject('none');
+    context.scriptPath = FilePath.join(context.dirs.shift(), 'evitarc.js');
+    return repl.load(context.scriptPath, {verbose: true});
   }
+  return tryLoad().catch(function(reason) {
+    if (!(reason instanceof Os.File.Error))
+      return;
+    tryLoad();
+  });
+}
 
-  /**
-   * @param {!Array.<string>} args
-   */
-  function start(args) {
-    Initializer.initialize();
-    loadUserScript();
-    /** @const @type {!EditorWindow} */
-    const editorWindow = new EditorWindow();
-    /** @const @type {!TextDocument} */
-    const doc = TextDocument.new('*scratch*');
-    doc.mode = Mode.create('plain');
-    /** @const @type {!TextRange} */
-    const range = new TextRange(doc);
-    /** @const @type {!TextWindow} */
-    const textWindow = new TextWindow(range);
-    editorWindow.appendChild(textWindow);
-    editorWindow.realize();
-    args.forEach(fileName => Editor.open(fileName));
-  }
+/**
+ * @param {!Array.<string>} args
+ */
+function start(args) {
+  Initializer.initialize();
+  loadUserScript();
+  /** @const @type {!EditorWindow} */
+  const editorWindow = new EditorWindow();
+  /** @const @type {!TextDocument} */
+  const doc = TextDocument.new('*scratch*');
+  doc.mode = Mode.create('plain');
+  /** @const @type {!TextRange} */
+  const range = new TextRange(doc);
+  /** @const @type {!TextWindow} */
+  const textWindow = new TextWindow(range);
+  editorWindow.appendChild(textWindow);
+  editorWindow.realize();
+  args.forEach(fileName => Editor.open(fileName));
+}
 
-  Object.defineProperties(editors, {start: {value: start}});
-})();
+/** @const @type {function(!Array.<string>)} */
+editors.start = start;
+});

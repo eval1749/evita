c08328c08c86f2e91ebda9329eaa7867e0be8fc1
eval1749 <eval1749@gmail.com>
2016-05-02 21:49:45 +0900

    Add |goog.provide('windows')| for "//evita/dom/jslib"

--- evita/dom/jslib/windows_externs.js
+++ evita/dom/jslib/windows_externs.js
@@ -2,8 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/** @type {!Object} */
-var windows;
+goog.provide('windows');
 
 /**
  * @param {!Window} window

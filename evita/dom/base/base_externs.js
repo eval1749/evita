96132720ad8d018fe4ea7c3dc01e836cf06e7fc8
eval1749 <eval1749@gmail.com>
2016-05-02 21:42:30 +0900

    Add "//evita/base/base_externs.js"

--- evita/dom/base/base_externs.js
+++ evita/dom/base/base_externs.js
@@ -2,5 +2,14 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/** @type {!Object} */
-// var base;
+goog.provide('base');
+
+/** @constructor */
+base.Singleton = function() {};
+
+/**
+ * @template T
+ * @param {function(new: T)} constructor
+ * @return {!T}
+ */
+base.Singleton.get = function(constructor) {};

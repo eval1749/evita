c448eb4462e385a9f9784e37ede8396949fc4f38
eval1749 <eval1749@gmail.com>
2016-05-07 14:52:21 +0900

    Introduce "references" variable in js_module_externs() GN template
    This patch introduces "references" variable to specify list of referenced
    IDL files, e.g. interfaces having "JsNamespace" extended attribute, to
    |js_module_externs()| GN template to avoid leak |CSSStyleDeclaration| to
    global namespace from "css" namespace.
    
    BUG=n/a
    TEST=n/a; no behavior changes

--- evita/dom/css/css_externs.js
+++ evita/dom/css/css_externs.js
@@ -3,8 +3,3 @@
 // found in the LICENSE file.
 
 goog.provide('css');
-
-// TODO(eval1749): We should teach IDL to JS externs about JavaScript namespace
-// of |CSSStyleDeclaration| for "Element.idl".
-/** @constructor */
-var CSSStyleDeclaration = css.CSSStyleDeclaration;

2bd72dcf1fa3bc7310789840e068e483ceae59d4
eval1749 <eval1749@gmail.com>
2016-05-12 10:17:07 +0900

    Make ginx::FinishConstructorCall() not to set this.prototype
    This patch makes ginx::FinishConstructorCall() not to set |this.prototype|
    to |new.target.prototype| since V8 bugs[1] makes V8 does so.
    
    [1] https://bugs.chromium.org/p/v8/issues/detail?id=5001

--- evita/ginx/constructor_template.cc
+++ evita/ginx/constructor_template.cc
@@ -11,35 +11,17 @@
 namespace ginx {
 namespace internal {
 
-base::string16 AsString(v8::Local<v8::Value> value) {
-  v8::String::Value string_value(value);
-  if (!string_value.length())
-    return base::string16();
-  return base::string16(reinterpret_cast<base::char16*>(*string_value),
-                        static_cast<size_t>(string_value.length()));
-}
-
+// This function is used in callback functions generated from IDL compiler.
 void FinishConstructCall(const v8::FunctionCallbackInfo<v8::Value>& info,
                          AbstractScriptable* impl) {
   if (!impl) {
     // Constructor may throw an exception.
     return;
   }
-  const auto& wrapper = info.Holder();
-  impl->Bind(info.GetIsolate(), wrapper);
-
-  const auto& context = info.GetIsolate()->GetCurrentContext();
-  // TODO(eval1749): Should we have string "prototype" in another place?
-  const auto& prototype_str = gin::StringToV8(info.GetIsolate(), "prototype");
-  const auto& prototype =
-      info.NewTarget().As<v8::Function>()->Get(prototype_str);
-  const auto& couldSetPrototype = wrapper->SetPrototype(context, prototype);
-  if (couldSetPrototype.IsNothing()) {
-    DVLOG(0) << "SetPrototype failed";
-    return;
-  }
+  impl->Bind(info.GetIsolate(), info.Holder());
 }
 
+// This function is used in callback functions generated from IDL compiler.
 bool IsValidConstructCall(const v8::FunctionCallbackInfo<v8::Value>& info) {
   auto const isolate = info.GetIsolate();
   if (!info.IsConstructCall()) {
@@ -49,9 +31,7 @@ bool IsValidConstructCall(const v8::FunctionCallbackInfo<v8::Value>& info) {
   }
   // When we're creating wrapper, e.g. Scriptable::GetWrapper(), we have
   // already had Scriptable object.
-  if (ginx::PerIsolateData::From(isolate)->is_creating_wrapper())
-    return false;
-  return true;
+  return !ginx::PerIsolateData::From(isolate)->is_creating_wrapper();
 }
 }  // namespace internal
 }  // namespace ginx

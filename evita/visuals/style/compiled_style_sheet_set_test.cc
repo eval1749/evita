af14544fe0c79114ddb51e99dae83c5fa1450930
eval1749 <eval1749@gmail.com>
2016-05-16 23:38:01 +0900

    Add CompiledStyleSheetSetTest.Hover

--- evita/visuals/style/compiled_style_sheet_set_test.cc
+++ evita/visuals/style/compiled_style_sheet_set_test.cc
@@ -61,16 +61,21 @@ TEST(CompiledStyleSheetSetTest, Basic) {
                             .ClassList({L"c1", L"c2"})
                             .End(L"tag")
                             .End(L"tag")
+                            .Begin(L"tag", L"c1c3")
+                            .ClassList({L"c1", L"c3"})
+                            .End(L"tag")
                             .Build();
 
   const auto tag = document->first_child()->as<Element>();
   const auto tag_c1 = document->GetElementById(L"c1");
   const auto tag_c1c2 = document->GetElementById(L"c1c2");
+  const auto tag_c1c3 = document->GetElementById(L"c1c3");
   const auto tag_id = document->GetElementById(L"id");
 
   const auto& tag_style = ComputeStyle(compiled, *tag);
   const auto& tag_c1_style = ComputeStyle(compiled, *tag_c1);
   const auto& tag_c1c2_style = ComputeStyle(compiled, *tag_c1c2);
+  const auto& tag_c1c3_style = ComputeStyle(compiled, *tag_c1c3);
   const auto& tag_id_style = ComputeStyle(compiled, *tag_id);
 
   EXPECT_EQ(css::Color(css::ColorValue(1, 0, 0)), tag_style->color());
@@ -82,8 +87,44 @@ TEST(CompiledStyleSheetSetTest, Basic) {
   EXPECT_EQ(css::Color(css::ColorValue(1, 1, 0)), tag_c1c2_style->color());
   EXPECT_EQ(css::Height(css::Length(20)), tag_c1c2_style->height());
 
+  EXPECT_EQ(css::Color(css::ColorValue(0, 1, 0)), tag_c1c3_style->color());
+  EXPECT_EQ(css::Height(css::Length(20)), tag_c1c2_style->height());
+
   EXPECT_EQ(css::Color(css::ColorValue(0, 0, 1)), tag_id_style->color());
   EXPECT_EQ(css::Height(css::Length(20)), tag_id_style->height());
+}
+
+TEST(CompiledStyleSheetSetTest, Hover) {
+  const auto style_sheet = new css::StyleSheet();
+  style_sheet->AppendRule(
+      ParseSelector(L"tag"),
+      css::StyleBuilder().SetColor(1, 0, 0).SetHeight(20).Build());
+  style_sheet->AppendRule(ParseSelector(L".c1"),
+                          css::StyleBuilder().SetColor(0, 0, 1).Build());
+  style_sheet->AppendRule(ParseSelector(L".c2"),
+                          css::StyleBuilder().SetColor(0, 1, 0).Build());
+  CompiledStyleSheetSet compiled({style_sheet});
+
+  const auto document = NodeTreeBuilder()
+                            .Begin(L"tag", L"c1")
+                            .ClassList({L"c1", L"hover"})
+                            .End(L"tag")
+                            .Begin(L"tag", L"c2")
+                            .ClassList({L"c2"})
+                            .End(L"tag")
+                            .Build();
+
+  const auto tag_c1 = document->GetElementById(L"c1");
+  const auto tag_c2 = document->GetElementById(L"c2");
+
+  const auto& tag_c1_style = ComputeStyle(compiled, *tag_c1);
+  const auto& tag_c2_style = ComputeStyle(compiled, *tag_c2);
+
+  EXPECT_EQ(css::Color(css::ColorValue(0, 0, 1)), tag_c1_style->color());
+  EXPECT_EQ(css::Height(css::Length(20)), tag_c1_style->height());
+
+  EXPECT_EQ(css::Color(css::ColorValue(0, 1, 0)), tag_c2_style->color());
+  EXPECT_EQ(css::Height(css::Length(20)), tag_c2_style->height());
 }
 
 }  // namespace visuals

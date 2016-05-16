eval1749/evita

commit 92cd8409a9f3736fad496408f4da10a20ac0483f

Make visuals::Painter to utilize drawing rectangle for simple border case

evita/visuals/paint/painter.cc
@@ -201,6 +201,25 @@ void PaintVisitor::PaintBorderINeeded(const Box& box) {
     return;
 #endif
   const auto& border = box.border();
+  if (border.IsSimple()) {
+    const auto& color = border.top_color();
+    const auto height = box.bounds().height();
+    const auto size = border.top();
+    const auto width = box.bounds().width();
+    builder_.AddNew<DrawRectDisplayItem>(
+        gfx::FloatRect(box.bounds().size() - gfx::FloatSize(1.0f, 1.0f)), color,
+        size);
+    AddDirtyBounds(gfx::FloatRect(gfx::FloatSize(width, 1.0f)));
+    AddDirtyBounds(
+        gfx::FloatRect(gfx::FloatPoint(), gfx::FloatSize(1.0f, height)));
+    AddDirtyBounds(
+        gfx::FloatRect(box.bounds().top_right() - gfx::FloatSize(1.0f, 0.0f),
+                       gfx::FloatSize(1.0f, height)));
+    AddDirtyBounds(
+        gfx::FloatRect(box.bounds().bottom_left() - gfx::FloatSize(0.0f, 1.0f),
+                       gfx::FloatSize(width, 1.0f)));
+    return;
+  }
   if (border.top()) {
     FillRectAndMark(
         gfx::FloatRect(gfx::FloatPoint(),

commit ae716c1f309a6cd69cf242d51064aefa82ff11bc

Don't draw size=0 border
This patch makes visuals::Painter not to draw size=0 rectangle regressed
by 92cd8409a9f3736fad496408f4da10a20ac0483f

evita/visuals/paint/painter.cc
@@ -202,9 +202,11 @@ void PaintVisitor::PaintBorderINeeded(const Box& box) {
 #endif
   const auto& border = box.border();
   if (border.IsSimple()) {
+    const auto size = border.top();
+    if (size == 0)
+      return;
     const auto& color = border.top_color();
     const auto height = box.bounds().height();
-    const auto size = border.top();
     const auto width = box.bounds().width();
     builder_.AddNew<DrawRectDisplayItem>(
         gfx::FloatRect(box.bounds().size() - gfx::FloatSize(1.0f, 1.0f)), color,
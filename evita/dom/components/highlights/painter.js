a3de9d70a023fee3a92a2bdeaa0fe9c9a9579837
eval1749 <eval1749@gmail.com>
2016-07-09 22:29:12 +0900

    Fix order of goog.provide()

--- evita/dom/components/highlights/painter.js
+++ evita/dom/components/highlights/painter.js
@@ -2,8 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-goog.provide('highlights.Paint');
 goog.provide('highlights.KeywordPaint');
+goog.provide('highlights.Paint');
 
 goog.require('base.Logger');
 goog.require('highlights');

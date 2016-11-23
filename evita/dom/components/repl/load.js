ccd19b12b7a10d1bd652576028f7029a89853605
eval1749 <eval1749@gmail.com>
2016-11-23 22:04:29 +0900

    Utilzie "async" keyword in load()x

--- evita/dom/components/repl/load.js
+++ evita/dom/components/repl/load.js
@@ -14,53 +14,51 @@ const LINE_COMMENT = '\x2F/';
  *  encoding: string
  *  verbose: boolean
  */
-function load(scriptPath, options = {}) {
+async function load(scriptPath, options = {}) {
   /** @const @type {string} */
   const encoding =
       options.encoding ? /** @type{string} */ (options.encoding) : 'utf-8';
   if (options.verbose)
     console.log(LINE_COMMENT, 'loading', scriptPath);
   /** @type {Os.File} */
-  let file = null;
-  return (async(function * () {
-           file = yield Os.File.open(scriptPath);
-           /** @type {number} */
-           let totalRead = 0;
-           /** @type {string} */
-           let text = '';
-           /** @type {!ArrayBufferView} */
-           const buffer = new Uint8Array(4096);
-           /** @type {!TextDecoder} */
-           const decoder = new TextDecoder(encoding, {fatal: true});
-           for (;;) {
-             /** @const @type {number} */
-             const numRead = yield file.read(buffer);
-             if (numRead === 0)
-               break;
-             text += decoder.decode(buffer.subarray(0, numRead));
-             totalRead += numRead;
-           }
-           file.close();
-           file = null;
-           text = text.replace(/\r\n/g, '\n');
-           if (options.verbose)
-             console.log(LINE_COMMENT, 'loaded', scriptPath, totalRead);
-           /** @const @type {!RunScriptResult} */
-           const result = Editor.runScript(text, scriptPath);
-           if (result.exception instanceof SyntaxError) {
-             const syntaxError = result.exception + '; ' + scriptPath + '(' +
-                 result.lineNumber + ':' + result.startColumn + ':' +
-                 result.endColumn + ')';
-             throw syntaxError;
-           }
-           if (result.exception)
-             throw result.exception;
-           return Promise.resolve();
-         }))().catch((reason) => {
+  let file = await Os.File.open(scriptPath);
+  try {
+    /** @type {number} */
+    let totalRead = 0;
+    /** @type {string} */
+    let text = '';
+    /** @type {!ArrayBufferView} */
+    const buffer = new Uint8Array(4096);
+    /** @type {!TextDecoder} */
+    const decoder = new TextDecoder(encoding, {fatal: true});
+    for (;;) {
+      /** @const @type {number} */
+      const numRead = await file.read(buffer);
+      if (numRead === 0)
+        break;
+      text += decoder.decode(buffer.subarray(0, numRead));
+      totalRead += numRead;
+    }
+    file.close();
+    file = null;
+    text = text.replace(/\r\n/g, '\n');
+    if (options.verbose)
+      console.log(LINE_COMMENT, 'loaded', scriptPath, totalRead);
+    /** @const @type {!RunScriptResult} */
+    const result = Editor.runScript(text, scriptPath);
+    if (result.exception instanceof SyntaxError) {
+      const syntaxError = result.exception + '; ' + scriptPath + '(' +
+          result.lineNumber + ':' + result.startColumn + ':' +
+          result.endColumn + ')';
+      throw syntaxError;
+    }
+    if (result.exception)
+      throw result.exception;
+    return totalRead;
+  } finally {
     if (file)
       file.close();
-    throw reason;
-  });
+  }
 }
 
 /**

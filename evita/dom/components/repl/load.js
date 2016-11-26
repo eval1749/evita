17f4fef4150075ece433c2be34fd69ee597d8533
eval1749 <eval1749@gmail.com>
2016-11-26 13:25:33 +0900

    Re-factor load() to split script file loading and executing script

--- evita/dom/components/repl/load.js
+++ evita/dom/components/repl/load.js
@@ -7,8 +7,36 @@ const LINE_COMMENT = '\x2F/';
 
 /**
  * @param {string} scriptPath
+ * @param {string} encoding
+ * @return {!Promise<number>}
+ */
+async function readScript(scriptPath, encoding) {
+  /** @type {Os.File} */
+  const file = await Os.File.open(scriptPath);
+  try {
+    /** @type {!Array<string>} */
+    const texts = [];
+    /** @type {!ArrayBufferView} */
+    const buffer = new Uint8Array(4096);
+    /** @type {!TextDecoder} */
+    const decoder = new TextDecoder(encoding, {fatal: true});
+    for (;;) {
+      /** @const @type {number} */
+      const numRead = await file.read(buffer);
+      if (numRead === 0)
+        break;
+      texts.push(decoder.decode(buffer.subarray(0, numRead)));
+    }
+    return texts.join('').replace(/\r\n/g, '\n');
+  } finally {
+    file.close();
+  }
+}
+
+/**
+ * @param {string} scriptPath
  * @param {!Object=} options
- * @return {!Promise}
+ * @return {!Promise<number>}
  *
  * Options:
  *  encoding: string
@@ -20,45 +48,21 @@ async function load(scriptPath, options = {}) {
       options.encoding ? /** @type{string} */ (options.encoding) : 'utf-8';
   if (options.verbose)
     console.log(LINE_COMMENT, 'loading', scriptPath);
-  /** @type {Os.File} */
-  let file = await Os.File.open(scriptPath);
-  try {
-    /** @type {number} */
-    let totalRead = 0;
-    /** @type {string} */
-    let text = '';
-    /** @type {!ArrayBufferView} */
-    const buffer = new Uint8Array(4096);
-    /** @type {!TextDecoder} */
-    const decoder = new TextDecoder(encoding, {fatal: true});
-    for (;;) {
-      /** @const @type {number} */
-      const numRead = await file.read(buffer);
-      if (numRead === 0)
-        break;
-      text += decoder.decode(buffer.subarray(0, numRead));
-      totalRead += numRead;
-    }
-    file.close();
-    file = null;
-    text = text.replace(/\r\n/g, '\n');
-    if (options.verbose)
-      console.log(LINE_COMMENT, 'loaded', scriptPath, totalRead);
-    /** @const @type {!RunScriptResult} */
-    const result = Editor.runScript(text, scriptPath);
-    if (result.exception instanceof SyntaxError) {
-      const syntaxError = result.exception + '; ' + scriptPath + '(' +
-          result.lineNumber + ':' + result.startColumn + ':' +
-          result.endColumn + ')';
-      throw syntaxError;
-    }
-    if (result.exception)
-      throw result.exception;
-    return totalRead;
-  } finally {
-    if (file)
-      file.close();
+  /** @type {string} */
+  const scriptSource = await readScript(scriptPath, encoding);
+  if (options.verbose)
+    console.log(LINE_COMMENT, 'loaded', scriptPath, scriptSource.length);
+  /** @const @type {!RunScriptResult} */
+  const result = Editor.runScript(scriptSource, scriptPath);
+  if (result.exception instanceof SyntaxError) {
+    const syntaxError = result.exception + '; ' + scriptPath + '(' +
+        result.lineNumber + ':' + result.startColumn + ':' +
+        result.endColumn + ')';
+    throw syntaxError;
   }
+  if (result.exception)
+    throw result.exception;
+  return scriptSource.length;
 }
 
 /**

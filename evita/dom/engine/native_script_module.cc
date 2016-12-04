d00e02fa05de65a414e1705e4c23c2f3309f2af0
eval1749 <eval1749@gmail.com>
2016-12-05 02:11:09 +0900

    Make NativeScriptModule::Compile() to set fileName, lineNumber and sourceLine property to a exception throwed by compiler

--- evita/dom/engine/native_script_module.cc
+++ evita/dom/engine/native_script_module.cc
@@ -114,6 +114,71 @@ ModuleResolver* ModuleResolver::GetInstance() {
   return base::Singleton<ModuleResolver>::get();
 }
 
+bool IsInstanceOf(v8::Local<v8::Context> context,
+                  v8::Local<v8::Object> object,
+                  v8::Local<v8::Object> clazz) {
+  DCHECK(clazz->IsConstructor());
+  auto* const isolate = context->GetIsolate();
+  auto constructorKey = gin::StringToV8(isolate, "constructor");
+  for (v8::Local<v8::Value> runner = object; runner->IsObject();
+       runner = runner->ToObject()->GetPrototype()) {
+    auto constructor = runner->ToObject(context)
+                           .ToLocalChecked()
+                           ->Get(context, constructorKey)
+                           .FromMaybe(v8::Local<v8::Value>());
+    if (constructor == clazz)
+      return true;
+  }
+  return false;
+}
+
+// Populate Mozilla compiatible |fileName|, |lineNumber| and |columnNumber|
+// properties to |Error| object if possible.
+void ProcessMessage(v8::Local<v8::Context> context,
+                    const v8::TryCatch& try_catch) {
+  auto const message = try_catch.Message();
+  if (message.IsEmpty())
+    return;
+  auto const js_error = try_catch.Exception()->ToObject(context).FromMaybe(
+      v8::Local<v8::Object>());
+  if (js_error.IsEmpty())
+    return;
+  auto* const isolate = context->GetIsolate();
+  auto const error_class = context->Global()
+                               ->Get(context, gin::StringToV8(isolate, "Error"))
+                               .ToLocalChecked()
+                               ->ToObject(context)
+                               .FromMaybe(v8::Local<v8::Object>());
+  if (!IsInstanceOf(context, js_error, error_class))
+    return;
+
+  auto const fileNameKey = gin::StringToV8(isolate, "fileName");
+  if (!js_error->Has(context, fileNameKey).FromMaybe(false))
+    js_error->Set(fileNameKey, message->GetScriptResourceName());
+
+  auto const sourceLineKey = gin::StringToV8(isolate, "sourceLine");
+  if (!js_error->Has(context, sourceLineKey).FromMaybe(false)) {
+    auto sourceLine =
+        message->GetSourceLine(context).FromMaybe(v8::Local<v8::String>());
+    if (!sourceLine.IsEmpty())
+      js_error->Set(gin::StringToV8(isolate, "sourceLine"), sourceLine);
+  }
+
+  auto const lineNumberKey = gin::StringToV8(isolate, "lineNumber");
+  if (!js_error->Has(context, lineNumberKey).FromMaybe(false)) {
+    const int lineNumber = message->GetLineNumber(context).FromMaybe(0);
+    js_error->Set(gin::StringToV8(isolate, "lineNumber"),
+                  gin::ConvertToV8(isolate, lineNumber));
+  }
+
+  auto const columnNumberKey = gin::StringToV8(isolate, "columnNumber");
+  if (!js_error->Has(context, columnNumberKey).FromMaybe(false)) {
+    const int columnNumber = message->GetStartColumn(context).FromMaybe(0);
+    js_error->Set(gin::StringToV8(isolate, "columnNumber"),
+                  gin::ConvertToV8(isolate, columnNumber));
+  }
+}
+
 }  // namespace
 
 enum class NativeScriptModule::State {
@@ -160,12 +225,15 @@ NativeScriptModule* NativeScriptModule::Compile(
   auto* const isolate = script_host->isolate();
   auto* const runner = script_host->runner();
   ginx::Runner::Scope runner_scope(runner);
+  v8::TryCatch try_catch(isolate);
   v8::ScriptOrigin script_origin(gin::StringToV8(isolate, specifier));
   v8::ScriptCompiler::Source source(gin::StringToV8(isolate, script_text),
                                     script_origin);
   auto module = v8::ScriptCompiler::CompileModule(isolate, &source)
                     .FromMaybe(v8::Local<v8::Module>());
   if (module.IsEmpty()) {
+    ProcessMessage(runner->context(), try_catch);
+    try_catch.ReThrow();
     exception_state->set_is_thrown();
     return nullptr;
   }

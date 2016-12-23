07e73100559b2d355a3fc8d8ba2a57d7477b01f8
eval1749 <eval1749@gmail.com>
2016-12-23 11:16:01 +0900

    joana - Utilize template function in OperatorCache to simplify code for improving code health.

--- joana/ir/operator_factory.cc
+++ joana/ir/operator_factory.cc
@@ -24,24 +24,49 @@ class OperatorFactory::Cache final {
   explicit Cache(Zone* zone);
   ~Cache() = default;
 
-  const Operator* Find(OperationCode opcode) const;
-  const Operator* Find(OperationCode opcode, size_t value) const;
+  const Operator& GetBoolOperator(bool value) const;
+  const Operator& Set(const Operator& op);
+  const Operator* TryGet(OperationCode opcode) const;
 
-  const Operator* FindByFloat64(float64_t data) const;
-  const Operator* FindByInt64(int64_t data) const;
-  const Operator* FindByString(base::StringPiece16 data) const;
+  // TODO(eval1749): We should share |Cache| implementation with
+  // |TypeFactory::Cache| via template.
+  template <typename Key>
+  const Operator* Find(const Key& key) {
+    const auto& map = MapFor(key);
+    const auto& it = map.find(key);
+    return it == map.end() ? nullptr : it->second;
+  }
 
-  const Operator& GetBoolOperator(bool data) const;
-
-  const Operator* Set(const Operator* op);
-  const Operator* Set(const Operator* op, size_t value);
-
-  const Operator* SetWithFloat64(const Operator* op, float64_t data);
-  const Operator* SetWithInt64(const Operator* op, int64_t data);
-  const Operator* SetWithString(const Operator* op, base::StringPiece16 data);
+  template <typename Key>
+  const Operator& Register(const Key& key, const Operator& op) {
+    auto& map = MapFor(key);
+    const auto& result = map.emplace(key, &op);
+    DCHECK(result.second);
+    return *result.first->second;
+  }
 
  private:
   Zone& zone_;
+
+  std::unordered_map<float64_t, const Operator*>& MapFor(float64_t) {
+    return float64_map_;
+  }
+
+  std::unordered_map<int64_t, const Operator*>& MapFor(int64_t) {
+    return int64_map_;
+  }
+
+  std::map<std::pair<OperationCode, size_t>, const Operator*>& MapFor(
+      const std::pair<OperationCode, size_t>&) {
+    return size_map_;
+  }
+
+  std::unordered_map<base::StringPiece16,
+                     const Operator*,
+                     base::StringPiece16Hash>&
+  MapFor(base::StringPiece16) {
+    return string_map_;
+  }
 
   const Operator& false_operator_;
   const Operator& true_operator_;
@@ -63,72 +88,19 @@ OperatorFactory::Cache::Cache(Zone* zone)
       true_operator_(*new (&zone_) LiteralBoolOperator(true)),
       map_(kNumberOfOperations) {}
 
-const Operator* OperatorFactory::Cache::Find(OperationCode opcode) const {
-  return map_[static_cast<size_t>(opcode)];
-}
-
-const Operator* OperatorFactory::Cache::Find(OperationCode opcode,
-                                             size_t value) const {
-  const auto& it = size_map_.find(std::make_pair(opcode, value));
-  return it == size_map_.end() ? nullptr : it->second;
-}
-
-const Operator* OperatorFactory::Cache::FindByFloat64(float64_t data) const {
-  const auto& it = float64_map_.find(data);
-  return it == float64_map_.end() ? nullptr : it->second;
-}
-
-const Operator* OperatorFactory::Cache::FindByInt64(int64_t data) const {
-  const auto& it = int64_map_.find(data);
-  return it == int64_map_.end() ? nullptr : it->second;
-}
-
-const Operator* OperatorFactory::Cache::FindByString(
-    base::StringPiece16 data) const {
-  if (data.size() > kMaxStringLengthForCache)
-    return nullptr;
-  const auto& it = string_map_.find(data);
-  return it == string_map_.end() ? nullptr : it->second;
-}
-
 const Operator& OperatorFactory::Cache::GetBoolOperator(bool data) const {
   return data ? true_operator_ : false_operator_;
 }
 
-const Operator* OperatorFactory::Cache::Set(const Operator* op) {
-  DCHECK_EQ(map_.at(static_cast<size_t>(op->opcode())), nullptr);
-  DCHECK_EQ(op->format().number_of_members(), 0u) << op;
-  map_[static_cast<size_t>(op->opcode())] = op;
+const Operator& OperatorFactory::Cache::Set(const Operator& op) {
+  DCHECK_EQ(map_.at(static_cast<size_t>(op.opcode())), nullptr);
+  DCHECK_EQ(op.format().number_of_members(), 0u) << op;
+  map_[static_cast<size_t>(op.opcode())] = &op;
   return op;
 }
 
-const Operator* OperatorFactory::Cache::Set(const Operator* op, size_t value) {
-  const auto& result =
-      size_map_.emplace(std::make_pair(op->opcode(), value), op);
-  DCHECK(result.second);
-  return result.first->second;
-}
-
-const Operator* OperatorFactory::Cache::SetWithFloat64(const Operator* op,
-                                                       float64_t data) {
-  const auto& result = float64_map_.emplace(data, op);
-  DCHECK(result.second);
-  return result.first->second;
-}
-
-const Operator* OperatorFactory::Cache::SetWithInt64(const Operator* op,
-                                                     int64_t data) {
-  const auto& result = int64_map_.emplace(data, op);
-  DCHECK(result.second);
-  return result.first->second;
-}
-
-const Operator* OperatorFactory::Cache::SetWithString(
-    const Operator* op,
-    base::StringPiece16 data) {
-  const auto& result = string_map_.emplace(data, op);
-  DCHECK(result.second);
-  return result.first->second;
+const Operator* OperatorFactory::Cache::TryGet(OperationCode opcode) const {
+  return map_[static_cast<size_t>(opcode)];
 }
 
 //
@@ -139,18 +111,19 @@ OperatorFactory::OperatorFactory(Zone* zone)
 
 OperatorFactory::~OperatorFactory() = default;
 
-#define IMPLEMENT_FACTORY_FUNCTION_0(name)              \
-  const Operator& OperatorFactory::New##name() {        \
-    if (auto* op = cache_->Find(OperationCode::##name)) \
-      return *op;                                       \
-    return *cache_->Set(new (&zone_) name##Operator()); \
+#define IMPLEMENT_FACTORY_FUNCTION_0(name)                \
+  const Operator& OperatorFactory::New##name() {          \
+    if (auto* op = cache_->TryGet(OperationCode::##name)) \
+      return *op;                                         \
+    return cache_->Set(*new (&zone_) name##Operator());   \
   }
 
-#define IMPLEMENT_FACTORY_FUNCTION_1(name, type1)                     \
-  const Operator& OperatorFactory::New##name(type1 value1) {          \
-    if (auto* op = cache_->Find(OperationCode::##name, value1))       \
-      return *op;                                                     \
-    return *cache_->Set(new (&zone_) name##Operator(value1), value1); \
+#define IMPLEMENT_FACTORY_FUNCTION_1(name, type1)                        \
+  const Operator& OperatorFactory::New##name(type1 value1) {             \
+    const auto& key = std::make_pair(OperationCode::##name, value1);     \
+    if (auto* op = cache_->Find(key))                                    \
+      return *op;                                                        \
+    return cache_->Register(key, *new (&zone_) name##Operator(value1));  \
   }
 
 IMPLEMENT_FACTORY_FUNCTION_0(Exit)
@@ -160,22 +133,21 @@ const Operator& OperatorFactory::NewLiteralBool(bool data) {
 }
 
 const Operator& OperatorFactory::NewLiteralFloat64(float64_t data) {
-  if (auto* op = cache_->FindByFloat64(data))
+  if (auto* op = cache_->Find(data))
     return *op;
-  return *cache_->SetWithFloat64(new (&zone_) LiteralFloat64Operator(data),
-                                 data);
+  return cache_->Register(data, *new (&zone_) LiteralFloat64Operator(data));
 }
 
 const Operator& OperatorFactory::NewLiteralInt64(int64_t data) {
-  if (auto* op = cache_->FindByInt64(data))
+  if (auto* op = cache_->Find(data))
     return *op;
-  return *cache_->SetWithInt64(new (&zone_) LiteralInt64Operator(data), data);
+  return cache_->Register(data, *new (&zone_) LiteralInt64Operator(data));
 }
 
 const Operator& OperatorFactory::NewLiteralString(base::StringPiece16 data) {
-  if (auto* op = cache_->FindByString(data))
+  if (auto* op = cache_->Find(data))
     return *op;
-  return *cache_->SetWithString(new (&zone_) LiteralStringOperator(data), data);
+  return cache_->Register(data, *new (&zone_) LiteralStringOperator(data));
 }
 
 IMPLEMENT_FACTORY_FUNCTION_0(LiteralVoid)

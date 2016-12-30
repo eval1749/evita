9e2f8f5a166733b70a8760acab7b373afe5fff80
eval1749 <eval1749@gmail.com>
2016-12-30 16:11:20 +0900

    joana - Add missing files "joana/ast/compilation_units.{cc,h}"

---
+++ joana/ast/compilation_units.cc
@@ -0,0 +1,53 @@
+// Copyright (c) 2016 Project Vogue. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "joana/ast/compilation_units.h"
+
+namespace joana {
+namespace ast {
+
+//
+// CompilationUnit
+//
+CompilationUnit::CompilationUnit(
+    Zone* zone,
+    const SourceCodeRange& range,
+    const StatementList& statements,
+    const std::unordered_map<const Node*, const ast::JsDoc*>& jsdoc_map)
+    : NodeTemplate(&statements, range),
+      jsdoc_map_(zone, jsdoc_map.begin(), jsdoc_map.end()) {}
+
+CompilationUnit::~CompilationUnit() = default;
+
+const JsDoc* CompilationUnit::JsDocFor(const ast::Node& node) const {
+  const auto& it = jsdoc_map_.find(&node);
+  return it == jsdoc_map_.end() ? nullptr : it->second;
+}
+
+//
+// Externs
+//
+Externs::Externs(
+    Zone* zone,
+    const SourceCodeRange& range,
+    const StatementList& statements,
+    const std::unordered_map<const Node*, const ast::JsDoc*>& jsdoc_map)
+    : CompilationUnit(zone, range, statements, jsdoc_map) {}
+
+Externs::~Externs() = default;
+
+//
+// Module
+//
+Module::Module(
+    Zone* zone,
+    const SourceCodeRange& range,
+    const StatementList& statements,
+    const std::unordered_map<const Node*, const ast::JsDoc*>& jsdoc_map)
+    : CompilationUnit(zone, range, statements, jsdoc_map) {}
+
+Module::~Module() = default;
+
+}  // namespace ast
+}  // namespace joana
---
+++ joana/ast/compilation_units.h
@@ -0,0 +1,85 @@
+// Copyright (c) 2016 Project Vogue. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef JOANA_AST_COMPILATION_UNITS_H_
+#define JOANA_AST_COMPILATION_UNITS_H_
+
+#include <unordered_map>
+
+#include "joana/ast/node.h"
+#include "joana/base/memory/zone_unordered_map.h"
+
+namespace joana {
+namespace ast {
+
+class JsDoc;
+class StatementList;
+
+//
+// CompilationUnit
+//
+class JOANA_AST_EXPORT CompilationUnit
+    : public NodeTemplate<Node, const StatementList*> {
+  DECLARE_ABSTRACT_AST_NODE(CompilationUnit, Node);
+
+ public:
+  ~CompilationUnit() override;
+
+  const StatementList& statements() const { return *member_at<0>(); }
+
+  const JsDoc* JsDocFor(const ast::Node& node) const;
+
+ protected:
+  CompilationUnit(
+      Zone* zone,
+      const SourceCodeRange& range,
+      const StatementList& statements,
+      const std::unordered_map<const Node*, const ast::JsDoc*>& jsdoc_map);
+
+ private:
+  const ZoneUnorderedMap<const Node*, const ast::JsDoc*> jsdoc_map_;
+
+  DISALLOW_COPY_AND_ASSIGN(CompilationUnit);
+};
+
+//
+// Externs
+//
+class JOANA_AST_EXPORT Externs final : public CompilationUnit {
+  DECLARE_CONCRETE_AST_NODE(Externs, CompilationUnit);
+
+ public:
+  ~Externs() final;
+
+ private:
+  Externs(Zone* zone,
+         const SourceCodeRange& range,
+         const StatementList& statements,
+         const std::unordered_map<const Node*, const ast::JsDoc*>& jsdoc_map);
+
+  DISALLOW_COPY_AND_ASSIGN(Externs);
+};
+
+//
+// Module
+//
+class JOANA_AST_EXPORT Module final : public CompilationUnit {
+  DECLARE_CONCRETE_AST_NODE(Module, CompilationUnit);
+
+ public:
+  ~Module() final;
+
+ private:
+  Module(Zone* zone,
+         const SourceCodeRange& range,
+         const StatementList& statements,
+         const std::unordered_map<const Node*, const ast::JsDoc*>& jsdoc_map);
+
+  DISALLOW_COPY_AND_ASSIGN(Module);
+};
+
+}  // namespace ast
+}  // namespace joana
+
+#endif  // JOANA_AST_COMPILATION_UNITS_H_

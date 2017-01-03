9cf67ee5d2cdcae6731946cf5a8b0909a30576c5
eval1749 <eval1749@gmail.com>
2017-01-03 23:22:49 +0900

    joana - Cleanup ast::Node

--- joana/ast/node.h
+++ joana/ast/node.h
@@ -5,29 +5,22 @@
 #ifndef JOANA_AST_NODE_H_
 #define JOANA_AST_NODE_H_
 
-#include <array>
 #include <iosfwd>
-#include <iterator>
-#include <tuple>
-#include <type_traits>
-#include <vector>
 
-#include "base/logging.h"
 #include "base/macros.h"
 #include "joana/ast/ast_export.h"
-#include "joana/base/castable.h"
-#include "joana/base/memory/zone_allocated.h"
-#include "joana/base/memory/zone_vector.h"
 #include "joana/base/source_code_range.h"
 
 namespace joana {
 namespace ast {
 
 enum class TokenKind;
-enum class TokenKind;
 class Syntax;
 enum class SyntaxCode;
 
+//
+// Node
+//
 class JOANA_AST_EXPORT Node final {
  public:
   ~Node();
@@ -43,6 +36,7 @@ class JOANA_AST_EXPORT Node final {
   bool operator==(SyntaxCode syntax_code) const;
   bool operator!=(SyntaxCode syntax_code) const;
 
+  // Returns number of operands in this node.
   size_t arity() const { return arity_; }
   const Node& child_at(size_t index) const;
   const SourceCode& source_code() const { return range_.source_code(); }
@@ -57,9 +51,20 @@ class JOANA_AST_EXPORT Node final {
  private:
   friend class NodeFactory;
 
+  // TODO(eval1749): Once we need to have other |int| values, we should reduce
+  // size of |arity_|.
+  // |arity_| holds number of child nodes. This value equals to
+  // |syntax_.arity()| if |!syntax_.is_variadic()|.
   const size_t arity_;
+
+  // Range of source code where this node comes from.
   const SourceCodeRange range_;
+
+  // The syntax of this node.
   const Syntax& syntax_;
+
+  // |nodes_| should be the last member of this class. Child nodes are allocated
+  // starting from here.
   const Node* nodes_[1];
 
   DISALLOW_COPY_AND_ASSIGN(Node);

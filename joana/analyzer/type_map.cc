0a9fa6d22348294b4a47410048c2896d6bd28231
eval1749 <eval1749@gmail.com>
2017-01-14 20:47:50 +0900

    joana - Make analyzer::TypeMap::RegisterType() to check node and type validity

--- joana/analyzer/type_map.cc
+++ joana/analyzer/type_map.cc
@@ -5,8 +5,8 @@
 #include "joana/analyzer/type_map.h"
 
 #include "base/logging.h"
-#include "joana/analyzer/type.h"
-#include "joana/ast/node.h"
+#include "joana/analyzer/types.h"
+#include "joana/ast/declarations.h"
 
 namespace joana {
 namespace analyzer {
@@ -18,6 +18,16 @@ TypeMap::TypeMap() {}
 TypeMap::~TypeMap() = default;
 
 void TypeMap::RegisterType(const ast::Node& node, const Type& type) {
+#if DCHECK_IS_ON()
+  if (node.Is<ast::Class>()) {
+    DCHECK(type.Is<ClassType>()) << node << ' ' << type;
+  } else if (node.Is<ast::Function>()) {
+    DCHECK(type.Is<ClassType>() || type.Is<FunctionType>()) << node << ' '
+                                                            << type;
+  } else if (node.Is<ast::Method>()) {
+    DCHECK(type.Is<FunctionType>()) << node << ' ' << type;
+  }
+#endif
   const auto& result = type_map_.emplace(&node, &type);
   DCHECK(result.second) << "Node can have only one type " << node << std::endl
                         << type << std::endl

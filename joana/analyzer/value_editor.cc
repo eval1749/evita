bb6e0149754c67c8eecacde13d8665271a147776
eval1749 <eval1749@gmail.com>
2017-01-21 14:29:41 +0900

    joana - Make analyzer::Value::Editor::AddAssignments() to check assigment node.

--- joana/analyzer/value_editor.cc
+++ joana/analyzer/value_editor.cc
@@ -4,10 +4,27 @@
 
 #include "joana/analyzer/value_editor.h"
 
+#include "joana/analyzer/built_in_world.h"
 #include "joana/analyzer/values.h"
+#include "joana/ast/bindings.h"
+#include "joana/ast/declarations.h"
+#include "joana/ast/expressions.h"
+#include "joana/ast/jsdoc_syntaxes.h"
 
 namespace joana {
 namespace analyzer {
+
+namespace {
+
+bool IsValidAssignment(const ast::Node& node) {
+  return node.Is<ast::AssignmentExpression>() ||
+         node.Is<ast::BindingNameElement>() || node.Is<ast::Class>() ||
+         node.Is<ast::JsDocDocument>() || node.Is<ast::Function>() ||
+         node.Is<ast::Method>() ||
+         node == BuiltInWorld::GetInstance()->NameOf(ast::TokenKind::Global);
+}
+
+}  // namespace
 
 //
 // Value::Editor
@@ -16,6 +33,7 @@ Value::Editor::Editor() = default;
 Value::Editor::~Editor() = default;
 
 void Value::Editor::AddAssignment(ValueHolder* binding, const ast::Node& node) {
+  DCHECK(IsValidAssignment(node)) << *binding << ' ' << node;
   binding->assignments_.push_back(&node);
 }
 

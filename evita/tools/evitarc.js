67877b10def9004de62475b405fcd90fd450e56e
eval1749 <eval1749@gmail.com>
2017-01-28 11:37:36 +0900

    Change copyright year to 2017 of snipet file

--- evita/tools/evitarc.js
+++ evita/tools/evitarc.js
@@ -191,7 +191,7 @@ Editor.bindKey(TextWindow, 'Ctrl+Shift+M', function() {
 // Templates
 (function() {
   const copyrightLines = [
-    'Copyright (c) 2016 Project Vogue. All rights reserved.',
+    'Copyright (c) 2017 Project Vogue. All rights reserved.',
     'Use of this source code is governed by a BSD-style license that can be',
     'found in the LICENSE file.'
   ];

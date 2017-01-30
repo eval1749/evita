9fc6e5d52299c8ea1f370ef7bd3f02f239b8a420
eval1749 <eval1749@gmail.com>
2017-01-30 12:49:20 +0900

    joana - Add missing commits "properties_editor.{cc,h}"

---
+++ joana/analyzer/properties_editor.cc
@@ -0,0 +1,30 @@
+// Copyright (c) 2017 Project Vogue. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "joana/analyzer/properties_editor.h"
+
+#include "joana/analyzer/values.h"
+#include "joana/ast/tokens.h"
+
+namespace joana {
+namespace analyzer {
+
+Properties::Editor::Editor() = default;
+Properties::Editor::~Editor() = default;
+
+void Properties::Editor::Add(Properties* properties, Property* property) {
+  if (property->key() == ast::SyntaxCode::Name) {
+    const auto& result = properties->name_map_.emplace(
+        ast::Name::IdOf(property->key()), property);
+    DCHECK(result.second);
+    return;
+  }
+  const auto& string_key = property->key().range().GetString();
+  const auto& result =
+      properties->computed_name_map_.emplace(string_key, property);
+  DCHECK(result.second);
+}
+
+}  // namespace analyzer
+}  // namespace joana
---
+++ joana/analyzer/properties_editor.h
@@ -0,0 +1,30 @@
+// Copyright (c) 2017 Project Vogue. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef JOANA_ANALYZER_PROPERTIES_EDITOR_H_
+#define JOANA_ANALYZER_PROPERTIES_EDITOR_H_
+
+#include "joana/analyzer/properties.h"
+
+namespace joana {
+namespace analyzer {
+
+//
+// Properties::Editor
+//
+class Properties::Editor final {
+ public:
+  Editor();
+  ~Editor();
+
+  void Add(Properties* properties, Property* property);
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(Editor);
+};
+
+}  // namespace analyzer
+}  // namespace joana
+
+#endif  // JOANA_ANALYZER_PROPERTIES_EDITOR_H_

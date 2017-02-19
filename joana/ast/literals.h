2c433fa5bcfcf21f292bf48b14bccbe4c3f2e4fa
eval1749 <eval1749@gmail.com>
2017-02-19 22:32:18 +0900

    joana - Introudce ast::Literal class for ease of checking literal nodes

--- joana/ast/literals.h
+++ joana/ast/literals.h
@@ -5,115 +5,105 @@
 #ifndef JOANA_AST_LITERALS_H_
 #define JOANA_AST_LITERALS_H_
 
-#include "joana/ast/syntax.h"
-#include "joana/ast/tokens.h"
+#include "joana/ast/expressions.h"
 #include "joana/base/float_type.h"
 
 namespace joana {
 namespace ast {
 
-DECLARE_AST_SYNTAX_1(BooleanLiteral, bool, value)
-DECLARE_AST_SYNTAX_0(NullLiteral)
-DECLARE_AST_SYNTAX_1(NumericLiteral, float64_t, value)
-DECLARE_AST_SYNTAX_0(StringLiteral)
-DECLARE_AST_SYNTAX_0(UndefinedLiteral)
-
-#if 0
-
-using Name = Token;
-
-class JOANA_AST_EXPORT Literal : public Token {
-  DECLARE_ABSTRACT_AST_NODE(Literal, Token);
+//
+// Literal
+//
+class JOANA_AST_EXPORT Literal : public Expression {
+  DECLARE_ABSTRACT_AST_SYNTAX(Literal, Expression);
 
  public:
   ~Literal() override;
 
  protected:
-  explicit Literal(const SourceCodeRange& range);
+  Literal(SyntaxCode syntax_code, const Format& format);
 
  private:
   DISALLOW_COPY_AND_ASSIGN(Literal);
 };
 
-class JOANA_AST_EXPORT BooleanLiteral final : public Literal {
-  DECLARE_CONCRETE_AST_NODE(BooleanLiteral, Literal);
+//
+// BooleanLiteral
+//
+class JOANA_AST_EXPORT BooleanLiteral final
+    : public SyntaxTemplate<Expression, bool> {
+  DECLARE_CONCRETE_AST_SYNTAX(BooleanLiteral, Expression);
 
  public:
   ~BooleanLiteral() final;
 
-  bool value() const { return value_; }
-
  private:
-  BooleanLiteral(const Node& name, bool value);
-
-  const bool value_;
+  explicit BooleanLiteral(bool value);
 
   DISALLOW_COPY_AND_ASSIGN(BooleanLiteral);
 };
 
-class JOANA_AST_EXPORT NullLiteral final : public Literal {
-  DECLARE_CONCRETE_AST_NODE(NullLiteral, Literal);
+//
+// NullLiteral
+//
+class JOANA_AST_EXPORT NullLiteral final : public SyntaxTemplate<Expression> {
+  DECLARE_CONCRETE_AST_SYNTAX(NullLiteral, Expression);
 
  public:
   ~NullLiteral() final;
 
  private:
-  explicit NullLiteral(const Node& name);
+  NullLiteral();
 
   DISALLOW_COPY_AND_ASSIGN(NullLiteral);
 };
 
-class JOANA_AST_EXPORT NumericLiteral final : public Literal {
-  DECLARE_CONCRETE_AST_NODE(NumericLiteral, Literal);
+//
+// NumericLiteral
+//
+class JOANA_AST_EXPORT NumericLiteral final
+    : public SyntaxTemplate<Expression, float64_t> {
+  DECLARE_CONCRETE_AST_SYNTAX(NumericLiteral, Expression);
 
  public:
   ~NumericLiteral() final;
 
-  double value() const { return value_; }
-
  private:
-  NumericLiteral(const SourceCodeRange& range, double value);
-
-  // Implements |Node| members
-  void PrintMoreTo(std::ostream* ostream) const final;
-
-  const double value_;
+  explicit NumericLiteral(float64_t value);
 
   DISALLOW_COPY_AND_ASSIGN(NumericLiteral);
 };
 
-class JOANA_AST_EXPORT StringLiteral final : public Literal {
-  DECLARE_CONCRETE_AST_NODE(StringLiteral, Literal);
+//
+// StringLiteral
+//
+class JOANA_AST_EXPORT StringLiteral final : public SyntaxTemplate<Expression> {
+  DECLARE_CONCRETE_AST_SYNTAX(StringLiteral, Expression);
 
  public:
   ~StringLiteral() final;
 
-  const base::string16& data() const { return data_; }
-
  private:
-  StringLiteral(const SourceCodeRange& range, base::StringPiece16 data);
-
-  // Implements |Node| members
-  void PrintMoreTo(std::ostream* ostream) const final;
-
-  const base::string16 data_;
+  StringLiteral();
 
   DISALLOW_COPY_AND_ASSIGN(StringLiteral);
 };
 
-class JOANA_AST_EXPORT UndefinedLiteral final : public Literal {
-  DECLARE_CONCRETE_AST_NODE(UndefinedLiteral, Literal);
+//
+// UndefinedLiteral
+//
+class JOANA_AST_EXPORT UndefinedLiteral final
+    : public SyntaxTemplate<Expression> {
+  DECLARE_CONCRETE_AST_SYNTAX(UndefinedLiteral, Expression);
 
  public:
   ~UndefinedLiteral() final;
 
  private:
-  explicit UndefinedLiteral(const Node& name);
+  UndefinedLiteral();
 
   DISALLOW_COPY_AND_ASSIGN(UndefinedLiteral);
 };
-
-#endif
 
 }  // namespace ast
 }  // namespace joana
